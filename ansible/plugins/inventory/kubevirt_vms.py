91bd0f45be82f6921cd6d4c347215f92325a49a6
Martin Sivak <msivak@redhat.com>
2019-04-11 11:33:39 +0200

    Add support for calling ansible modules from pytest
    The goal is to allow usage of existing or custom modules
    from Python code using fixtures like ansible_module:
    
    ```
    ansible_module.lineinfile(...)
    ```
    
    The patch also comes with support for dynamic inventory plugins
    that can autodetect nodes and VMs from running cluster.
    
    Please see https://github.com/ansible/pytest-ansible for more
    detailed description of pytest-ansible features.
    
    Change-Id: I8f8dd2558fa072d00c5de4e528ad8e3eaff15f4a
    Reviewed-on: https://code.engineering.redhat.com/gerrit/167921
    Tested-by: Martin Sivak <msivak@redhat.com>
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Petr Horacek <phoracek@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

---
+++ ansible/plugins/inventory/kubevirt_vms.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# vim: sw=4 sts=4 et ft=python
+"""Ansible inventory plugin for retrieving all the kubevirt VMs
+
+Use it by enabling `kubevirt_vms` in ansible.cfg and then add oc.yml
+to your inventory with the following content:
+
+```
+plugin: kubevirt_vms
+```
+"""
+from __future__ import absolute_import
+
+from six import iteritems
+
+from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
+
+from kubernetes import config
+from openshift.dynamic import DynamicClient
+
+# HACK import the inventory plugin openshift_nodes while running in Ansible
+import imp
+import os.path
+openshift_nodes = imp.load_source(
+    'openshift_nodes',
+    os.path.join(os.path.dirname(__file__), 'openshift_nodes.py'))
+ansible_annotation_vars = openshift_nodes.ansible_annotation_vars
+dict_merge = openshift_nodes.dict_merge
+
+
+def first_ip(vmi):
+    return next(intf.ipAddress
+                for intf in vmi.status.interfaces
+                if (intf.ipAddress != "127.0.0.1" and
+                    any(s.status == "True" and s.type == "Ready"
+                        for s in vmi.status.conditions)))
+
+
+def vmi_entry(vmi, extra_ssh_args):
+    """Converts a VMI instance into a host dict that is better suited
+       for the ansible inventory generator."""
+    return {
+        "name": vmi.metadata.name if vmi.metadata.name != "localhost" else "vm",
+        "namespace": vmi.metadata.namespace,
+        "groups": ["kubevirt-" + vmi.metadata.namespace],
+        "vars": dict_merge({
+            "ansible_ssh_host": first_ip(vmi),
+            "ansible_ssh_extra_args": extra_ssh_args,
+            "ansible_become": False,
+            "ansible_user": "root",
+            "ansible_become_method": "sudo",
+            "ansible_ssh_pass": "unknown"
+        }, ansible_annotation_vars(vmi.metadata.annotations))
+    }
+
+
+def get_vms():
+    k8s_client = config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+
+    v1_vmis = dyn_client.resources.get(api_version='kubevirt.io/v1alpha3', kind='VirtualMachineInstance')
+
+    v1_vmi_list = v1_vmis.get()
+    vmi_details = [vmi for vmi in v1_vmi_list.items]
+
+    # Find a master node and get SSH proxy options
+    # from it. This is needed to connect to minishift VMs
+    # as they are hidden withing the minishift host virtual machine
+    master_nodes = [node
+                    for node in openshift_nodes.get_nodes()
+                    if "master" in node["groups"]]
+    master_node = master_nodes[0]
+    extra_ssh_args = master_node.get("vars", {}).get("ssh_via_arguments", "")
+
+    return [vmi_entry(vmi, extra_ssh_args) for vmi in vmi_details
+            if vmi.status.interfaces]
+
+
+class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
+    NAME = 'kubevirt_vms'
+
+    def verify_file(self, path):
+        ''' return true/false if this is possibly a valid file for this plugin to consume '''
+        # base class verifies that file exists and is readable by current user
+        return super(InventoryModule, self).verify_file(path) and \
+            path.endswith(('kubevirt.yaml',
+                           'kubevirt.yml'))
+
+    def parse(self, inventory, loader, path, cache=True):
+        # call base method to ensure properties are available for use with other helper methods
+        super(InventoryModule, self).parse(inventory, loader, path, cache)
+
+        # this method will parse 'common format' inventory sources and
+        # update any options declared in DOCUMENTATION as needed
+        config = self._read_config_data(path)
+
+        def host_name(h):
+            return config.get("host_format", "{namespace}-{name}").format(**h)
+
+        for vm in get_vms():
+            name = host_name(vm)
+            self.inventory.add_host(name)
+            for k, v in iteritems(vm["vars"]):
+                self.inventory.set_variable(name, k, v)
+            for group in vm["groups"]:
+                self.inventory.add_group(group)
+                self.inventory.add_child(group, name)
+            for group in config.get("keyed_groups", []):
+                self.inventory.add_group(group)
+                self.inventory.add_child(group, name)
---
+++ ansible/plugins/inventory/openshift_nodes.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# vim: sw=4 sts=4 et ft=python
+"""Ansible inventory plugin to get openshift nodes from a running cluster.
+
+Use it by enabling `openshift_nodes` in ansible.cfg and then add oc.yml
+to your inventory with the following content:
+
+```
+plugin: openshift_nodes
+```
+"""
+
+from __future__ import absolute_import
+
+from kubernetes import config
+from openshift.dynamic import DynamicClient
+
+from six import iteritems
+import json
+
+from ansible.plugins.inventory import BaseInventoryPlugin
+
+
+def ansible_annotation_vars(annotations):
+    if annotations is None:
+        return {}
+    return json.loads(annotations.get("ansible", "{}"))
+
+
+def dict_merge(*dicts):
+    out = {}
+    for d in dicts:
+        out.update(d)
+    return out
+
+
+def compute_groups(labels):
+    groups = []
+    for k, v in iteritems(labels):
+        if k.startswith("node-role.kubernetes.io/") and v:
+            groups.append(k.split("/")[1])
+
+    return groups or ["master", "infra", "worker"]
+
+
+def get_nodes():
+    k8s_client = config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+
+    v1_nodes = dyn_client.resources.get(api_version='v1', kind='Node')
+    v1_node_list = v1_nodes.get()
+    node_details = [node for node in v1_node_list.items]
+
+    def first_ip(node):
+        return next(addr.address
+                    for addr in node.status.addresses
+                    if (addr.address != "localhost" and
+                        addr.address != "127.0.0.1"))
+
+    nodes = [{
+        "name": node.metadata.name if node.metadata.name != "localhost" else "node",
+        "groups": ["cnv"] + compute_groups(node.metadata.labels or {}),
+        "vars": dict_merge({
+            "ansible_ssh_host": first_ip(node),
+            "ansible_become": True,
+            "ansible_become_method": "sudo",
+
+            # Enable for minishift
+            # "ansible_user": "docker",
+            # "ansible_ssh_private_key_file": "~/.minishift/machines/minishift/id_rsa",
+            # "ssh_via_arguments": """
+            #      -o UserKnownHostsFile=/dev/null
+            #      -o StrictHostKeyChecking=no
+            #      -o 'ProxyCommand ssh -o UserKnownHostsFile=/dev/null
+            #                           -o StrictHostKeyChecking=no
+            #                           -W %h:%p
+            #                           -i ~/.minishift/machines/minishift/id_rsa
+            #                           docker@""" + first_ip(node) + "'"
+
+        }, ansible_annotation_vars(node.metadata.annotations))
+    } for node in node_details]
+
+    return nodes
+
+
+class InventoryModule(BaseInventoryPlugin):
+    NAME = 'openshift_nodes'  # used internally by Ansible, it should match the file name but not required
+
+    def verify_file(self, path):
+        ''' return true/false if this is possibly a valid file for this plugin to consume '''
+        # base class verifies that file exists and is readable by current user
+        return super(InventoryModule, self).verify_file(path) and \
+            path.endswith(('openshift.yaml',
+                           'openshift.yml',
+                           'minishift.yaml',
+                           'minishift.yml',
+                           'oc.yaml',
+                           'oc.yml'))
+
+    def parse(self, inventory, loader, path, cache=True):
+        # call base method to ensure properties are available for use with other helper methods
+        super(InventoryModule, self).parse(inventory, loader, path, cache)
+
+        # This method will parse 'common format' inventory sources and
+        # update any options declared in DOCUMENTATION as needed
+        # Enable the following line once that is needed.
+        # config = self._read_config_data(path)
+
+        for node in get_nodes():
+            self.inventory.add_host(node["name"])
+            for k, v in iteritems(node["vars"]):
+                self.inventory.set_variable(node["name"], k, v)
+            for group in node["groups"]:
+                self.inventory.add_group(group)
+                self.inventory.add_child(group, node["name"])
