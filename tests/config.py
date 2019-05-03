c8a6aa106edc12923a886228c46b7aac91abaf3a
Dan Kenigsberg <danken@redhat.com>
2019-05-03 14:14:47 +0300

    config: drop two unused constants
    Change-Id: Ia6e3d2e70ac310241238b94c2729665ec441397b
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/169350
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Tested-by: Meni Yakove <myakove@redhat.com>

--- tests/config.py
+++ tests/config.py
@@ -5,10 +5,5 @@ KUBEVIRT_NS = "kubevirt"
 NETWORK_NS = "network-tests-namespace"
 VIRT_NS = "cnv-virt-ns"
 
-
-# VM distro
-FEDORA_VM = "fedora"
-CIRROS_VM = "cirros"
-
 # VMS TEMPLATES
 VM_YAML_TEMPLATE = "tests/manifests/vm-template-fedora.yaml"
