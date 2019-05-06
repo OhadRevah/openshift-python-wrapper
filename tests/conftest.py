0e878d4420ae4688830ef72984f83cb93aaa59e6
Meni Yakove <myakove@redhat.com>
2019-05-06 13:07:13 +0300

    Move env related fixture to top level conftest.
    When you run specific tests for example tests/virt it will fail
    on bond_support for example since bond_support fixture is called
    only when running tests/network and create_vms fixture need bond_support
    variable
    
    Change-Id: I11d9fed272f277b305c954cde3385816575f7e48
    Signed-off-by: Meni Yakove <myakove@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/169471
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,10 @@ import os
 import kubernetes
 import pytest
 from openshift.dynamic import DynamicClient
+from pytest_testconfig import config as py_config
+
+from resources.node import Node
+from resources.pod import Pod
 
 
 def pytest_configure():
@@ -82,3 +86,81 @@ def default_client():
     Get DynamicClient
     """
     return DynamicClient(kubernetes.config.new_client_from_config())
+
+
+@pytest.fixture(scope='session')
+def schedulable_node_ips(default_client):
+    """
+    Store all kubevirt.io/schedulable=true IPs
+    """
+    node_ips = {}
+    for node in Node.get_resources(
+        default_client, label_selector="kubevirt.io/schedulable=true"
+    ):
+        for addr in node.instance.status.addresses:
+            if addr.type == "InternalIP":
+                node_ips[node.name] = addr.address
+    return node_ips
+
+
+@pytest.fixture(scope='session')
+def get_privileged_pods(default_client):
+    """
+    Get ovs-cni pods names
+    """
+    for pod in Pod.get_resources(default_client, label_selector=py_config['priviliged_pod_label_selector']):
+        node = pod.node()
+        if [i for i in node.instance.metadata.labels.keys() if 'worker' in i]:
+            pytest.privileged_pods.append(pod)
+            pod_containers = pod.containers()
+            if pod_containers:
+                pytest.privileged_pod_container = pod_containers[0].name
+
+    assert pytest.privileged_pods, "No privileged pods found"
+
+
+@pytest.fixture(scope='session')
+def is_bare_metal(get_privileged_pods):
+    """
+    Check if setup is on bare-metal
+    """
+    for pod in pytest.privileged_pods:
+        pod_container = pod.containers()[0].name
+        pytest.active_node_nics[pod.name] = []
+        assert pod.wait_for_status(status=Pod.Status.RUNNING)
+        nics = pod.execute(
+            command=[
+                "bash", "-c",
+                "ls -l /sys/class/net/ | grep -v virtual | grep net | rev | cut -d '/' -f 1 | rev"
+            ], container=pod_container
+        )
+        nics = nics.splitlines()
+        default_gw = pod.execute(
+            command=["ip", "route", "show", "default"], container=pod_container
+        )
+        for nic in nics:
+            nic_state = pod.execute(
+                command=["cat", f"/sys/class/net/{nic}/operstate"], container=pod_container
+            )
+            if nic_state.strip() == "up":
+                if nic in [i for i in default_gw.splitlines() if 'default' in i][0]:
+                    continue
+
+                pytest.active_node_nics[pod.name].append(nic)
+                driver = pod.execute(
+                    command=[
+                        "bash", "-c",
+                        f"basename $(readlink -f /sys/class/net/{nic}/device/driver/module/)"
+                    ], container=pod_container
+                )
+                pytest.real_nics_env = driver.strip() != "virtio_net"
+
+
+@pytest.fixture(scope='session')
+def bond_supported(is_bare_metal):
+    """
+    Check if setup support BOND (have more then 2 NICs up)
+    """
+    return max(
+        [len(pytest.active_node_nics[i.name]) for i in pytest.privileged_pods]
+    ) > 2
--- tests/network/conftest.py
+++ tests/network/conftest.py
@@ -5,11 +5,8 @@ Pytest conftest file for CNV network tests
 """
 
 import pytest
-from pytest_testconfig import config as py_config
 
 from resources.namespace import Namespace
-from resources.node import Node
-from resources.pod import Pod
 from tests.network import config
 
 
@@ -40,81 +37,3 @@ def create_namespaces(request):
     ns = Namespace(name=config.NETWORK_NS)
     ns.create(wait=True)
     ns.wait_for_status(status=Namespace.Status.ACTIVE)
-
-
-@pytest.fixture(scope='session')
-def schedulable_node_ips(default_client):
-    """
-    Store all kubevirt.io/schedulable=true IPs
-    """
-    node_ips = {}
-    for node in Node.get_resources(
-        default_client, label_selector="kubevirt.io/schedulable=true"
-    ):
-        for addr in node.instance.status.addresses:
-            if addr.type == "InternalIP":
-                node_ips[node.name] = addr.address
-    return node_ips
-
-
-@pytest.fixture(scope='session')
-def get_privileged_pods(default_client):
-    """
-    Get ovs-cni pods names
-    """
-    for pod in Pod.get_resources(default_client, label_selector=py_config['priviliged_pod_label_selector']):
-        node = pod.node()
-        if [i for i in node.instance.metadata.labels.keys() if 'worker' in i]:
-            pytest.privileged_pods.append(pod)
-            pod_containers = pod.containers()
-            if pod_containers:
-                pytest.privileged_pod_container = pod_containers[0].name
-
-    assert pytest.privileged_pods, "No privileged pods found"
-
-
-@pytest.fixture(scope='session')
-def is_bare_metal():
-    """
-    Check if setup is on bare-metal
-    """
-    for pod in pytest.privileged_pods:
-        pod_container = pod.containers()[0].name
-        pytest.active_node_nics[pod.name] = []
-        assert pod.wait_for_status(status=Pod.Status.RUNNING)
-        nics = pod.execute(
-            command=[
-                "bash", "-c",
-                "ls -l /sys/class/net/ | grep -v virtual | grep net | rev | cut -d '/' -f 1 | rev"
-            ], container=pod_container
-        )
-        nics = nics.splitlines()
-        default_gw = pod.execute(
-            command=["ip", "route", "show", "default"], container=pod_container
-        )
-        for nic in nics:
-            nic_state = pod.execute(
-                command=["cat", f"/sys/class/net/{nic}/operstate"], container=pod_container
-            )
-            if nic_state.strip() == "up":
-                if nic in [i for i in default_gw.splitlines() if 'default' in i][0]:
-                    continue
-
-                pytest.active_node_nics[pod.name].append(nic)
-                driver = pod.execute(
-                    command=[
-                        "bash", "-c",
-                        f"basename $(readlink -f /sys/class/net/{nic}/device/driver/module/)"
-                    ], container=pod_container
-                )
-                pytest.real_nics_env = driver.strip() != "virtio_net"
-
-
-@pytest.fixture(scope='session')
-def bond_supported():
-    """
-    Check if setup support BOND (have more then 2 NICs up)
-    """
-    return max(
-        [len(pytest.active_node_nics[i.name]) for i in pytest.privileged_pods]
-    ) > 2
