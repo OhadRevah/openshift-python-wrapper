75269e5559aa120a780fa56d9e60205e4cf5ad86
Irit goihman <igoihman@redhat.com>
2019-05-08 15:55:31 +0300

    storage: introduce PersistentVolumeClaim class
    PersistentVolumeClaim is used to mount a Persistent
    Volume to a pod. This is a way users can claim durable
    storage.
    
    Signed-off-by: Irit goihman <igoihman@redhat.com>
    Change-Id: Iedf744551b98ae75256f91c54a0c40ab27d7ca7e
    Reviewed-on: https://code.engineering.redhat.com/gerrit/169689
    Reviewed-by: Meni Yakove <myakove@redhat.com>

---
+++ resources/persistent_volume_claim.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from .resource import NamespacedResource
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class PersistentVolumeClaim(NamespacedResource):
+    """
+    PersistentVolumeClaim object
+    """
+
+    api_version = 'v1'
+    kind = 'PersistentVolumeClaim'
+
+    def bound(self):
+        """
+        Check if PVC is bound
+
+        Returns:
+            bool: True if bound else False
+        """
+        LOGGER.info(f"Check if {self.kind} {self.name} is bound")
+        return self.status() == 'Bound'
