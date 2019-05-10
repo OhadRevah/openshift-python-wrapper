e7217f1bff3fb518fde281a4bc8f1272be49b0a2
Meni Yakove <myakove@redhat.com>
2019-05-10 14:36:43 +0300

    Network: Use more specific fixtures.
    Test test_veth_removed_after_vms_deleted: fixtures for create VMs.
    Use contextmanager and per action fixtures.
    
    Change-Id: I1a02bf0b1ace0be6e01a45a56d2ffa445e039496
    Signed-off-by: Meni Yakove <myakove@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/169896
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>

--- tests/network/general/test_veth_removed_after_vms_deleted.py
+++ tests/network/general/test_veth_removed_after_vms_deleted.py
@@ -4,16 +4,21 @@
 Veth interfaces deleted after VMs are removed
 """
 
-import pytest
+import contextlib
+import logging
 
+import pytest
+from pytest_testconfig import config as py_config
+
+from resources.namespace import Namespace
+from resources.network_attachment_definition import BridgeNetworkAttachmentDefinition
 from resources.virtual_machine import VirtualMachine
-from tests.fixtures import (
-    create_vms_from_template,
-    wait_until_vmis_running,
-    start_vms,
-)
+from tests.network import utils as net_utils, config
 from utilities import utils
-from . import config
+
+LOGGER = logging.getLogger(__name__)
+BR1TEST = "br1test"
+BR1VLAN100 = "br1vlan100"
 
 
 def count_veth_devices_on_host(pod, pod_container):
@@ -34,43 +39,133 @@ def count_veth_devices_on_host(pod, pod_container):
     return int(out.strip())
 
 
+class VirtualMachineAttachedToBridge(VirtualMachine):
+    def _add_bridge_interface(self, res, name):
+        res['spec']['template']["spec"]["domain"]["devices"]["interfaces"].append({
+            "name": name,
+            "bridge": {},
+        })
+
+    def _add_multus_network(self, res, name, net_name):
+        res['spec']['template']["spec"]["networks"].append({
+            "name": name,
+            "multus": {
+                "networkName": net_name,
+            },
+        })
+
+    def _to_dict(self):
+        res = super()._to_dict()
+        vm_attrs = {
+            "label": "fedora-vm",
+            "cpu_cores": 1,
+            "memory": "1024Mi",
+            "name": self.name
+        }
+        json_out = utils.generate_yaml_from_template(file_=config.VM_YAML_FEDORA, **vm_attrs)
+        res['metadata'] = json_out['metadata']
+        res['spec'] = json_out['spec']
+
+        # Add multus networks and interfaces
+        net1_network = "net1"
+        self._add_multus_network(res=res, name=net1_network, net_name=BR1TEST)
+        self._add_bridge_interface(res=res, name=net1_network)
+
+        net2_network = "net2"
+        self._add_multus_network(res=res, name=net2_network, net_name=BR1VLAN100)
+        self._add_bridge_interface(res=res, name=net2_network)
+
+        return res
+
+
+@contextlib.contextmanager
+def bridge_nad(namespace, name):
+    cni_type = py_config['template_defaults']['bridge_cni_name']
+    with BridgeNetworkAttachmentDefinition(
+            namespace=namespace.name,
+            name=name,
+            bridge_name=BR1TEST,
+            cni_type=cni_type) as nad:
+        yield nad
+
+
+@pytest.fixture()
+def namespace():
+    with Namespace(name=__name__.split(".")[-1].replace("_", "-")) as ns:
+        yield ns
+
+
+@pytest.fixture()
+def br1test_nad(namespace):
+    with bridge_nad(namespace=namespace, name=BR1TEST) as nad:
+        yield nad
+
+
+@pytest.fixture()
+def br1vlan100_nad(namespace):
+    with bridge_nad(namespace=namespace, name=BR1VLAN100) as nad:
+        yield nad
+
+
+@pytest.fixture()
+def bridge_device(network_utility_pods):
+    with net_utils.Bridge(name=BR1TEST, worker_pods=network_utility_pods) as dev:
+        yield from dev
+
+
+@pytest.fixture()
+def bridge_attached_vma(namespace):
+    with VirtualMachineAttachedToBridge(namespace=namespace.name, name="vma") as vm:
+        assert vm.start()
+        yield vm
+
+
+@pytest.fixture()
+def bridge_attached_vmb(namespace):
+    with VirtualMachineAttachedToBridge(namespace=namespace.name, name="vmb") as vm:
+        assert vm.start()
+        yield vm
+
+
+@pytest.fixture()
+def running_bridge_attached_vma(bridge_attached_vma):
+    assert bridge_attached_vma.vmi().wait_until_running()
+    return bridge_attached_vma
+
+
+@pytest.fixture()
+def running_bridge_attached_vmb(bridge_attached_vmb):
+    assert bridge_attached_vmb.vmi().wait_until_running()
+    return bridge_attached_vmb
+
+
 @pytest.mark.polarion("CNV-681")
-@pytest.mark.usefixtures(
-    'network_cr_linux_bridge_br1',
-    'network_cr_linux_bridge_br1vlan100',
-    'linux_bridge_br1',
-    create_vms_from_template.__name__,
-    start_vms.__name__,
-    wait_until_vmis_running.__name__,
-)
-class TestVethRemovedAfterVmsDeleted(object):
+def test_veth_removed_from_host_after_vm_deleted(
+    network_utility_pods,
+    br1test_nad,
+    br1vlan100_nad,
+    bridge_device,
+    bridge_attached_vma,
+    bridge_attached_vmb,
+    running_bridge_attached_vma,
+    running_bridge_attached_vmb
+):
     """
-    Check that veth interfaces are removed from host after VMs deleted
+    Check that veth interfaces are removed from host after VM deleted
     """
-    namespace = config.NETWORK_NS
-    vms = config.VETH_REMOVED_VMS
-    template = config.VM_YAML_FEDORA
-    template_kwargs = config.VM_FEDORA_ATTRS
+    for vm in (bridge_attached_vma, bridge_attached_vmb):
+        vm_interfaces = vm.instance.status.interfaces or []
+        for pod in network_utility_pods:
+            pod_container = pod.containers()[0].name
+            if pod.node.name == vm.node.name:
+                host_veth_before_delete = count_veth_devices_on_host(pod, pod_container)
+                assert vm.delete(wait=True)
+                expect_host_veth = host_veth_before_delete - len(vm_interfaces)
 
-    def test_veth_removed_from_host_after_vm_deleted(self, network_utility_pods):
-        """
-        Check that veth interfaces are removed from host after VM deleted
-        """
-        for vm in self.vms:
-            vm_object = VirtualMachine(name=vm, namespace=self.namespace)
-            vm_interfaces = vm_object.instance.status.interfaces or []
-            for pod in network_utility_pods:
-                pod_container = pod.containers()[0].name
-                pod_node = pod.node
-                if pod_node.name == vm_object.node.name:
-                    host_vath_before_delete = count_veth_devices_on_host(pod, pod_container)
-                    assert vm_object.delete(wait=True)
-                    expect_host_veth = host_vath_before_delete - len(vm_interfaces)
-
-                    sampler = utils.TimeoutSampler(
-                        timeout=30, sleep=1,
-                        func=lambda pod, pod_container, expect_host_veth:
-                            count_veth_devices_on_host(pod, pod_container) == expect_host_veth,
-                        pod=pod, pod_container=pod_container, expect_host_veth=expect_host_veth
-                    )
-                    sampler.wait_for_func_status(result=True)
+                sampler = utils.TimeoutSampler(
+                    timeout=30, sleep=1,
+                    func=lambda pod, pod_container, expect_host_veth:
+                        count_veth_devices_on_host(pod, pod_container) == expect_host_veth,
+                    pod=pod, pod_container=pod_container, expect_host_veth=expect_host_veth
+                )
+                sampler.wait_for_func_status(result=True)
