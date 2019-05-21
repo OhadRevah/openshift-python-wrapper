83830d1304045278097115c4bf04fc25e5d2a5af
Daniel Erez <derez@redhat.com>
2019-05-21 18:30:37 +0300

    storage utils: use resource client
    utils -> create_dv_from_template:
    use client from DataVolume resource for 'create_from_dict'.
    
    Change-Id: Ic103dc6cc91fd57d25aee19bd38ab49508e7aef1
    Signed-off-by: Daniel Erez <derez@redhat.com>
    Signed-off-by: Fred Rolland <frolland@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/170956
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/storage/utils.py
+++ tests/storage/utils.py
@@ -4,11 +4,10 @@ from resources.datavolume import DataVolume
 from utilities import utils
 
 
-def create_dv_from_template(dyn_client, template, **kwargs):
+def create_dv_from_template(template, **kwargs):
     """
     Create DataVolume from template
     Args:
-        dyn_client (DynamicClient): client to interact with OpenShift
         template (string): path to DataVolume template
         kwargs(dict): a dictionary containing all arguments values in template
 
@@ -18,5 +17,5 @@ def create_dv_from_template(dyn_client, template, **kwargs):
     assert 'name' in kwargs.keys() and 'namespace' in kwargs.keys()
     dv = DataVolume(name=kwargs['name'], namespace=kwargs['namespace'])
     json_out = utils.generate_yaml_from_template(file_=template, **kwargs)
-    assert dv.create_from_dict(dyn_client=dyn_client, data=json_out)
+    assert dv.create_from_dict(dyn_client=dv.client, data=json_out)
     return dv
