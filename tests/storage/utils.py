3d92763afdd64825b528e97e73ebb9b03385aa1d
Fred Rolland <frolland@redhat.com>
2019-05-22 15:43:30 +0300

    Add storage class to config yaml
    Change-Id: Id6aec0224a30e9a7bc155c1df06357f3b1772755
    Signed-off-by: Daniel Erez <derez@redhat.com>
    Signed-off-by: Fred Rolland <frolland@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/171079
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/storage/utils.py
+++ tests/storage/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from pytest_testconfig import config as py_config
 from resources.datavolume import DataVolume
 from utilities import utils
 
@@ -15,6 +16,8 @@ def create_dv_from_template(template, **kwargs):
         DataVolume created from yaml
     """
     assert 'name' in kwargs.keys() and 'namespace' in kwargs.keys()
+    template_conf = py_config['storage_defaults']
+    kwargs.update(template_conf)
     dv = DataVolume(name=kwargs['name'], namespace=kwargs['namespace'])
     json_out = utils.generate_yaml_from_template(file_=template, **kwargs)
     assert dv.create_from_dict(dyn_client=dv.client, data=json_out)
