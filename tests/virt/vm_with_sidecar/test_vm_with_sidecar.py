4aabdfddb205a60f89117eaaf896dbb8725163c2
Meni Yakove <myakove@redhat.com>
2019-05-22 18:07:27 +0300

    virt:  Don't use global generic fixtures
    vm_with_sidecar test failed since we move some fixture to network
    confitest, this patch remove the dependence of those fixtures and use
    specific fixtures for the test.
    
    Change-Id: Ide029b238f657396743ae443882f55ecdbefa7b4
    Reviewed-on: https://code.engineering.redhat.com/gerrit/171099
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Israel Pinto <ipinto@redhat.com>
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/virt/vm_with_sidecar/test_vm_with_sidecar.py
+++ tests/virt/vm_with_sidecar/test_vm_with_sidecar.py
@@ -1,43 +1,54 @@
-# -*- coding: utf-8 -*-
-
 """
 VM with sidecar
 """
+
 import pytest
 
-from tests.fixtures import (
-    create_vms_from_template,
-    wait_until_vmis_running,
-    wait_for_vmis_interfaces_report,
-    start_vms,
-)
-from tests.virt.vm_with_sidecar import config
+from tests import utils as test_utils
 from utilities import console
+from tests import config
 
 CHECK_DMIDECODE_PACKAGE = "sudo dmidecode -s baseboard-manufacturer | grep 'Radical Edward' | wc -l\n"
 
 
-class TestVMWithSidecar(object):
-    """
-    Test VM with sidecar
-    """
-    vms = config.VMS
-    namespace = config.VIRT_NS
-    template = config.VM_YAML_FEDORA
-    template_kwargs = config.VM_FEDORA_ATTRS
-
-    @pytest.mark.usefixtures(
-        create_vms_from_template.__name__,
-        start_vms.__name__,
-        wait_until_vmis_running.__name__,
-        wait_for_vmis_interfaces_report.__name__,
+@pytest.fixture()
+def side_car_vm(default_client):
+    name = "vmi-with-sidecar-hook"
+    vm_params = {
+        "metadata": {
+            "annotations": {
+                "hooks.kubevirt.io/hookSidecars": '[{"image": "kubevirt/example-hook-sidecar:v0.13.3"}]',
+                "smbios.vm.kubevirt.io/baseBoardManufacturer": "Radical Edward",
+            },
+            'labels': {'special': name},
+        },
+        "cloud_init": {
+            "bootcmd": ["dnf install -y dmidecode qemu-guest-agent"],
+            "runcmd": ["systemctl start qemu-guest-agent"]
+        }
+    }
+    vm = test_utils.create_vm_from_template(
+        default_client=default_client, name="vmi-with-sidecar-hook",
+        namespace=config.VIRT_NS, template=config.VM_YAML_FEDORA,
+        template_params=config.VM_FEDORA_ATTRS, vm_params=vm_params,
     )
-    def test_vm_with_sidecar_hook(self):
-        """
-        Test VM with sidecar hook, Install dmidecode with annotation
-        smbios.vm.kubevirt.io/baseBoardManufacturer: "Radical Edward"
-        And check that package includes manufacturer: "Radical Edward"
-        """
-        with console.Fedora(vm=config.VM_NAME, namespace=config.VIRT_NS) as vm_console:
-            vm_console.sendline(CHECK_DMIDECODE_PACKAGE)
-            vm_console.expect("1", timeout=20)
+    assert vm.start(wait=True)
+    yield vm
+    vm.delete(wait=True)
+
+
+@pytest.fixture()
+def running_side_car_vm(side_car_vm):
+    test_utils.wait_for_vm_interfaces(side_car_vm.vmi(), timeout=720)
+    yield side_car_vm
+
+
+def test_vm_with_sidecar_hook(running_side_car_vm):
+    """
+    Test VM with sidecar hook, Install dmidecode with annotation
+    smbios.vm.kubevirt.io/baseBoardManufacturer: "Radical Edward"
+    And check that package includes manufacturer: "Radical Edward"
+    """
+    with console.Fedora(vm=running_side_car_vm.name, namespace=config.VIRT_NS) as vm_console:
+        vm_console.sendline(CHECK_DMIDECODE_PACKAGE)
+        vm_console.expect("1", timeout=20)
