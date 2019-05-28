9d068829749991375e1562247ce2fca782c578a2
ipinto <ipinto@redhat.com>
2019-05-28 10:21:18 +0300

    [VIRT] Test owner references for VM, VMI, POD
    Change-Id: I91032353f1013e5a377345cc1de25b64f22f2609
    Reviewed-on: https://code.engineering.redhat.com/gerrit/171608
    Tested-by: Israel Pinto <ipinto@redhat.com>
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

---
+++ tests/virt/general/test_vm_owner_references.py
@@ -0,0 +1,51 @@
+"""
+Check VM, VMI, POD owner references
+"""
+
+import pytest
+from tests import utils as test_utils
+from tests import config
+
+
+@pytest.fixture()
+def fedora_vm(default_client):
+    name = "owner-references-vm"
+    vm = test_utils.create_vm_from_template(
+        default_client=default_client, name=name,
+        namespace=config.VIRT_NS, template=config.VM_YAML_FEDORA,
+        template_params=config.VM_FEDORA_ATTRS, vm_params={}
+    )
+    assert vm.start(wait=True)
+    yield vm
+    vm.delete(wait=True)
+
+
+@pytest.mark.polarion("CNV-1275")
+def test_owner_references_on_vm(fedora_vm):
+    """
+    Check the Owner References is fill with right data
+    like:
+        ownerReferences:
+          kind: <kind from custom resource>
+          apiVersion: <apiVersion from custom resource>
+          uid: <uid from custom resource>
+          controller: If True:
+                      The reference points to the managing controller(the owner)
+                      Garbage Collector's behavior related to the object and its owner.
+                      If False:
+                      Garbage Collector manages the object as an object without an owner,
+                      allows to delete it freely.
+    """
+    vmi = fedora_vm.vmi()
+    owner_references_pod = vmi.virt_launcher_pod().instance.metadata.ownerReferences[0]
+    owner_references_vmi = vmi.instance.metadata.ownerReferences[0]
+    # check pod owner references block
+    # kind
+    assert owner_references_pod.kind == "VirtualMachineInstance", "Pod owner references kind should be VMI"
+    assert owner_references_vmi.kind == "VirtualMachine", "VMI owner references kind should be VM"
+    # name (all relate to vm name)
+    assert owner_references_vmi.name == fedora_vm.name, "VMI owner references is not VM name"
+    assert owner_references_pod.name == owner_references_vmi.name, "Pod and VMI name are not equals"
+    # controller
+    assert owner_references_pod.controller is True, "Pod controller is not set to True"
+    assert owner_references_vmi.controller is True, "VMI controller is not set to True"
