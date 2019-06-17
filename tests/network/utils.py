cd75916b75013c13262076b2e8060c4bd6c7dc16
Dan Kenigsberg <danken@redhat.com>
2019-06-17 11:38:44 +0300

    drop unused function and template file
    Change-Id: I292f40cdb3a2b0f82f2f31e2d8285b7b0bb41775
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/173610
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/network/utils.py
+++ tests/network/utils.py
@@ -5,37 +5,9 @@ import logging
 from pytest_testconfig import config as py_config
 
 from resources.network_attachment_definition import BridgeNetworkAttachmentDefinition
-from utilities.utils import generate_yaml_from_template
 from utilities import console
 
 LOGGER = logging.getLogger(__name__)
-
-
-def generate_network_cr_from_template(name, namespace, bridge=None, cni=None, vlan=None):
-    """
-    Generate network CR from template (Jinja)
-
-    Args:
-        name (str): Network name.
-        namespace (str): Namespace where to create the network CR.
-        bridge (str): Bridge name.
-        cni (str): cni name. (cnv-bridge, bridge, ovs etc..)
-        vlan (str): VLAN id.
-
-    Returns:
-        dict: Generated dict from the template.
-    """
-    file_ = "tests/manifests/network/network-cr-template.yml"
-    template_params = {
-        'name': name,
-        'namespace': namespace,
-        'bridge': bridge or name,
-        'cni': cni or 'cnv-bridge',
-        'vlan': f'"vlan": {vlan},' if vlan else ''
-    }
-    return generate_yaml_from_template(file_=file_, **template_params)
-
-
 @contextlib.contextmanager
 def _bridge(pod, name):
     LOGGER.info(f"Adding bridge {name} using {pod.name}")
