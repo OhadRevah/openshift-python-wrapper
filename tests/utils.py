e7bee630cee84654f48a3f943767ece8f2f2a269
Dan Kenigsberg <danken@redhat.com>
2019-06-17 17:03:23 +0300

    drop unused get_fixture_val & update_vms_pod_ip_info
    Change-Id: I90c9fbe5c4feee844ba6b77915b70047f07ad337
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/173670
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Tested-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,7 @@
 import logging
-import operator
 import urllib.error
 import urllib.request
 
-from _pytest.fixtures import FixtureLookupError
 from autologs.autologs import generate_logs
 from pytest_testconfig import config as py_config
 
@@ -52,46 +50,6 @@ def wait_for_vm_interfaces(vmi, timeout=600):
     except utils.TimeoutExpiredError:
         LOGGER.error('Guest agent is not installed or not active')
         raise
-
-
-def get_fixture_val(request, attr_name, default_value=None):
-    """
-    Get request.getfixturevalue()
-
-    Args:
-        request (Request): request fixture object
-        attr_name (str): Attribute name to get
-        default_value (any): Default value if attribute not found
-
-    Returns:
-        any: fixturevalue if found else default value
-    """
-
-    def get_attr_helper(attribute, obj, default=None):
-        """
-        Helper to get attribute value from any object, works with nested
-        attributes like obj.attr1.attr2.attr3
-
-        Args:
-            attribute (str): path to the attribute
-            obj (object): object to get attribute from
-            default: default value is attribute is not found
-
-        Returns:
-            any: attr value if present, default otherwise
-        """
-        try:
-            return operator.attrgetter(attribute)(obj)
-        except AttributeError:
-            return default
-
-    try:
-        val = request.getfixturevalue(attr_name)
-        if val is None:
-            raise FixtureLookupError(argname=attr_name, request=request)
-        return val
-    except FixtureLookupError:
-        return get_attr_helper(attribute=attr_name, obj=request.cls, default=default_value)
 
 
 def get_images_http_server():
