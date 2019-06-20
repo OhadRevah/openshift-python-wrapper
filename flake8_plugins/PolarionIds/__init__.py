f35005376740919885df07d2da97c79ea296505f
Meni Yakove <myakove@redhat.com>
2019-06-21 01:12:30 +0300

    flake8 PolarionIds support polarion IDs on parametrize fixture
    Change-Id: I577b04a423234ca5abfd8eadbfd568a6d381a3b8
    Signed-off-by: Meni Yakove <myakove@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/174155
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Israel Pinto <ipinto@redhat.com>

--- flake8_plugins/PolarionIds/__init__.py
+++ flake8_plugins/PolarionIds/__init__.py
@@ -31,6 +31,39 @@ def iter_test_functions(tree):
             yield elm
 
 
+def find_func_in_tree(tree, name):
+    for elm in tree.body:
+        if isinstance(elm, ast.FunctionDef):
+            if elm.name == name:
+                return elm
+
+
+def iter_polarion_ids_from_pytest_fixture(tree, name):
+    func = find_func_in_tree(tree, name)
+    if func:
+        if not func.decorator_list:
+            return None
+
+        for deco in func.decorator_list:
+            if not hasattr(deco, 'func'):
+                continue
+
+            if deco.func.value.id == 'pytest' and deco.func.attr == 'fixture':
+                for deco_keyword in deco.keywords:
+                    if deco_keyword.arg == 'params':
+                        for deco_elts in deco_keyword.value.elts:
+                            has_polarion_id = False
+                            for deco_elts_keyword in deco_elts.keywords:
+                                if (
+                                    deco_elts_keyword.arg == 'marks' and
+                                    deco_elts_keyword.value.func.attr == 'polarion'
+                                ):
+                                    has_polarion_id = True
+                                    yield deco_elts_keyword.value.args[0]
+                            if not has_polarion_id:
+                                yield deco_elts
+
+
 class PolarionIds(object):
     """
     flake8 extension check that every test has Polarion ID attach to it.
@@ -55,14 +88,44 @@ class PolarionIds(object):
             PID001.format(f_name=f.name, params=params),
             self.name)
 
-    def _if_non_cnv(self, f, deco):
-        if deco.args:
-            if not re.match(r'CNV-\d+', deco.args[0].s):
-                yield (
-                    f.lineno,
-                    f.col_offset,
-                    PID002.format(f_name=f.name, pid=deco.args[0].s),
-                    self.name)
+    def _if_non_cnv(self, f, polarion_id):
+        if not re.match(r'CNV-\d+', polarion_id):
+            yield (
+                f.lineno,
+                f.col_offset,
+                PID002.format(f_name=f.name, pid=polarion_id),
+                self.name)
+
+    def _non_decorated_fixture(self, f, polarion_id):
+        param = ''
+        if isinstance(polarion_id, ast.Call):
+            param = polarion_id.args[0].elts[0].s
+
+        yield (
+            polarion_id.lineno,
+            polarion_id.col_offset,
+            PID001.format(f_name=f.name, params=param),
+            self.name)
+
+    def _if_non_cnv_fixture(self, f, polarion_id):
+        if not re.match(r'CNV-\d+', polarion_id.s):
+            yield (
+                polarion_id.lineno,
+                polarion_id.col_offset,
+                PID002.format(f_name=f.name, pid=polarion_id.s),
+                self.name)
+
+    def _check_pytest_fixture_polarion_ids(self, f):
+        has_polarion_id = False
+        for f_arg in f.args.args:
+            for polarion_id in iter_polarion_ids_from_pytest_fixture(self.tree, f_arg.arg):
+                if isinstance(polarion_id, ast.Str):
+                    has_polarion_id = True
+                    yield from self._if_non_cnv_fixture(f, polarion_id)
+                else:
+                    yield from self._non_decorated_fixture(f, polarion_id)
+        if not has_polarion_id:
+            yield from self._non_decorated(f, '')
 
     def run(self):
         """
@@ -70,7 +133,9 @@ class PolarionIds(object):
         """
         for f in iter_test_functions(self.tree):
             if not f.decorator_list:
-                yield from self._non_decorated(f)
+                # Test is missing Polarion ID, check if test use parametrize fixture
+                # with Polarion ID.
+                yield from self._check_pytest_fixture_polarion_ids(f)
 
             for deco in f.decorator_list:
                 if not hasattr(deco, 'func'):
@@ -78,7 +143,8 @@ class PolarionIds(object):
 
                 if deco.func.value.value.id == 'pytest' and deco.func.value.attr == 'mark':
                     if deco.func.attr == 'polarion':
-                        yield from self._if_non_cnv(f, deco)
+                        if deco.args:
+                            yield from self._if_non_cnv(f, deco.args[0].s)
 
                     elif deco.func.attr == 'parametrize':
                         if deco.args:
@@ -100,15 +166,14 @@ class PolarionIds(object):
                                         if isinstance(pk.value, ast.Tuple):
                                             for elt_val in pk.value.elts:
                                                 if elt_val.func.attr == 'polarion':
-                                                    yield from self._if_non_cnv(f, elt_val)
+                                                    yield from self._if_non_cnv(f, elt_val.args[0].s)
 
                                         # In case one mark on test param
                                         elif pk.arg == 'marks' and pk.value.func.attr == 'polarion':
-                                            if pk.value:
-                                                yield from self._if_non_cnv(f, pk.value)
-                                                continue
+                                            yield from self._if_non_cnv(f, pk.value.args[0].s)
+                                            continue
                                         else:
                                             # In case no mark on test param
                                             yield from self._non_decorated(f, elt.args[0].s)
                 else:
-                    yield from self._non_decorated(f)
+                    yield from self._non_decorated(f, '')
