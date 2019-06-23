9489c14aca66828ad6f4e5bc21e326f442b3f62d
Fred Rolland <frolland@redhat.com>
2019-06-23 09:35:32 +0300

    Cleanup storage code
    Remove unused code and yaml files.
    
    Change-Id: I77380e906d2ff998f4b5c4cb5046651057f7a41c
    Signed-off-by: Fred Rolland <frolland@redhat.com>
    Signed-off-by: Daniel Erez <derez@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/174309
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Reviewed-by: Tzvi Avni <tavni@redhat.com>
    Reviewed-by: Irit Goihman <igoihman@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/storage/utils.py
+++ tests/storage/utils.py
@@ -1,28 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pytest_testconfig import config as py_config
-from resources.datavolume import DataVolume
 from resources.virtual_machine import VirtualMachine
-from utilities import utils
-
-
-def create_dv_from_template(template, **kwargs):
-    """
-    Create DataVolume from template
-    Args:
-        template (string): path to DataVolume template
-        kwargs(dict): a dictionary containing all arguments values in template
-
-    Returns:
-        DataVolume created from yaml
-    """
-    assert 'name' in kwargs.keys() and 'namespace' in kwargs.keys()
-    template_conf = py_config['storage_defaults']
-    kwargs.update(template_conf)
-    dv = DataVolume(name=kwargs['name'], namespace=kwargs['namespace'])
-    json_out = utils.generate_yaml_from_template(file_=template, **kwargs)
-    assert dv.create_from_dict(dyn_client=dv.client, data=json_out)
-    return dv
 
 
 class VirtualMachineWithDV(VirtualMachine):
