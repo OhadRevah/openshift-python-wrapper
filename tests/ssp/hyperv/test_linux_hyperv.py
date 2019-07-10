f64f2e14fe34476db060c68bc8cd8a55ba2e0c18
Xenia Lisovskaya <xlisovsk@redhat.com>
2019-07-09 21:38:34 -0300

    [SSP] hyperV feature - checking VM XML     *Overview*:         It's test case bring CNV-2651 automation         Please note: also I want to commit another         hyperv test case(CNV-2652), which should         comming inside another PR - inside windows         patch. This patch represents Linux-only         part of hyperv automation
        *History of this patch*:
           This patch was splitted from another one:
           https://code.engineering.redhat.com/gerrit/#/c/174569/
    
    Change-Id: I638d10c1f9aac20533f8b78541ed5d2a4e211514
    Depends-On: Ic9316d2944323dd78f27a6db3d2490752ac824f0
    Reviewed-on: https://code.engineering.redhat.com/gerrit/175149
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Tested-by: Xenia Lisovskaia <xlisovsk@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

---
+++ tests/ssp/hyperv/test_linux_hyperv.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+[SSP] hyperv feature - checking VMI XML
+This test case includes only Linux based test case
+"""
+
+import xmltodict
+import pytest
+from tests.utils import FedoraVirtualMachine
+from resources.namespace import Namespace
+
+
+class HyperVVM(FedoraVirtualMachine):
+    def __init__(self, name, namespace):
+        super().__init__(
+            name=name, namespace=namespace, label="HyperV", cpu_cores=1, memory="1G"
+        )
+
+    def _to_dict(self):
+        res = super()._to_dict()
+        res["spec"]["template"]["spec"]["domain"]["clock"] = {
+            "utc": {},
+            "timer": {
+                "hpet": {"present": False},
+                "pit": {"tickPolicy": "delay"},
+                "rtc": {"tickPolicy": "catchup"},
+                "hyperv": {},
+            },
+        }
+        res["spec"]["template"]["spec"]["domain"]["features"] = {
+            "acpi": {},
+            "apic": {},
+            "hyperv": {
+                "relaxed": {},
+                "vapic": {},
+                "synictimer": {},
+                "vpindex": {},
+                "synic": {},
+                "spinlocks": {"spinlocks": 8191},
+            },
+        }
+        return res
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ssp_linuxhyperv_namespace():
+    with Namespace(name="cnv-ssp-linuxhyperv-ns") as ns:
+        ns.wait_for_status(status=Namespace.Status.ACTIVE)
+        yield ns
+
+
+@pytest.mark.polarion("CNV-2651")
+def test_linux_hyperv(ssp_linuxhyperv_namespace):
+    """
+    Linux test: check hyperV with VM dumpxml
+    """
+    with HyperVVM(name="hyperv-test", namespace=ssp_linuxhyperv_namespace.name) as vm:
+        vm.start(wait=True)
+        vmi = vm.vmi
+        vmi.wait_until_running()
+        dataxml = vmi.get_xml()
+        xml_dict = xmltodict.parse(dataxml, process_namespaces=True)
+        features = xml_dict["domain"]["features"]
+        hyperv = features["hyperv"]
+        assert hyperv["relaxed"]["@state"] == "on"
+        assert hyperv["vapic"]["@state"] == "on"
+        assert hyperv["spinlocks"]["@state"] == "on"
+        assert hyperv["spinlocks"]["@retries"] == "8191"
