97e7cfa08658aeb086102032a31be55b2c2eae7c
ipinto <ipinto@redhat.com>
2019-07-31 05:23:16 +0300

    [virt] add test vm with RNG
    Change-Id: I70b8abaff3567f7fab62c98921565bfd7616337d
    Reviewed-on: https://code.engineering.redhat.com/gerrit/177340
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Tested-by: Israel Pinto <ipinto@redhat.com>
    Reviewed-by: Dan Kenigsberg <danken@redhat.com>
    Reviewed-by: Israel Pinto <ipinto@redhat.com>

---
+++ tests/virt/general/rng/test_rng.py
@@ -0,0 +1,60 @@
+"""
+Test VM with RNG
+"""
+
+import pytest
+
+from tests import utils as test_utils
+from tests.utils import FedoraVirtualMachine
+from utilities import console
+from resources.namespace import Namespace
+
+
+class FedoraVirtualMachineWithRNG(FedoraVirtualMachine):
+    def __init__(self, name, namespace, interfaces=None, networks=None, **vm_attr):
+        super().__init__(
+            name=name,
+            namespace=namespace,
+            interfaces=interfaces,
+            networks=networks,
+            **vm_attr,
+        )
+
+    def _to_dict(self):
+        res = super()._to_dict()
+        res["spec"]["template"]["spec"]["domain"]["devices"].setdefault("rng", {})
+        return res
+
+
+@pytest.fixture(scope="module", autouse=True)
+def rng_namespace():
+    with Namespace(name="rng-test") as ns:
+        ns.wait_for_status(status=Namespace.Status.ACTIVE)
+        yield ns
+
+
+@pytest.fixture()
+def rng_vm(default_client, rng_namespace):
+    name = "vmi-with-rng"
+    with FedoraVirtualMachineWithRNG(name=name, namespace=rng_namespace.name) as vm:
+        vm.start(wait=True)
+        assert test_utils.wait_for_vm_interfaces(vm.vmi)
+        yield vm
+
+
+@pytest.mark.polarion("CNV-791")
+def test_vm_with_rng(rng_vm):
+    """
+    Test VM with RNG
+     - check random device should be present
+     - create random data with each device
+    """
+    with console.Console(vm=rng_vm) as vm_console:
+        vm_console.sendline("cat /sys/devices/virtual/misc/hw_random/rng_current")
+        vm_console.expect("virtio_rng.0", timeout=20)
+        for device in ["random", "hwrng"]:
+            vm_console.sendline(
+                f"sudo dd count=10 bs=1024 if=/dev/{device} of=/tmp/{device}.txt"
+            )
+            vm_console.sendline(f"ls /tmp/{device}.txt | wc -l")
+            vm_console.expect("1", timeout=20)
