e721a185c48777cc21de406d11223a515df638c7
Or Shoval <oshoval@redhat.com>
2019-08-18 17:20:41 +0300

    Fix Folder of kubevirtci
    Change-Id: I231b06785f81007c92ddcbf73a323db9091dc548
    Reviewed-on: https://code.engineering.redhat.com/gerrit/178794
    Continuous-Integration: rhv-devops-jenkins <rhv-devops-jenkins@redhat.com>
    Tested-by: Or Shoval <oshoval@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- local-cluster/hack/install-kubevirtci.sh
+++ local-cluster/hack/install-kubevirtci.sh
@@ -3,7 +3,7 @@
 commit="7ff84096b06a6a7d59ec83bfdf81be6f74a5c542"
 
 script_dir=$(dirname "$(readlink -f "$0")")
-kubevirtci_dir=kubevirtci
+kubevirtci_dir=local-cluster/kubevirtci
 
 rm -rf $kubevirtci_dir
 git clone https://github.com/kubevirt/kubevirtci $kubevirtci_dir
