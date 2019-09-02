a7fdc944c2b8ab3fb4034a06d644ad88148334dd
Meni Yakove <myakove@redhat.com>
2019-09-02 13:09:12 +0300

    Get the right must-gather image for u/s and d/s
    Change-Id: I35a818d7a72d8ab1126147922fd7eb60270a4a57

--- tests/must_gather/conftest.py
+++ tests/must_gather/conftest.py
@@ -5,24 +5,30 @@ must gather test
 """
 
 import logging
-import pytest
 import shutil
 from subprocess import check_output
+
+import pytest
+from pytest_testconfig import config as py_config
 
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='module') # noqa BLK100
+@pytest.fixture(scope="module")
 def cnv_must_gather(tmpdir_factory):
     """
     Run cnv-must-gather for data collection.
     """
-    LOGGER.info('Running cnv_must_gather')
-    image = 'quay.io/kubevirt/must-gather'
-    path = tmpdir_factory.mktemp('must_gather')
+    image = py_config["must_gather"]["url"]
+    version = py_config["must_gather"].get("version")
+    if version:
+        image = f"{image}:{version}"
+
+    path = tmpdir_factory.mktemp("must_gather")
     try:
-        mg_cmd = f'oc adm must-gather --image={image} --dest-dir={path}'
-        check_output(mg_cmd, shell=True)
+        must_gather_cmd = f"oc adm must-gather --image={image} --dest-dir={path}"
+        LOGGER.info(f"Running: {must_gather_cmd}")
+        check_output(must_gather_cmd, shell=True)
         yield path
     finally:
         shutil.rmtree(path)
