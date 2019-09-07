f11a8a168084c2269930189c15ccdb784310deef
Meni Yakove <myakove@redhat.com>
2019-09-07 10:39:23 +0300

    Move network fixtures to top level conftest.
    For upgrade tests usages.
    
    Change-Id: I154b962c90d641a64f0641099d39d9fa562ae600

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ Pytest conftest file for CNV tests
 """
 import base64
 import os
+import os.path
 from subprocess import Popen, check_output, CalledProcessError, PIPE
 
 import bcrypt
@@ -13,11 +14,14 @@ import pytest
 from openshift.dynamic import DynamicClient
 from pytest_testconfig import config as py_config
 
+from resources.daemonset import DaemonSet
 from resources.node import Node
 from resources.oauth import OAuth
+from resources.pod import Pod
 from resources.secret import Secret
 from resources.utils import TimeoutSampler
 from tests import utils as test_utils
+from utilities import utils
 
 UNPRIVILEGED_USER = "unprivileged-user"
 UNPRIVILEGED_PASSWORD = "unprivileged-password"
@@ -221,3 +225,83 @@ def images_external_http_server():
 @pytest.fixture()
 def images_https_server():
     return test_utils.get_images_https_server()
+
+
+@pytest.fixture(scope="session")
+def net_utility_daemonset(default_client):
+    """
+    Deploy network utility daemonset into the kube-system namespace.
+
+    This daemonset deploys a pod on every node with hostNetwork and the main usage is to run commands on the hosts.
+    For example to create linux bridge and other components related to the host configuration.
+    """
+    with NetUtilityDaemonSet(name="net-utility", namespace="kube-system") as ds:
+        ds.wait_until_deployed()
+        yield ds
+
+
+@pytest.fixture(scope="session")
+def network_utility_pods(net_utility_daemonset, default_client):
+    """
+    Get network utility pods.
+    When the tests start we deploy a pod on every host in the cluster using a daemonset.
+    These pods have a label of cnv-test=net-utility and they are privileged pods with hostnetwork=true
+    """
+    return list(Pod.get(default_client, label_selector="cnv-test=net-utility"))
+
+
+@pytest.fixture(scope="session")
+def nodes_active_nics(network_utility_pods):
+    """
+    Get nodes active NICs. (Only NICs that are in UP state)
+    First NIC is management NIC
+    """
+    nodes_nics = {}
+    for pod in network_utility_pods:
+        pod_container = pod.containers[0].name
+        nodes_nics[pod.node.name] = []
+        nics = pod.execute(
+            command=[
+                "bash",
+                "-c",
+                "ls -l /sys/class/net/ | grep -v virtual | grep net | rev | cut -d '/' -f 1 | rev",
+            ],
+            container=pod_container,
+        )
+        nics = nics.splitlines()
+        default_gw = pod.execute(
+            command=["ip", "route", "show", "default"], container=pod_container
+        )
+        for nic in nics:
+            nic_state = pod.execute(
+                command=["cat", f"/sys/class/net/{nic}/operstate"],
+                container=pod_container,
+            )
+            if nic_state.strip() == "up":
+                if nic in [i for i in default_gw.splitlines() if "default" in i][0]:
+                    nodes_nics[pod.node.name].insert(0, nic)
+                    continue
+
+                nodes_nics[pod.node.name].append(nic)
+    return nodes_nics
+
+
+@pytest.fixture(scope="session")
+def multi_nics_nodes(nodes_active_nics):
+    """
+    Check if nodes has more then 1 active NIC
+    """
+    return min(len(nics) for nics in nodes_active_nics.values()) > 2
+
+
+class NetUtilityDaemonSet(DaemonSet):
+    def _to_dict(self):
+        res = super()._to_dict()
+        res.update(
+            utils.generate_yaml_from_template(
+                file_=os.path.join(
+                    os.path.dirname(__file__), "net-utility-daemonset.yaml"
+                )
+            )
+        )
+        return res
--- tests/network/conftest.py
+++ tests/network/conftest.py
@@ -4,26 +4,7 @@
 Pytest conftest file for CNV network tests
 """
 
-import os.path
-
 import pytest
-
-from resources.daemonset import DaemonSet
-from resources.pod import Pod
-from utilities import utils
-
-
-class NetUtilityDaemonSet(DaemonSet):
-    def _to_dict(self):
-        res = super()._to_dict()
-        res.update(
-            utils.generate_yaml_from_template(
-                file_=os.path.join(
-                    os.path.dirname(__file__), "net-utility-daemonset.yaml"
-                )
-            )
-        )
-        return res
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -38,73 +19,6 @@ def network_init(
     Create network test namespaces
     """
     pass
-
-
-@pytest.fixture(scope="session")
-def net_utility_daemonset(default_client):
-    """
-    Deploy network utility daemonset into the kube-system namespace.
-
-    This daemonset deploys a pod on every node with hostNetwork and the main usage is to run commands on the hosts.
-    For example to create linux bridge and other components related to the host configuration.
-    """
-    with NetUtilityDaemonSet(name="net-utility", namespace="kube-system") as ds:
-        ds.wait_until_deployed()
-        yield ds
-
-
-@pytest.fixture(scope="session")
-def network_utility_pods(default_client):
-    """
-    Get network utility pods.
-    When the tests start we deploy a pod on every host in the cluster using a daemonset.
-    These pods have a label of cnv-test=net-utility and they are privileged pods with hostnetwork=true
-    """
-    return list(Pod.get(default_client, label_selector="cnv-test=net-utility"))
-
-
-@pytest.fixture(scope="session")
-def nodes_active_nics(network_utility_pods):
-    """
-    Get nodes active NICs. (Only NICs that are in UP state)
-    First NIC is management NIC
-    """
-    nodes_nics = {}
-    for pod in network_utility_pods:
-        pod_container = pod.containers[0].name
-        nodes_nics[pod.node.name] = []
-        nics = pod.execute(
-            command=[
-                "bash",
-                "-c",
-                "ls -l /sys/class/net/ | grep -v virtual | grep net | rev | cut -d '/' -f 1 | rev",
-            ],
-            container=pod_container,
-        )
-        nics = nics.splitlines()
-        default_gw = pod.execute(
-            command=["ip", "route", "show", "default"], container=pod_container
-        )
-        for nic in nics:
-            nic_state = pod.execute(
-                command=["cat", f"/sys/class/net/{nic}/operstate"],
-                container=pod_container,
-            )
-            if nic_state.strip() == "up":
-                if nic in [i for i in default_gw.splitlines() if "default" in i][0]:
-                    nodes_nics[pod.node.name].insert(0, nic)
-                    continue
-
-                nodes_nics[pod.node.name].append(nic)
-    return nodes_nics
-
-
-@pytest.fixture(scope="session")
-def multi_nics_nodes(nodes_active_nics):
-    """
-    Check if nodes has more then 1 active NIC
-    """
-    return min(len(nics) for nics in nodes_active_nics.values()) > 2
 
 
 @pytest.fixture(scope="session")
