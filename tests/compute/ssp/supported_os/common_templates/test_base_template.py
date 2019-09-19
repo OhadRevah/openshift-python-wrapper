046a2caa28c0ad8b2744d46337237cabaa08b2d2
Meni Yakove <myakove@redhat.com>
2019-09-19 13:43:21 +0300

    Move CNV_TEMPLATES_NAME to test file and remove config.py.
    Change-Id: Ic37b05b320628f93c5983e2c26e2e08987543b16

--- tests/compute/ssp/supported_os/common_templates/test_base_template.py
+++ tests/compute/ssp/supported_os/common_templates/test_base_template.py
@@ -6,7 +6,86 @@ Base templates test
 
 import pytest
 from resources.template import Template
-from tests.compute.ssp import config as virt_config
+
+
+CNV_TEMPLATES_NAME = [
+    "centos6-server-large",
+    "centos6-server-medium",
+    "centos6-server-small",
+    "centos6-server-tiny",
+    "centos7-desktop-large",
+    "centos7-desktop-medium",
+    "centos7-desktop-small",
+    "centos7-desktop-tiny",
+    "centos7-server-large",
+    "centos7-server-medium",
+    "centos7-server-small",
+    "centos7-server-tiny",
+    "centos8-desktop-large",
+    "centos8-desktop-medium",
+    "centos8-desktop-small",
+    "centos8-desktop-tiny",
+    "centos8-server-large",
+    "centos8-server-medium",
+    "centos8-server-small",
+    "centos8-server-tiny",
+    "fedora-desktop-large",
+    "fedora-desktop-medium",
+    "fedora-desktop-small",
+    "fedora-desktop-tiny",
+    "fedora-highperformance-large",
+    "fedora-highperformance-medium",
+    "fedora-highperformance-small",
+    "fedora-highperformance-tiny",
+    "fedora-server-large",
+    "fedora-server-medium",
+    "fedora-server-small",
+    "fedora-server-tiny",
+    "opensuse-server-large",
+    "opensuse-server-medium",
+    "opensuse-server-small",
+    "opensuse-server-tiny",
+    "rhel6-desktop-large",
+    "rhel6-desktop-medium",
+    "rhel6-desktop-small",
+    "rhel6-desktop-tiny",
+    "rhel6-server-large",
+    "rhel6-server-medium",
+    "rhel6-server-small",
+    "rhel6-server-tiny",
+    "rhel7-desktop-large",
+    "rhel7-desktop-medium",
+    "rhel7-desktop-small",
+    "rhel7-desktop-tiny",
+    "rhel7-highperformance-large",
+    "rhel7-highperformance-medium",
+    "rhel7-highperformance-small",
+    "rhel7-highperformance-tiny",
+    "rhel7-server-large",
+    "rhel7-server-medium",
+    "rhel7-server-small",
+    "rhel7-server-tiny",
+    "rhel8-desktop-large",
+    "rhel8-desktop-medium",
+    "rhel8-desktop-small",
+    "rhel8-desktop-tiny",
+    "rhel8-highperformance-large",
+    "rhel8-highperformance-medium",
+    "rhel8-highperformance-small",
+    "rhel8-highperformance-tiny",
+    "rhel8-server-large",
+    "rhel8-server-medium",
+    "rhel8-server-small",
+    "rhel8-server-tiny",
+    "ubuntu-desktop-large",
+    "ubuntu-desktop-medium",
+    "ubuntu-desktop-small",
+    "ubuntu-desktop-tiny",
+    "win2k12r2-desktop-large",
+    "win2k12r2-desktop-medium",
+    "win2k12r2-server-large",
+    "win2k12r2-server-medium",
+]
 
 
 @pytest.fixture()
@@ -29,7 +108,7 @@ def test_base_templates_annotations(get_base_templates):
     """
     Check all CNV templates exists, by label: template.kubevirt.io/type=base
     """
-    assert len(get_base_templates) == len(virt_config.CNV_TEMPLATES_NAME), (
+    assert len(get_base_templates) == len(CNV_TEMPLATES_NAME), (
         f"Not all base CNV templates exists\n exist templates:\n "
-        f"{get_base_templates} expected:\n {virt_config.CNV_TEMPLATES_NAME}",
+        f"{get_base_templates} expected:\n {CNV_TEMPLATES_NAME}",
     )
