3b22cabcfd62568f55eef021db5d886f2813aab1
Denys Shchedrivyi <dshchedr@redhat.com>
2019-10-21 22:17:59 -0400

    1) Add test - node maintenance with RHEL Template VM
    2) Add new python module: pytest-lazy-fixture, that allows to use fixtures as parameters in tests
    
    Change-Id: Ic354671b4e6b700c74f0e283ec403f96f2c5dc8f

--- tests/compute/virt/migration/test_node_maintenance.py
+++ tests/compute/virt/migration/test_node_maintenance.py
@@ -8,6 +8,7 @@ from contextlib import contextmanager
 from subprocess import run
 
 import pytest
+from pytest_lazyfixture import lazy_fixture
 from resources.node_maintenance import NodeMaintenance
 from resources.template import Template
 from resources.utils import TimeoutSampler
@@ -37,13 +38,13 @@ def node_maintenance_ns(unprivileged_client):
 
 
 @contextmanager
-def running_sleep_in_fedora(vm):
+def running_sleep_in_linux(vm_cli):
     process = "sleep 1000"
-    with console.Fedora(vm) as vm_console:
+    with vm_cli as vm_console:
         vm_console.sendline(f"nohup {process} &")
         vm_console.expect("nohup: ")
     yield
-    with console.Fedora(vm) as vm_console:
+    with vm_cli as vm_console:
         vm_console.sendline(f'ps aux | grep "{process}" | grep -v grep | wc -l')
         vm_console.expect("1")
 
@@ -60,6 +61,20 @@ def drain_node_console(node):
         run(f"oc adm uncordon {node.name}", shell=True)
 
 
+@contextmanager
+def create_dv(dv_name, url, node_maintenance_ns):
+    with DataVolumeTestResource(
+        name=dv_name,
+        namespace=node_maintenance_ns.name,
+        url=url,
+        size="30Gi",
+        access_modes=DataVolumeTestResource.AccessMode.RWX,
+        volume_mode=DataVolumeTestResource.VolumeMode.BLOCK,
+    ) as dv:
+        dv.wait(timeout=1200)
+        yield dv
+
+
 @pytest.fixture(scope="module")
 def skip_when_other_vmi_present(default_client):
     if list(VirtualMachineInstance.get(default_client)):
@@ -67,7 +82,7 @@ def skip_when_other_vmi_present(default_client):
 
 
 @pytest.fixture()
-def vm0(node_maintenance_ns, unprivileged_client):
+def vm_container_disk_fedora(node_maintenance_ns, unprivileged_client):
     name = f"vm-nodemaintenance-{random.randrange(99999)}"
     with VirtualMachineForTests(
         name=name,
@@ -80,6 +95,30 @@ def vm0(node_maintenance_ns, unprivileged_client):
         vm.start(wait=True)
         vm.vmi.wait_until_running()
         yield vm
+
+
+@pytest.fixture()
+def vm_template_dv_rhel8(
+    node_maintenance_ns, unprivileged_client, images_external_http_server
+):
+    vm_dv_name = "rhel8-template-node-maintenance"
+    url = f"{images_external_http_server}rhel-images/rhel-81/rhel-81.qcow2"
+    template_labels = [
+        f"{Template.Labels.OS}/rhel8.0",
+        f"{Template.Labels.WORKLOAD}/server",
+        f"{Template.Labels.FLAVOR}/tiny",
+    ]
+    with create_dv(vm_dv_name, url, node_maintenance_ns) as dv:
+        with VirtualMachineForTestsFromTemplate(
+            name="dv-rhel8-node-maintenance",
+            namespace=node_maintenance_ns.name,
+            client=unprivileged_client,
+            labels=template_labels,
+            template_dv=dv.name,
+        ) as vm:
+            vm.start(wait=True)
+            vm.vmi.wait_until_running()
+            yield vm
 
 
 @pytest.fixture()
@@ -109,38 +148,27 @@ def winrmcli_pod(vm_win10, nodes):
 
 
 @pytest.fixture()
-def vm_win10(dv_win10, node_maintenance_ns, unprivileged_client):
-    vm_name = "windows-node-maintenance"
-    with VirtualMachineForTestsFromTemplate(
-        name=vm_name,
-        namespace=node_maintenance_ns.name,
-        client=unprivileged_client,
-        labels=dv_win10.template_labels,
-        template_dv=dv_win10.name,
-    ) as vm:
-        vm.start(wait=True)
-        vm.vmi.wait_until_running()
-        yield vm
-
-
-@pytest.fixture()
-def dv_win10(images_external_http_server, node_maintenance_ns):
+def vm_win10(node_maintenance_ns, unprivileged_client, images_external_http_server):
+    vm_dv_name = "windows-template-node-maintenance"
+    url = (
+        f"{images_external_http_server}windows-images/window_qcow2_images/win_10.qcow2"
+    )
     template_labels = [
         f"{Template.Labels.OS}/win10",
         f"{Template.Labels.WORKLOAD}/desktop",
         f"{Template.Labels.FLAVOR}/large",
     ]
-    with DataVolumeTestResource(
-        name="dv-windows-node-maintenance",
-        namespace=node_maintenance_ns.name,
-        url=f"{images_external_http_server}windows-images/window_qcow2_images/win_10.qcow2",
-        size="30Gi",
-        access_modes="ReadWriteMany",
-        volume_mode="Block",
-        template_labels=template_labels,
-    ) as dv:
-        dv.wait(timeout=1200)
-        yield dv
+    with create_dv(vm_dv_name, url, node_maintenance_ns) as dv:
+        with VirtualMachineForTestsFromTemplate(
+            name=vm_dv_name,
+            namespace=node_maintenance_ns.name,
+            client=unprivileged_client,
+            labels=template_labels,
+            template_dv=dv.name,
+        ) as vm:
+            vm.start(wait=True)
+            vm.vmi.wait_until_running()
+            yield vm
 
 
 def check_windows_boot_time(vm, winrmcli_pod, timeout=120):
@@ -174,39 +202,60 @@ def check_draining_process(default_client, source_pod, vm):
 
 
 @pytest.mark.polarion("CNV-2286")
-def test_node_maintenance_fedora(
-    skip_when_other_vmi_present, skip_when_one_node, vm0, default_client
+def test_node_maintenance_job(
+    skip_when_other_vmi_present,
+    skip_when_one_node,
+    vm_container_disk_fedora,
+    default_client,
 ):
-    source_pod = vm0.vmi.virt_launcher_pod
+    source_pod = vm_container_disk_fedora.vmi.virt_launcher_pod
     source_node = source_pod.node
 
-    with running_sleep_in_fedora(vm0):
+    with running_sleep_in_linux(console.Fedora(vm_container_disk_fedora)):
         with NodeMaintenance(name="node-maintenance-job", node=source_node) as nm:
             nm.wait_for_status(status=nm.Status.RUNNING)
             check_draining_process(
-                default_client=default_client, source_pod=source_pod, vm=vm0
+                default_client=default_client,
+                source_pod=source_pod,
+                vm=vm_container_disk_fedora,
             )
             nm.wait_for_status(status=nm.Status.SUCCEEDED)
         virt_utils.wait_for_node_unschedulable_status(node=source_node, status=False)
 
 
-@pytest.mark.polarion("CNV-3006")
-def test_node_drain_console(
-    skip_when_other_vmi_present, skip_when_one_node, vm0, default_client
+@pytest.mark.parametrize(
+    "vm, os",
+    [
+        pytest.param(
+            lazy_fixture("vm_container_disk_fedora"),
+            "Fedora",
+            marks=(pytest.mark.polarion("CNV-3006")),
+        ),
+        pytest.param(
+            lazy_fixture("vm_template_dv_rhel8"),
+            "RHEL",
+            marks=(pytest.mark.polarion("CNV-2292")),
+        ),
+    ],
+)
+def test_node_drain_using_console(
+    skip_when_other_vmi_present, skip_when_one_node, default_client, vm, os
 ):
-    source_pod = vm0.vmi.virt_launcher_pod
+    source_pod = vm.vmi.virt_launcher_pod
     source_node = source_pod.node
 
-    with running_sleep_in_fedora(vm0):
+    vm_cli = console.Fedora(vm) if os == "Fedora" else console.RHEL(vm)
+
+    with running_sleep_in_linux(vm_cli):
         with drain_node_console(node=source_node):
             check_draining_process(
-                default_client=default_client, source_pod=source_pod, vm=vm0
+                default_client=default_client, source_pod=source_pod, vm=vm
             )
         virt_utils.wait_for_node_unschedulable_status(node=source_node, status=False)
 
 
 @pytest.mark.polarion("CNV-2048")
-def test_node_maintenance_windows(
+def test_node_drain_template_windows(
     skip_when_other_vmi_present,
     skip_when_one_node,
     vm_win10,
