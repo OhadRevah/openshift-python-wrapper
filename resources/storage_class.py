a13cc7205ca529364842a4b3cbe117ed8205689c
Qixuan Wang <qwang@redhat.com>
2019-12-05 15:28:22 +0800

    Add VolumeBindingMode and Provisioner to StorageClass
    Change-Id: I1812c1429368d9f20e3b827dc3687b13e39a0fc9
    Signed-off-by: Qixuan Wang <qwang@redhat.com>

--- resources/storage_class.py
+++ resources/storage_class.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from .resource import Resource
 
 
@@ -9,6 +11,28 @@ class StorageClass(Resource):
     api_group = "storage.k8s.io"
 
     class Types:
+        """
+        These are names of StorageClass instances when you run `oc get sc`
+        """
+
         LOCAL = "local-sc"
         HOSTPATH = "kubevirt-hostpath-provisioner"
         ROOK = "rook-ceph-block"
+
+    class Provisioner:
+        HOSTPATH = "kubevirt.io/hostpath-provisioner"
+        LOCAL_SC = "kubernetes.io/no-provisioner"
+        CEPH_RBD = "rook-ceph.rbd.csi.ceph.com"
+
+    class VolumeBindingMode:
+        """
+        VolumeBindingMode indicates how PersistentVolumeClaims should be provisioned and bound.
+        When unset, Immediate is used.
+        When "Immediate", if you want to use the "node aware" hostpath-provisioner,
+        ProvisionOnNode annotations should be introduced to PVC.
+        Or in order to be able to use the hpp without specifying the node on the PVC,
+        since CNV-2.2, hpp supports for "WaitForFirstConsumer".
+        """
+
+        Immediate = "Immediate"
+        WaitForFirstConsumer = "WaitForFirstConsumer"
