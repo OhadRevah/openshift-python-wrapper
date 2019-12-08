d951be04ef46782ff4531c65b90bba0605f32bc1
Meni Yakove <myakove@redhat.com>
2019-12-08 13:29:11 +0200

    Jumbo frame tests for OVS.
    Change-Id: I903f260f6169f89fda1e4d036c2492c7c1233383

--- tests/network/jumbo_frame/test_jumbo_frame.py
+++ tests/network/jumbo_frame/test_jumbo_frame.py
@@ -5,11 +5,11 @@ VM to VM connectivity with  custom MTU (jumbo frame)
 import pytest
 from tests.network.utils import (
     assert_ping_successful,
+    bridge_device,
+    bridge_nad,
     get_vmi_ip_v4_by_name,
-    linux_bridge_nad,
     nmcli_add_con_cmds,
 )
-from utilities.network import LinuxBridgeNodeNetworkConfigurationPolicy
 from utilities.virt import (
     FEDORA_CLOUD_INIT_PASSWORD,
     VirtualMachineForTests,
@@ -19,35 +19,36 @@ from utilities.virt import (
 
 
 @pytest.fixture(scope="module")
-def br1test_nad(namespace):
-    with linux_bridge_nad(
+def nad(bridge_device_matrix, namespace, network_utility_pods, nodes_active_nics):
+    with bridge_nad(
         namespace=namespace,
-        name="br1test-nad",
-        bridge="br1test",
+        nad_type=bridge_device_matrix,
+        nad_name="br1test-nad",
+        bridge_name="br1test",
         tuning=True,
         mtu=9000,
     ) as nad:
         yield nad
 
 
-@pytest.fixture(scope="module", autouse=True)
-def bridge_on_all_nodes(network_utility_pods, nodes_active_nics, br1test_nad):
+@pytest.fixture(scope="module")
+def bridge(bridge_device_matrix, network_utility_pods, nodes_active_nics, nad):
     ports = [nodes_active_nics[network_utility_pods[0].node.name][1]]
-
-    with LinuxBridgeNodeNetworkConfigurationPolicy(
-        name="jumbo-frame",
-        bridge_name=br1test_nad.bridge_name,
-        worker_pods=network_utility_pods,
+    with bridge_device(
+        bridge_type=bridge_device_matrix,
+        nncp_name=f"{nad.bridge_name}-nncp",
+        bridge_name=nad.bridge_name,
+        network_utility_pods=network_utility_pods,
         ports=ports,
-        mtu=br1test_nad.mtu,
+        mtu=nad.mtu,
     ) as br:
         yield br
 
 
 @pytest.fixture(scope="module")
-def bridge_attached_vma(nodes, namespace, unprivileged_client, br1test_nad):
+def bridge_attached_vma(nodes, namespace, unprivileged_client, nad):
     name = "vma"
-    networks = {br1test_nad.name: br1test_nad.name}
+    networks = {nad.name: nad.name}
     cloud_init_data = FEDORA_CLOUD_INIT_PASSWORD
     cloud_init_data["bootcmd"] = nmcli_add_con_cmds("eth1", "192.168.0.1")
 
@@ -66,9 +67,9 @@ def bridge_attached_vma(nodes, namespace, unprivileged_client, br1test_nad):
 
 
 @pytest.fixture(scope="module")
-def bridge_attached_vmb(nodes, namespace, unprivileged_client, br1test_nad):
+def bridge_attached_vmb(nodes, namespace, unprivileged_client, nad):
     name = "vmb"
-    networks = {br1test_nad.name: br1test_nad.name}
+    networks = {nad.name: nad.name}
     cloud_init_data = FEDORA_CLOUD_INIT_PASSWORD
     cloud_init_data["bootcmd"] = nmcli_add_con_cmds("eth1", "192.168.0.2")
 
@@ -107,7 +108,8 @@ def test_connectivity_over_linux_bridge_large_mtu(
     skip_if_no_multinic_nodes,
     skip_when_one_node,
     namespace,
-    br1test_nad,
+    bridge,
+    nad,
     bridge_attached_vma,
     bridge_attached_vmb,
     running_bridge_attached_vmia,
@@ -120,8 +122,6 @@ def test_connectivity_over_linux_bridge_large_mtu(
     ip_header = 20
     assert_ping_successful(
         src_vm=bridge_attached_vma,
-        dst_ip=get_vmi_ip_v4_by_name(
-            vmi=running_bridge_attached_vmib, name=br1test_nad.name
-        ),
-        packetsize=br1test_nad.mtu - ip_header - icmp_header,
+        dst_ip=get_vmi_ip_v4_by_name(vmi=running_bridge_attached_vmib, name=nad.name),
+        packetsize=nad.mtu - ip_header - icmp_header,
     )
