8b4b0fd5b28c66e7fe249f374dbb479b4dfd6c51
Maya Rashish <mrashish@redhat.com>
2019-12-24 13:46:16 +0200

    Add missing volume_mode argument in create_dv and PodWithPVC calls.
    Change-Id: I9a5b64aeca28c54d6c037bfda53e7057c203c710

--- tests/storage/test_hostpath.py
+++ tests/storage/test_hostpath.py
@@ -8,6 +8,7 @@ import logging
 
 import pytest
 import tests.storage.utils as storage_utils
+from pytest_testconfig import config as py_config
 from resources.datavolume import DataVolume
 from resources.storage_class import StorageClass
 from tests.storage.utils import CDI_IMAGES_DIR, CIRROS_IMAGES_DIR
@@ -41,7 +42,10 @@ def skip_when_hpp_no_waitforfirstconsumer(skip_test_if_no_hpp_sc, hpp_storage_cl
 def verify_image_location_via_dv_pod_with_pvc(dv, nodes):
     dv.wait()
     with storage_utils.PodWithPVC(
-        namespace=dv.namespace, name=f"{dv.name}-pod", pvc_name=dv.pvc.name
+        namespace=dv.namespace,
+        name=f"{dv.name}-pod",
+        pvc_name=dv.pvc.name,
+        volume_mode=py_config["default_volume_mode"],
     ) as pod:
         pod.wait_for_status(status="Running")
         LOGGER.debug("Check pod location...")
@@ -77,6 +81,7 @@ def test_hostpath_pod_reference_pvc(storage_ns, nodes, skip_test_if_no_hpp_sc):
         content_type=DataVolume.ContentType.KUBEVIRT,
         size="20Gi",
         storage_class=StorageClass.Types.HOSTPATH,
+        volume_mode=py_config["default_volume_mode"],
         hostpath_node=nodes[0].name,
     ) as dv:
         verify_image_location_via_dv_pod_with_pvc(dv, nodes)
@@ -99,6 +104,7 @@ def test_hpp_not_specify_node_immediate(
         content_type=DataVolume.ContentType.KUBEVIRT,
         size="35Gi",
         storage_class=StorageClass.Types.HOSTPATH,
+        volume_mode=py_config["default_volume_mode"],
     ) as dv:
         dv.wait_for_status(
             status=dv.Status.PENDING, timeout=120, stop_status=dv.Status.SUCCEEDED
@@ -122,6 +128,7 @@ def test_hpp_specify_node_immediate(
         content_type=DataVolume.ContentType.KUBEVIRT,
         size="35Gi",
         storage_class=StorageClass.Types.HOSTPATH,
+        volume_mode=py_config["default_volume_mode"],
         hostpath_node=nodes[0].name,
     ) as dv:
         dv.wait(timeout=600)
@@ -161,6 +168,7 @@ def test_hostpath_http_import_dv(
         url=f"{get_images_external_http_server()}{CDI_IMAGES_DIR}/{CIRROS_IMAGES_DIR}/{image_name}",
         size="500Mi",
         storage_class=StorageClass.Types.HOSTPATH,
+        volume_mode=py_config["default_volume_mode"],
         hostpath_node=nodes[0].name,
     ) as dv:
         verify_image_location_via_dv_virt_launcher_pod(dv, nodes)
