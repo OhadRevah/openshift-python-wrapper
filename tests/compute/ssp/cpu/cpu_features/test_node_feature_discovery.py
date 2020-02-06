66420b43af56390cd31ed51cc6f258643864dfe1
ipinto <ipinto@redhat.com>
2020-02-06 14:52:50 +0200

    [SSP] kvm-info-nfd
    Test Hyper-V flags exist on nodes using kvm-info-nfd-plugin
    https://github.com/kubevirt/kvm-info-nfd-plugin
    
    Change-Id: Icd3947e3f16e4316aa3434b5ac266730519c305c

--- tests/compute/ssp/cpu/cpu_features/test_node_feature_discovery.py
+++ tests/compute/ssp/cpu/cpu_features/test_node_feature_discovery.py
@@ -12,29 +12,34 @@ from utilities.virt import VirtualMachineForTests, fedora_vm_body
 
 
 @pytest.fixture(scope="module")
-def nodes_cpu_labels_dict(nodes):
+def nodes_labels_dict(nodes):
     """
-    Collects all labels from nodes and creates dict of cpu-models/features per node.
+    Collects all labels from nodes and creates dict of cpu-models/features/kvm-info per node.
     Return dict:
-    {'<node_name>': {'cpu_models': [<cpu_models>], 'cpu_features': [<cpu_features>]}}
+    {'<node_name>': {'cpu_models': [<cpu_models>], 'cpu_features': [<cpu_features>], 'kvm-info': [<kvm-info>]}}
     """
-    cpu_dict = {}
+    node_labels_dict = {}
 
     for node in nodes:
-        cpu_dict[node.name] = {}
+        node_labels_dict[node.name] = {}
         labels_dict = dict(node.instance.metadata.labels)
-        cpu_dict[node.name]["cpu_models"] = [
+        node_labels_dict[node.name]["cpu_models"] = [
             label.split("cpu-model-")[1]
             for label in labels_dict
             if "feature.node.kubernetes.io/cpu-model-" in label
         ]
-        cpu_dict[node.name]["cpu_features"] = [
+        node_labels_dict[node.name]["cpu_features"] = [
             label.split("cpu-feature-")[1]
             for label in labels_dict
             if "feature.node.kubernetes.io/cpu-feature-" in label
         ]
+        node_labels_dict[node.name]["kvm-info"] = [
+            label.split("kvm-info-cap-hyperv-")[1]
+            for label in labels_dict
+            if "feature.node.kubernetes.io/kvm-info-cap-hyperv-" in label
+        ]
 
-    return cpu_dict
+    return node_labels_dict
 
 
 @pytest.fixture(scope="module")
@@ -88,39 +93,65 @@ def cpu_test_vm(namespace):
         yield vm
 
 
-def cpu_label_checker(node_cpu_dict, cpu_list, dict_key):
+def node_label_checker(node_label_dict, label_list, dict_key):
     """
-    Check node labes for cpu models/features.
+    Check node labels for cpu models/features/kvm-info.
     Return dict:
-    {'<node_name>': [<cpu_models/features>]}
+    {'<node_name>': [<cpu_models/features/kvm-info>]}
     """
     return {
-        node: [value for value in cpu_list if value in node_cpu_dict[node][dict_key]]
-        for node in node_cpu_dict
+        node: [
+            value for value in label_list if value in node_label_dict[node][dict_key]
+        ]
+        for node in node_label_dict
     }
 
 
 @pytest.mark.polarion("CNV-2797")
-def test_obsolete_cpus_in_node_labels(nodes_cpu_labels_dict, obsolete_cpus_list):
+def test_obsolete_cpus_in_node_labels(nodes_labels_dict, obsolete_cpus_list):
     """
     Test obsolete CPUs. Obsolete CPUs don't appear in node labels.
     """
-    test_dict = cpu_label_checker(
-        node_cpu_dict=nodes_cpu_labels_dict,
-        cpu_list=obsolete_cpus_list,
+    test_dict = node_label_checker(
+        node_label_dict=nodes_labels_dict,
+        label_list=obsolete_cpus_list,
         dict_key="cpu_models",
     )
     assert not any(test_dict.values()), f"Obsolete CPU found in labels\n{test_dict}"
 
 
 @pytest.mark.polarion("CNV-2798")
-def test_min_cpus_in_node_labels(nodes_cpu_labels_dict, libvirt_min_cpu_features_list):
+def test_min_cpus_in_node_labels(nodes_labels_dict, libvirt_min_cpu_features_list):
     """
     Test min CPU. Min CPU features don't appear in node labels.
     """
-    test_dict = cpu_label_checker(
-        node_cpu_dict=nodes_cpu_labels_dict,
-        cpu_list=libvirt_min_cpu_features_list,
+    test_dict = node_label_checker(
+        node_label_dict=nodes_labels_dict,
+        label_list=libvirt_min_cpu_features_list,
         dict_key="cpu_features",
     )
     assert not any(test_dict.values()), f"Min CPU feature found in labels\n{test_dict}"
+
+
+@pytest.mark.polarion("CNV-3607")
+def test_kvm_info_nfd(nodes_labels_dict):
+    kvm_info_nfd_labels = [
+        "vpindex",
+        "runtime",
+        "time",
+        "synic",
+        "synic2",
+        "tlbflush",
+        "reset",
+        "frequencies",
+        "reenlightenment",
+        "base",
+        "ipi",
+        "synictimer",
+    ]
+    test_dict = node_label_checker(
+        node_label_dict=nodes_labels_dict,
+        label_list=kvm_info_nfd_labels,
+        dict_key="kvm-info",
+    )
+    assert any(test_dict.values()), f"KVM info not found in labels\n{test_dict}"
