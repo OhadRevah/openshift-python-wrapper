e885c28edc1c7ff5343e59fe6c5aef8b36bc8900
rnetser <rnetser@redhat.com>
2020-03-24 14:00:41 +0200

    Templates - add template namespace and update template name
    Template validator requires the template namespace.
    This is currently done manually (KNIP-1055" target="_blank" rel="nofollow">https://issues.redhat.com/browse/KNIP-1055).
    Template name needs to be updated to the full template name
    (https://bugzilla.redhat.com/show_bug.cgi?id=1816518)
    
    Change-Id: I0c3c62759979bdcf499db73e9fc8a1a57210508a

--- resources/template.py
+++ resources/template.py
@@ -33,6 +33,16 @@ class Template(NamespacedResource):
             except KeyError:
                 continue
         instance_dict["parameters"] = params
+        # TODO: remove after fix - https://issues.redhat.com/browse/KNIP-1055 (bug 1753554)
+        # For template validator to be used - template namespace needs to be updated
+        instance_dict["objects"][0]["metadata"]["labels"][
+            "vm.kubevirt.io/template.namespace"
+        ] = instance_dict["metadata"]["namespace"]
+        # TODO: remove after fix - https://bugzilla.redhat.com/show_bug.cgi?id=1816518 - template
+        # name needs to be updated
+        instance_dict["objects"][0]["metadata"]["labels"][
+            "vm.kubevirt.io/template"
+        ] = instance_dict["metadata"]["name"]
         r = json.dumps(instance_dict)
         body = json.loads(r)
         response = self.client.request(
