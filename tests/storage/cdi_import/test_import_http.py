kubevirt/containerized-data-importer

commit 5ed55cb22cf23a82bd55405e955f75ab7075855a

Remove code that potentially let DV get into failed state with RestartOnFailure policy. (#1099)
Signed-off-by: Alexander Wels <awels@redhat.com>

pkg/controller/import-controller.go
@@ -229,9 +229,8 @@ func (r *ImportReconciler) updatePvcFromPod(pvc *corev1.PersistentVolumeClaim, p
 	}
 
 	anno[AnnImportPod] = string(pod.Name)
-	if !scratchExitCode {
-		r.updatePvcPhaseFromPod(anno, pod)
-	}
+	// Even if scratch space is needed, the pod state will still remain running, until the new pod is started.
+	anno[AnnPodPhase] = string(pod.Status.Phase)
 
 	// Check if the POD is waiting for scratch space, if so create some.
 	if pod.Status.Phase == corev1.PodPending && r.requiresScratchSpace(pvc) {
@@ -271,19 +270,6 @@ func (r *ImportReconciler) updatePVC(pvc *corev1.PersistentVolumeClaim) error {
 		return err
 	}
 	return nil
-}
-
-func (r *ImportReconciler) updatePvcPhaseFromPod(anno map[string]string, pod *corev1.Pod) {
-	// TODO: If we are in progress, and the pod phase is failed, we don't want the PVC to go into failed unless we have
-	// passed some threshold (number of retries, time passed, something). Otherwise the PVC can go from failed to in progress/success when
-	// a failed pod comes back to in progress/success.
-	anno[AnnPodPhase] = string(pod.Status.Phase)
-	//this is for a case where the import container is failing and the restartPolicy is OnFailure. In such case
-	//the pod phase is "Running" although the container state is Waiting. When the container recovers, its state
-	//changes back to "Running". If the pod failed because it didn't have scratch space, don't mark the import failed.
-	if pod.Status.Phase != corev1.PodPending && pod.Status.ContainerStatuses != nil && pod.Status.ContainerStatuses[0].State.Waiting != nil {
-		anno[AnnPodPhase] = string(corev1.PodFailed)
-	}
 }
 
 func (r *ImportReconciler) createImporterPod(pvc *corev1.PersistentVolumeClaim) error {

pkg/controller/import-controller_test.go
@@ -3,10 +3,11 @@ package controller
 import (
 	"context"
 	"fmt"
-	"k8s.io/apimachinery/pkg/runtime"
-	cdifake "kubevirt.io/containerized-data-importer/pkg/client/clientset/versioned/fake"
 	"reflect"
 	"strconv"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	cdifake "kubevirt.io/containerized-data-importer/pkg/client/clientset/versioned/fake"
 
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/extensions/table"
@@ -279,7 +280,7 @@ var _ = Describe("Update PVC from POD", func() {
 		pvc := createPvcInStorageClass("testPvc1", "default", &testStorageClass, map[string]string{AnnEndpoint: testEndPoint, AnnPodPhase: string(corev1.PodRunning)}, nil)
 		pod := createImporterTestPod(pvc, "testPvc1", nil)
 		pod.Status = corev1.PodStatus{
-			Phase: corev1.PodFailed,
+			Phase: corev1.PodRunning,
 			ContainerStatuses: []corev1.ContainerStatus{
 				{
 					LastTerminationState: corev1.ContainerState{

tests/datavolume_test.go
@@ -129,19 +129,19 @@ var _ = Describe("[vendor:cnv-qe@redhat.com][level:component]DataVolume tests", 
 
 		},
 			table.Entry("[rfe_id:1115][crit:high][test_id:1357]succeed creating import dv with given valid url", "import-http", "", tinyCoreIsoURL, "dv-phase-test-1", "", controller.ImportSucceeded, cdiv1.Succeeded),
-			table.Entry("[rfe_id:1115][crit:high][posneg:negative][test_id:1358]fail creating import dv due to invalid DNS entry", "import-http", "", "http://i-made-this-up.kube-system/tinyCore.iso", "dv-phase-test-2", "Unable to connect to http data source", controller.ImportFailed, cdiv1.Failed),
-			table.Entry("[rfe_id:1115][crit:high][posneg:negative][test_id:1359]fail creating import dv due to file not found", "import-http", "", tinyCoreIsoURL+"not.real.file", "dv-phase-test-3", "Unable to connect to http data source", controller.ImportFailed, cdiv1.Failed),
+			table.Entry("[rfe_id:1115][crit:high][posneg:negative][test_id:1358]fail creating import dv due to invalid DNS entry", "import-http", "", "http://i-made-this-up.kube-system/tinyCore.iso", "dv-phase-test-2", "Unable to connect to http data source", controller.ImportFailed, cdiv1.ImportInProgress),
+			table.Entry("[rfe_id:1115][crit:high][posneg:negative][test_id:1359]fail creating import dv due to file not found", "import-http", "", tinyCoreIsoURL+"not.real.file", "dv-phase-test-3", "Unable to connect to http data source", controller.ImportFailed, cdiv1.ImportInProgress),
 			table.Entry("[rfe_id:1277][crit:high][test_id:1360]succeed creating clone dv", "clone", fillCommand, "", "dv-clone-test-1", "", controller.CloneSucceeded, cdiv1.Succeeded),
 			table.Entry("[rfe_id:1111][crit:high][test_id:1361]succeed creating blank image dv", "blank", "", "", "blank-image-dv", "", controller.ImportSucceeded, cdiv1.Succeeded),
 			table.Entry("[rfe_id:138][crit:high][test_id:1362]succeed creating upload dv", "upload", "", "", "upload-dv", "", controller.UploadReady, cdiv1.UploadReady),
 			table.Entry("[rfe_id:1115][crit:high][test_id:1478]succeed creating import dv with given valid registry url", "import-registry", "", tinyCoreIsoRegistryURL, "dv-phase-test-4", "", controller.ImportSucceeded, cdiv1.Succeeded),
 			table.Entry("[rfe_id:1115][crit:high][test_id:1379]succeed creating import dv with given valid url (https)", "import-https", "", httpsTinyCoreIsoURL, "dv-phase-test-1", "", controller.ImportSucceeded, cdiv1.Succeeded),
-			table.PEntry("[rfe_id:1120][crit:high][posneg:negative][test_id:2555]fail creating import dv: invalid qcow large size", "import-http", "", invalidQcowLargeSizeURL, "dv-invalid-qcow-large-size", "Unable to process data: Invalid format qcow for image", controller.ImportFailed, cdiv1.Failed),
-			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2554]fail creating import dv: invalid qcow large json", "import-http", "", invalidQcowLargeJSONURL, "dv-invalid-qcow-large-json", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.Failed),
-			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2253]fail creating import dv: invalid qcow large memory", "import-http", "", invalidQcowLargeMemoryURL, "dv-invalid-qcow-large-memory", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.Failed),
-			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2139]fail creating import dv: invalid qcow backing file", "import-http", "", invalidQcowBackingFileURL, "dv-invalid-qcow-backing-file", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.Failed),
+			table.PEntry("[rfe_id:1120][crit:high][posneg:negative][test_id:2555]fail creating import dv: invalid qcow large size", "import-http", "", invalidQcowLargeSizeURL, "dv-invalid-qcow-large-size", "Unable to process data: Invalid format qcow for image", controller.ImportFailed, cdiv1.ImportInProgress),
+			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2554]fail creating import dv: invalid qcow large json", "import-http", "", invalidQcowLargeJSONURL, "dv-invalid-qcow-large-json", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.ImportInProgress),
+			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2253]fail creating import dv: invalid qcow large memory", "import-http", "", invalidQcowLargeMemoryURL, "dv-invalid-qcow-large-memory", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.ImportInProgress),
+			table.Entry("[rfe_id:1120][crit:high][posneg:negative][test_id:2139]fail creating import dv: invalid qcow backing file", "import-http", "", invalidQcowBackingFileURL, "dv-invalid-qcow-backing-file", "Unable to process data: exit status 1", controller.ImportFailed, cdiv1.ImportInProgress),
 			table.Entry("[rfe_id:1947][crit:high][test_id:2145]succeed creating import dv with given tar archive url", "import-archive", "", tarArchiveURL, "tar-archive-dv", "", controller.ImportSucceeded, cdiv1.Succeeded),
-			table.Entry("[rfe_id:1947][crit:high][test_id:2220]fail creating import dv with non tar archive url", "import-archive", "", tinyCoreIsoURL, "non-tar-archive-dv", "Unable to process data: exit status 2", controller.ImportFailed, cdiv1.Failed),
+			table.Entry("[rfe_id:1947][crit:high][test_id:2220]fail creating import dv with non tar archive url", "import-archive", "", tinyCoreIsoURL, "non-tar-archive-dv", "Unable to process data: exit status 2", controller.ImportFailed, cdiv1.ImportInProgress),
 			table.Entry("succeed creating import dv with streaming image conversion", "import-http", "", cirrosURL, "dv-phase-test-1", "", controller.ImportSucceeded, cdiv1.Succeeded),
 		)
 	})

tests/import_test.go
@@ -312,3 +312,35 @@ var _ = Describe("[rfe_id:1947][crit:high][test_id:2145][vendor:cnv-qe@redhat.co
 		Expect(same).To(BeTrue())
 	})
 })
+
+var _ = Describe("PVC import phase matches pod phase", func() {
+	f := framework.NewFrameworkOrDie(namespacePrefix)
+
+	It("Should never go to failed even if import fails", func() {
+		c := f.K8sClient
+		ns := f.Namespace.Name
+		httpEp := fmt.Sprintf("http://%s:%d", utils.FileHostName+"."+f.CdiInstallNs, utils.HTTPNoAuthPort)
+		pvcAnn := map[string]string{
+			controller.AnnEndpoint: httpEp + "/invaliddoesntexist",
+		}
+
+		By(fmt.Sprintf("Creating PVC with endpoint annotation %q", httpEp+"/invaliddoesntexist"))
+		pvc, err := utils.CreatePVCFromDefinition(c, ns, utils.NewPVCDefinition("import-archive", "100Mi", pvcAnn, nil))
+		Expect(err).NotTo(HaveOccurred(), "Error creating PVC")
+
+		By("Verify the pod status is succeeded on the target PVC")
+		found, err := utils.WaitPVCPodStatusRunning(c, pvc)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(found).To(BeTrue())
+
+		By("Verifying the phase annotation on the PVC never gets to failed")
+		// Try for 20 seconds.
+		stopTime := time.Now().Add(time.Second * 20)
+		for time.Now().Before(stopTime) {
+			testPvc, err := c.CoreV1().PersistentVolumeClaims(ns).Get(pvc.Name, metav1.GetOptions{})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(testPvc.GetAnnotations()[controller.AnnPodPhase]).To(BeEquivalentTo(v1.PodRunning))
+			time.Sleep(time.Millisecond * 50)
+		}
+	})
+})

tests/transport_test.go
@@ -110,14 +110,14 @@ var _ = Describe("Transport Tests", func() {
 			Expect(f.VerifyNotSparse(f.Namespace, pvc)).To(BeTrue())
 		} else {
 			By("Verify PVC status annotation says failed")
-			found, err := utils.WaitPVCPodStatusFailed(f.K8sClient, pvc)
+			found, err := utils.WaitPVCPodStatusRunning(f.K8sClient, pvc)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(found).To(BeTrue())
-			importer, err := utils.FindPodByPrefix(c, ns, common.ImporterPodName, common.CDILabelSelector)
-			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Unable to get importer pod %q", ns+"/"+common.ImporterPodName))
-			By("Verifying PVC is empty")
-			By(fmt.Sprintf("importer.Spec.NodeName %q", importer.Spec.NodeName))
-			Expect(framework.VerifyPVCIsEmpty(f, pvc, importer.Spec.NodeName)).To(BeTrue(), fmt.Sprintf("Found >0 imported files on PVC %q", pvc.Namespace+"/"+pvc.Name))
+			Eventually(func() bool {
+				importer, err := utils.FindPodByPrefix(c, ns, common.ImporterPodName, common.CDILabelSelector)
+				Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Unable to get importer pod %q", ns+"/"+common.ImporterPodName))
+				return importer.Status.ContainerStatuses[0].RestartCount > 0
+			}, timeout, pollingInterval).Should(BeTrue())
 		}
 	}
 

OhadRevah/openshift-python-wrapper

commit 1c0f2a93fa777b5aa03ea57e1578e7b7baebd9dd

[Storage] Fix: DV failed status
It caused by https://github.com/kubevirt/containerized-data-importer/pull/1099

Change-Id: Iba3ab11e3d8f52917a0dd8c753bef5d4c10917f9
Signed-off-by: Qixuan Wang <qwang@redhat.com>

tests/storage/cdi_import/test_import_http.py
@@ -77,7 +77,11 @@ def test_invalid_url(namespace, storage_class_matrix__module__):
         storage_class=storage_class,
         volume_mode=storage_class_matrix__module__[storage_class]["volume_mode"],
     ) as dv:
-        dv.wait_for_status(status=DataVolume.Status.FAILED, timeout=300)
+        dv.wait_for_status(
+            status=DataVolume.Status.IMPORT_IN_PROGRESS,
+            timeout=300,
+            stop_status=DataVolume.Status.SUCCEEDED,
+        )
 
 
 @pytest.mark.polarion("CNV-674")
@@ -293,7 +297,11 @@ def test_wrong_content_type(
         storage_class=storage_class,
         volume_mode=storage_class_matrix__module__[storage_class]["volume_mode"],
     ) as dv:
-        dv.wait_for_status(status=DataVolume.Status.FAILED, timeout=300)
+        dv.wait_for_status(
+            status=DataVolume.Status.IMPORT_IN_PROGRESS,
+            timeout=300,
+            stop_status=DataVolume.Status.SUCCEEDED,
+        )
 
 
 @pytest.mark.parametrize(
@@ -342,7 +350,11 @@ def test_import_invalid_qcow(
         storage_class=storage_class,
         volume_mode=storage_class_matrix__module__[storage_class]["volume_mode"],
     ) as dv:
-        dv.wait_for_status(status=DataVolume.Status.FAILED, timeout=90)
+        dv.wait_for_status(
+            status=DataVolume.Status.IMPORT_IN_PROGRESS,
+            timeout=300,
+            stop_status=DataVolume.Status.SUCCEEDED,
+        )
 
 
 @pytest.mark.parametrize(
@@ -381,7 +393,11 @@ def test_unpack_compressed(
         storage_class=storage_class,
         volume_mode=storage_class_matrix__module__[storage_class]["volume_mode"],
     ) as dv:
-        dv.wait_for_status(status=DataVolume.Status.FAILED, timeout=300)
+        dv.wait_for_status(
+            status=DataVolume.Status.IMPORT_IN_PROGRESS,
+            timeout=300,
+            stop_status=DataVolume.Status.SUCCEEDED,
+        )
 
 
 @pytest.mark.polarion("CNV-2811")
@@ -450,7 +466,11 @@ def test_certconfigmap_incorrect_cert(
         storage_class=storage_class,
         volume_mode=storage_class_matrix__module__[storage_class]["volume_mode"],
     ) as dv:
-        dv.wait_for_status(status=DataVolume.Status.FAILED, timeout=300)
+        dv.wait_for_status(
+            status=DataVolume.Status.IMPORT_IN_PROGRESS,
+            timeout=300,
+            stop_status=DataVolume.Status.SUCCEEDED,
+        )
 
 
 @pytest.mark.parametrize(