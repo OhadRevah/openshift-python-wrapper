c10c13d739c6abea59e4e11b4b0d2e4f283b10ab
Ondra Machacek <omachace@redhat.com>
2020-06-18 15:55:31 +0200

    [VMIMPORT] Check mac address with ovirt sdk
    Change-Id: Ib664db64ac7ab72ddbd7c7265260e122d5b0a4a3
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- tests/vmimport/rhv/test_import.py
+++ tests/vmimport/rhv/test_import.py
@@ -4,6 +4,7 @@ import logging
 import pytest
 import requests
 import yaml
+from providers.rhv import rhv
 from pytest_testconfig import config as py_config
 from resources.secret import Secret
 from resources.virtual_machine_import import VirtualMachineImport
@@ -12,6 +13,15 @@ from utilities.virt import create_vm_import
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="module")
+def rhv_provider():
+    return rhv.RHV(
+        url=py_config["rhv_url"],
+        username=py_config["rhv_username"],
+        password=py_config["rhv_password"],
+    )
 
 
 @pytest.fixture(scope="module")
@@ -47,15 +57,17 @@ def secret(namespace, ovirt_config):
 
 
 @pytest.mark.polarion("CNV-4381")
-def test_vm_import(secret, namespace):
+def test_vm_import(secret, namespace, rhv_provider):
+    source_vm_name = "cirros-vm-for-tests"
+    source_vm_cluster = "iscsi"
     target_vm_name = "test"
     with create_vm_import(
         name="import-vm-by-id",
         namespace=namespace.name,
         provider_credentials_secret_name=secret.name,
         provider_credentials_secret_namespace=secret.namespace,
-        vm_name="cirros-vm-for-tests",
-        cluster_name="iscsi",
+        vm_name=source_vm_name,
+        cluster_name=source_vm_cluster,
         target_vm_name=target_vm_name,
         start_vm=True,
         ovirt_mappings=utils.POD_MAPPING,
@@ -66,10 +78,21 @@ def test_vm_import(secret, namespace):
         vm = vmimport.vm
         assert vm.exists
         assert vm.instance["metadata"]["name"] == target_vm_name
-        check_vm_config(vm.instance)
+        check_vm_config(
+            vm=vm.instance,
+            rhv_provider=rhv_provider,
+            source_vm_name=source_vm_name,
+            source_vm_cluster=source_vm_cluster,
+        )
 
 
-def check_vm_config(vm):
+def check_vm_config(vm, rhv_provider, source_vm_name, source_vm_cluster):
+    vms_service = rhv_provider.api.system_service().vms_service()
+    source_vm = vms_service.list(
+        search=f"name={source_vm_name} cluster={source_vm_cluster}"
+    )[0]
+    source_vm_nic = vms_service.vm_service(source_vm.id).nics_service().list()[0]
+
     spec = vm.spec.template.spec
 
     domain = spec.domain
@@ -84,6 +107,6 @@ def check_vm_config(vm):
     assert domain.machine.type == "q35"
 
     assert len(interfaces) == 1
-    # TODO: use oVirt SDK to check correct Mac address.
+    assert interfaces[0].macAddress == source_vm_nic.mac.address
 
     assert len(spec.volumes) == 1
