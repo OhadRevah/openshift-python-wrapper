60aeea2af36f41bf368b226912083466aa9f26af
Kedar Bidarkar <kbidarka@redhat.com>
2020-06-18 20:23:24 +0530

    Ensure users in privileged scc is not updated after installing CNV
    Change-Id: Ieab5cad5bcd552cd99e7d5c3168fd047da7bc829

---
+++ tests/security/scc/test_default_scc.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests to check, the default Security Context Constraint
+"""
+
+import pytest
+from resources.security_context_constraints import SecurityContextConstraints
+
+
+@pytest.fixture(scope="module")
+def privileged_scc():
+    yield SecurityContextConstraints(name="privileged")
+
+
+@pytest.mark.polarion("CNV-4211")
+def test_users_in_privileged_scc_bz1831536(skip_not_openshift, privileged_scc):
+    """
+    Validate that Users in privileged SCC is not updated after installing CNV
+    """
+    assert len(privileged_scc.instance.users) == 2
+    assert privileged_scc.instance.users[0] == "system:admin"
+    assert (
+        privileged_scc.instance.users[1]
+        == "system:serviceaccount:openshift-infra:build-controller"
+    )
