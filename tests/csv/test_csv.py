0a8e3b918a98fa37c8750b276de883a8bae3eac0
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2020-06-30 10:11:44 +0300

    Add missing assertions. Move things around for testing more cases
    Change-Id: Ieed2b8af50abf217438ce0121537e257c2259797
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- tests/csv/test_csv.py
+++ tests/csv/test_csv.py
@@ -1,8 +1,12 @@
+from base64 import b64decode
+
 import pytest
 from resources.cluster_service_version import ClusterServiceVersion
 
 
-DEFAULT_KEYWORDS = [
+# Check CSV properties like keywords, title, provided by, links etc.
+
+EXPECTED_KEYWORDS_SET = {
     "KubeVirt",
     "Virtualization",
     "VM",
@@ -11,13 +15,13 @@ DEFAULT_KEYWORDS = [
     "Container native virtualization",
     "Virt",
     "Virtual",
-]
-NAMES = ["Source Code", "OpenShift virtualization", "KubeVirt Project"]
-URLS = [
-    "https://github.com/kubevirt",
-    "https://www.openshift.com/learn/topics/virtualization/",
-    "https://kubevirt.io",
-]
+}
+
+EXPECTED_LINK_MAP = {
+    "Source Code": "https://github.com/kubevirt",
+    "OpenShift virtualization": "https://www.openshift.com/learn/topics/virtualization/",
+    "KubeVirt Project": "https://kubevirt.io",
+}
 
 
 @pytest.fixture()
@@ -28,31 +32,58 @@ def csv(default_client):
         return csv
 
 
+@pytest.mark.polarion("CNV-4456")
+@pytest.mark.smoke
+def test_csv_keywords(csv):
+    """
+    Assert keywords. Check that each one of the expected keywords are actually there
+    """
+    assert EXPECTED_KEYWORDS_SET == set(csv.instance.spec.keywords)
+
+
+@pytest.mark.polarion("CNV-4457")
+@pytest.mark.smoke
+def test_csv_links(csv):
+    """
+    Check links list.
+    """
+    # links is a list of dicts, with keys of "name" and "url"
+    # translate the links list to a single name:url dict
+    csv_link_map = {
+        link_dict.get("name"): link_dict.get("url")
+        for link_dict in csv.instance.spec.links
+    }
+    # check that the links list contains all the required name:url pairs
+    assert EXPECTED_LINK_MAP == csv_link_map
+    # check that there are no duplication in links list
+    assert len(EXPECTED_LINK_MAP) == len(csv.instance.spec.links)
+
+
+@pytest.mark.polarion("CNV-4458")
+@pytest.mark.smoke
+def test_csv_icon(csv):
+    """
+    Assert Icon/Logo.
+    """
+    assert len(csv.instance.spec.icon) == 1
+    assert csv.instance.spec.icon[0].mediatype == "image/svg+xml"
+    svg = b64decode(s=csv.instance.spec.icon[0].base64data)
+    with open("tests/csv/logo.svg", "rb") as logo_file:
+        expected_svg = logo_file.read()
+
+    icon_match = svg == expected_svg
+    assert icon_match
+
+
 @pytest.mark.polarion("CNV-4376")
 @pytest.mark.smoke
 def test_csv_properties(csv):
     """
-    Check CSV properties like keywords, title, provided by, links etc.
+    Asserting remaining csv properties.
     """
-    annotations = csv.instance.metadata.annotations
-
-    # Checking List elements in another list.
-    # Assert keywords.
-    keywords = csv.instance.spec.keywords
-    assert all(item in DEFAULT_KEYWORDS for item in keywords)
-
-    # 'Links' contains list of dictionary, [key['name'] for key in links] this will create another
-    # list which of key 'name'. Similarly for url which is value of dictionary.
-    # Assert Links entities i.e. name and url.
-    links = csv.instance.spec.links
-    assert all(name in NAMES for name in [key["name"] for key in links])
-    assert all(url in URLS for url in [value["url"] for value in links])
-
-    # Assert Icon/Logo.
-    assert csv.instance.spec.icon
-
-    # Asserting remaining csv properties.
     assert csv.instance.spec.provider.name == "Red Hat"
     assert csv.instance.spec.displayName == "OpenShift virtualization"
+
+    annotations = csv.instance.metadata.annotations
     assert annotations.get("capabilities") == "Full Lifecycle"
     assert annotations.get("support") == "Red Hat"
