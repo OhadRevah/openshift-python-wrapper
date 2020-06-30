2fb4a6b9bf63499729a594a0a193ba0a65c4b9c2
Kedar Bidarkar <kbidarka@redhat.com>
2020-06-30 19:59:35 +0530

    Fix the test-id for users_privileged_scc_bz1821536 testcase
    Change-Id: If0be5e45c0d53a173e8537180568310a0d7e9349
    Signed-off-by: Kedar Bidarkar <kbidarka@redhat.com>

--- tests/security/scc/test_default_scc.py
+++ tests/security/scc/test_default_scc.py
@@ -13,7 +13,7 @@ def privileged_scc():
     yield SecurityContextConstraints(name="privileged")
 
 
-@pytest.mark.polarion("CNV-4211")
+@pytest.mark.polarion("CNV-4439")
 def test_users_in_privileged_scc_bz1831536(skip_not_openshift, privileged_scc):
     """
     Validate that Users in privileged SCC is not updated after installing CNV
