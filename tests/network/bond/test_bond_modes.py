dd4820958ad6ba35ff28b51887b668b89d57d1f9
yzaindbe <yzaindbe@redhat.com>
2020-07-22 13:18:53 +0300

    Change fixture names to include the test module name
    Change-Id: I7ab186866213e5eeaa023455737bb26acd8eb69d

--- tests/network/bond/test_bond_modes.py
+++ tests/network/bond/test_bond_modes.py
@@ -21,7 +21,7 @@ from utilities.virt import (
 
 
 @pytest.fixture(scope="class")
-def bond_nad(bridge_device_matrix__class__, namespace):
+def bond_modes_nad(bridge_device_matrix__class__, namespace):
     with network_nad(
         namespace=namespace,
         nad_type=bridge_device_matrix__class__,
@@ -32,7 +32,7 @@ def bond_nad(bridge_device_matrix__class__, namespace):
 
 
 @pytest.fixture(scope="class")
-def bond(
+def bond_modes_bond(
     link_aggregation_mode_no_connectivity_matrix__class__,
     network_utility_pods,
     nodes_active_nics,
@@ -54,8 +54,12 @@ def bond(
 
 
 @pytest.fixture(scope="class")
-def bond_bridge(
-    bridge_device_matrix__class__, network_utility_pods, worker_node1, bond_nad, bond,
+def bond_modes_bridge(
+    bridge_device_matrix__class__,
+    network_utility_pods,
+    worker_node1,
+    bond_modes_nad,
+    bond_modes_bond,
 ):
     """
     Create bridge and attach the BOND to it
@@ -63,21 +67,21 @@ def bond_bridge(
     with network_utils.network_device(
         interface_type=bridge_device_matrix__class__,
         nncp_name="bridge-on-bond",
-        interface_name=bond_nad.bridge_name,
+        interface_name=bond_modes_nad.bridge_name,
         network_utility_pods=network_utility_pods,
-        ports=[bond.bond_name],
+        ports=[bond_modes_bond.bond_name],
         node_selector=worker_node1.name,
     ) as br:
         yield br
 
 
 @pytest.fixture(scope="class")
-def bond_vm(
-    worker_node1, namespace, unprivileged_client, bond_nad, bond_bridge,
+def bond_modes_vm(
+    worker_node1, namespace, unprivileged_client, bond_modes_nad, bond_modes_bridge,
 ):
     name = "bond-vm"
     networks = OrderedDict()
-    networks[bond_nad.name] = bond_nad.name
+    networks[bond_modes_nad.name] = bond_modes_nad.name
 
     with VirtualMachineForTests(
         namespace=namespace.name,
@@ -95,19 +99,19 @@ def bond_vm(
 @pytest.mark.usefixtures("skip_no_bond_support")
 class TestBondModes:
     @pytest.mark.polarion("CNV-4382")
-    def test_bond_created(self, workers_ssh_executors, bond):
+    def test_bond_created(self, workers_ssh_executors, bond_modes_bond):
         bonding_path = "/sys/class/net/test-bond/bonding"
-        _exec = workers_ssh_executors[bond.node_selector]
+        _exec = workers_ssh_executors[bond_modes_bond.node_selector]
         mode = _exec.run_command(command=shlex.split(f"cat {bonding_path}/mode"))[1]
         slaves = _exec.run_command(command=shlex.split(f"cat {bonding_path}/slaves"))[1]
         worker_slaves = slaves.split()
         worker_slaves.sort()
-        bond.slaves.sort()
-        assert mode.split()[0] == bond.mode
-        assert worker_slaves == bond.slaves
+        bond_modes_bond.slaves.sort()
+        assert mode.split()[0] == bond_modes_bond.mode
+        assert worker_slaves == bond_modes_bond.slaves
 
     @pytest.mark.polarion("CNV-4383")
-    def test_vm_started(self, bond_vm):
-        bond_vm.start(wait=True)
-        bond_vm.vmi.wait_until_running()
-        wait_for_vm_interfaces(vmi=bond_vm.vmi)
+    def test_vm_started(self, bond_modes_vm):
+        bond_modes_vm.start(wait=True)
+        bond_modes_vm.vmi.wait_until_running()
+        wait_for_vm_interfaces(vmi=bond_modes_vm.vmi)
