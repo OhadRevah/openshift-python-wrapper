670d31ff1c39ab9d7637572825dc7dbcf1779055
Ofir Nash <onash@redhat.com>
2020-07-23 15:38:01 +0300

    Marked test_nmstate test's as destructive
    Change-Id: Ib03ab76b6c5efeb28de13c92883da729a8dc7aca

--- tests/network/nmstate/test_nmstate.py
+++ tests/network/nmstate/test_nmstate.py
@@ -9,6 +9,7 @@ LOGGER = logging.getLogger(__name__)
 REMOTE_IP = "8.8.8.8"
 
 
+@pytest.mark.destructive
 @pytest.mark.run(before="TestAfterBridgeTeardown")
 @pytest.mark.usefixtures("skip_rhel7_workers")
 class TestWithDhcpOverBridge:
@@ -49,6 +50,7 @@ class TestWithDhcpOverBridge:
 # Test class should be run as last, because it should check connectivity after,
 # bridge was created, got dhcp of management and release it back to the port
 # The first test marked with @pytest.mark.run(before="TestAfterBridgeTeardown") to ensure it.
+@pytest.mark.destructive
 @pytest.mark.usefixtures("skip_rhel7_workers")
 class TestAfterBridgeTeardown:
     @pytest.mark.polarion("CNV-3028")
