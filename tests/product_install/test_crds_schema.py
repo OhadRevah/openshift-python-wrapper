a6f51ecc6cdc5c4d2bf056f049ebcdfd355bfe3c
Satyajit Bulage <sbulage@redhat.com>
2020-08-17 15:29:40 +0530

    Added automation for JIRA:CNV-4377 related to CRD NonStructuralSchema.
    Change-Id: Ie23d155cfa455b314ce6efb0cf8e7b1c8a8fefa6
    Signed-off-by: Satyajit Bulage <sbulage@redhat.com>

---
+++ tests/product_install/test_crds_schema.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pytest
+from resources.custom_resource_definition import CustomResourceDefinition
+
+
+@pytest.fixture(scope="module")
+def all_crd_resources(default_client):
+    """
+    Returns List of CustomResourceDefinitions Resources.
+    """
+    return list(CustomResourceDefinition.get(default_client, group="kubevirt.io"))
+
+
+@pytest.mark.polarion("CNV-4695")
+def test_check_crd_non_structural_schema(all_crd_resources):
+    failed_crds = []
+    for kubevirt_crd_resource in all_crd_resources:
+        for resource_condition in kubevirt_crd_resource.instance.status.conditions:
+            if resource_condition["NonStructuralSchema"]:
+                failed_crds.append(kubevirt_crd_resource.name)
+
+    assert not failed_crds, f"CRD is having Non Structual Schema {failed_crds}"
