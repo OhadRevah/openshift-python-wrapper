362cc2ec53eb94c5e6fdf4435fbeb47645819976
rnetser <rnetser@redhat.com>
2020-09-08 19:11:06 +0300

    Fix SSP CR conditions
    - Test the CRs and not CRDs
    - Fix tested conditions
    - Add missing CR - kubevirtmetricsaggregations
    - Fix labeller name
    
    Depends on https://gitlab.cee.redhat.com/cnv-qe/ocp-python-wrapper/-/merge_requests/35
    
    Change-Id: Id3f5e32a02c5e54f31bdd2b78703bc0214cc0c94

--- tests/compute/ssp/operator_tests/test_ssp_custom_resources.py
+++ tests/compute/ssp/operator_tests/test_ssp_custom_resources.py
@@ -3,73 +3,62 @@
 import logging
 
 import pytest
+from pytest_testconfig import config as py_config
 from resources.custom_resource_definition import CustomResourceDefinition
+from resources.kubevirt_common_templates_bundle import KubevirtCommonTemplatesBundle
+from resources.kubevirt_metrics_aggregation import KubevirtMetricsAggregation
+from resources.kubevirt_node_labeller_bundle import KubevirtNodeLabellerBundle
+from resources.kubevirt_template_validaotr import KubevirtTemplateValidator
 
 
+CONDITIONS_DICT = {
+    CustomResourceDefinition.Condition.PROGRESSING: "False",
+    CustomResourceDefinition.Condition.AVAILABLE: "True",
+    CustomResourceDefinition.Condition.DEGRADED: "False",
+}
+HCO_NAMESPACE = py_config["hco_namespace"]
 LOGGER = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="class")
-def crd_resources(admin_client):
-    return CustomResourceDefinition.get(dyn_client=admin_client)
+def ssp_resource(request, admin_client):
+    # Returns the requested resource passed in request.param
+    cr = request.param["cr_name"]
+    cr_namespace = request.param["namespace"]
+    ssp_cr = list(cr.get(dyn_client=admin_client, namespace=cr_namespace))
+    assert ssp_cr, f"CR {cr.kind} was not found in namespace {cr_namespace}."
+    return ssp_cr[0]
 
 
-@pytest.fixture(scope="class")
-def ssp_resource(request, crd_resources):
-    ssp_resource = request.param
-    LOGGER.info(f"Get SSP {ssp_resource} resource.")
-    for resource in crd_resources:
-        if ssp_resource in resource.name:
-            return resource
-
-    return False
-
-
-def get_failed_resource_conditions(resource):
-    """ Check resource conditions; return failed conditions """
-    return [i for i in resource.instance.status.conditions if i["status"] != "True"]
-
-
-class TestSSPCustomResources:
-    @pytest.mark.parametrize(
-        "ssp_resource",
-        [
-            pytest.param(
-                "kubevirtcommontemplatesbundles",
-                marks=(pytest.mark.polarion("CNV-3771")),
-            ),
-            pytest.param(
-                "kubevirtnodelabellerbundles", marks=(pytest.mark.polarion("CNV-3773")),
-            ),
-            pytest.param(
-                "kubevirttemplatevalidators", marks=(pytest.mark.polarion("CNV-3774")),
-            ),
-        ],
-        indirect=True,
-    )
-    def test_verify_ssp_crd_existence(self, ssp_resource):
-        assert ssp_resource, "CRD was not found."
-
-    @pytest.mark.parametrize(
-        "ssp_resource",
-        [
-            pytest.param(
-                "kubevirtcommontemplatesbundles",
-                marks=(pytest.mark.polarion("CNV-2957")),
-            ),
-            pytest.param(
-                "kubevirtnodelabellerbundles", marks=(pytest.mark.polarion("CNV-3737")),
-            ),
-            pytest.param(
-                "kubevirttemplatevalidators", marks=(pytest.mark.polarion("CNV-3736")),
-            ),
-        ],
-        indirect=True,
-    )
-    def test_verify_ssp_crd_conditions(self, ssp_resource):
-        failed_resource_conditions = get_failed_resource_conditions(
-            resource=ssp_resource
-        )
-        assert (
-            not failed_resource_conditions
-        ), f"The following conditions failed: {failed_resource_conditions}."
+@pytest.mark.parametrize(
+    "ssp_resource",
+    [
+        pytest.param(
+            {"cr_name": KubevirtCommonTemplatesBundle, "namespace": "openshift"},
+            marks=(pytest.mark.polarion("CNV-2957")),
+        ),
+        pytest.param(
+            {"cr_name": KubevirtNodeLabellerBundle, "namespace": HCO_NAMESPACE},
+            marks=(pytest.mark.polarion("CNV-3737")),
+        ),
+        pytest.param(
+            {"cr_name": KubevirtTemplateValidator, "namespace": HCO_NAMESPACE},
+            marks=(pytest.mark.polarion("CNV-3736")),
+        ),
+        pytest.param(
+            {"cr_name": KubevirtMetricsAggregation, "namespace": HCO_NAMESPACE},
+            marks=(pytest.mark.polarion("CNV-4470")),
+        ),
+    ],
+    indirect=True,
+)
+def test_verify_ssp_crd_conditions(ssp_resource):
+    LOGGER.info(f"Check {ssp_resource.name} conditions.")
+    resource_conditions = {
+        condition.type: condition.status
+        for condition in ssp_resource.instance.status.conditions
+        if condition.type in CONDITIONS_DICT.keys()
+    }
+    assert (
+        resource_conditions == CONDITIONS_DICT
+    ), f"The following conditions failed: {resource_conditions}."
