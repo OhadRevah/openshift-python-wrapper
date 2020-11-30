de581badc9bbb8cdf6a76e42641dd2a329321e87
Meni Yakove <myakove@redhat.com>
2020-11-30 16:11:24 +0200

    py_config for smoke tests.
    Use only latest Fedora.
    
    Change-Id: I1ec9f9bc5d7fc3d1da767cc3af1b22c10ebc668f

---
+++ tests/global_config_smoke.py
@@ -0,0 +1,40 @@
+import os
+
+import pytest_testconfig
+from utilities.infra import Images
+
+
+global config
+global_config = pytest_testconfig.load_python(
+    py_file="tests/global_config.py", encoding="utf-8"
+)
+
+fedora_os_matrix = [
+    {
+        "fedora-33": {
+            "image_name": Images.Fedora.FEDORA33_IMG,
+            "image_path": os.path.join(Images.Fedora.DIR, Images.Fedora.FEDORA33_IMG),
+            "dv_size": Images.Fedora.DEFAULT_DV_SIZE,
+            # TODO: Modify to fedora33 once it is added to templates
+            "template_labels": {
+                "os": "fedora32",
+                "workload": "server",
+                "flavor": "tiny",
+            },
+        }
+    },
+]
+for _dir in dir():
+    val = locals()[_dir]
+    if not (
+        isinstance(val, bool)
+        or isinstance(val, list)
+        or isinstance(val, dict)
+        or isinstance(val, str)
+    ):
+        continue
+
+    if _dir in ["encoding", "py_file"]:
+        continue
+
+    config[_dir] = locals()[_dir]  # noqa: F821
