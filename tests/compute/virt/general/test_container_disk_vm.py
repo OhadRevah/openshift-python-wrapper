eb7a5bf0144dd2446c86af63f8d54b5f8c5df6b7
Meni Yakove <myakove@redhat.com>
2020-12-01 15:42:44 +0200

    Smoke test for VM with container disk.
    Change-Id: Iab93ea69b4926e7427215493c44b5fe236182862

---
+++ tests/compute/virt/general/test_container_disk_vm.py
@@ -0,0 +1,26 @@
+import pytest
+from utilities.console import Fedora
+from utilities.virt import (
+    FEDORA_CLOUD_INIT_PASSWORD,
+    VirtualMachineForTests,
+    fedora_vm_body,
+    wait_for_console,
+)
+
+
+@pytest.mark.smoke
+@pytest.mark.polarion("CNV-5501")
+def test_container_disk_vm(
+    namespace,
+    unprivileged_client,
+):
+    name = "container-disk-vm"
+    with VirtualMachineForTests(
+        namespace=namespace.name,
+        name=name,
+        client=unprivileged_client,
+        body=fedora_vm_body(name=name),
+        cloud_init_data=FEDORA_CLOUD_INIT_PASSWORD,
+    ) as vm:
+        vm.start(wait=True)
+        wait_for_console(vm=vm, console_impl=Fedora)
