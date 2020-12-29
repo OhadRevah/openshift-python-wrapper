395b1424018d60a286ece768a887ef1409ca1ad1
Inbar Rose <irose@redhat.com>
2020-12-29 09:09:33 +0200

    fix test to use the new "stable" channel introduced in 2.6.
    Change-Id: I63d66bddad9a86ffb9bc48c5c87ca6c6bf6d30fd

--- tests/install_upgrade_operators/csv/test_immutable_image_using_sha.py
+++ tests/install_upgrade_operators/csv/test_immutable_image_using_sha.py
@@ -3,26 +3,27 @@ from resources.package_manifest import PackageManifest
 
 
 @pytest.fixture()
-def images_list(admin_client, hco_namespace):
+def hco_package_stable_channel_images(admin_client, hco_namespace):
     """
-    Get package from PackageManifest.
+    Get a list of all images in the kubevirt-hyperconverged package on the stable channel
     """
-    image_list = []
     for package in PackageManifest.get(
-        dyn_client=admin_client, namespace=hco_namespace
+        dyn_client=admin_client,
+        namespace=hco_namespace,
+        name="kubevirt-hyperconverged",
     ):
-        if package.name == "kubevirt-hyperconverged":
-            for channel in package.instance.status.channels:
-                if 2.3 < float(channel.name):
-                    image_list += channel.currentCSVDesc["relatedImages"]
-
-    return image_list
+        for channel in package.instance.status.channels:
+            if channel.name == "stable":
+                return channel.currentCSVDesc["relatedImages"]
 
 
 @pytest.mark.polarion("CNV-4751")
-def test_immutable_image_using_sha(images_list):
+def test_immutable_image_using_sha(hco_package_stable_channel_images):
     """
-    Retrieve images from Package Manifest.
-    Check links contains SHA256 string in those images.
+    check all images of the stable channel on the kubevirt-hyperconverged Package Manifest.
+    make sure all images have SHA256 in their string (this indicates they are immutable)
     """
-    assert not [image for image in images_list if "sha256" not in image]
+    # verify all images contain "sha256" in their name. on failure this will be a list of images without "sha256"
+    assert not list(
+        filter(lambda image: "sha256" not in image, hco_package_stable_channel_images)
+    )
