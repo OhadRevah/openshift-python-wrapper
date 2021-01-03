92ee9c5d05cf4aee279efcd41dec88b99a0e755a
Meni Yakove <myakove@redhat.com>
2021-01-03 09:00:36 +0200

    Network: veth remove, check only VM veth.
    Remove vmb, not needed for the test.
    Check only VM veth and not all veth on the node.
    
    Change-Id: I7a5fea18d6acfa6de09d0344f54ff752784ae58d

--- tests/network/general/test_veth_removed_after_vms_deleted.py
+++ tests/network/general/test_veth_removed_after_vms_deleted.py
@@ -17,20 +17,26 @@ from utilities.virt import VirtualMachineForTests, fedora_vm_body
 LOGGER = logging.getLogger(__name__)
 BR1TEST = "br1test"
 BR2TEST = "br2test"
-NETWORKS = {"net1": BR1TEST, "net2": BR2TEST}
 
 
-def count_veth_devices_on_host(pod):
+def count_veth_devices_on_host(pod, bridge):
     """
     Return how many veth devices exist on the host running pod
 
     Args:
         pod (Pod): Pod object.
+        bridge (str): Master bridge name.
 
     Returns:
-        int: number of veth devices on host
+        int: number of veth devices on host for bridge.
     """
-    out = pod.execute(command=["bash", "-c", "ip -o link show type veth | wc -l"])
+    out = pod.execute(
+        command=[
+            "bash",
+            "-c",
+            f"ip -o link show type veth | grep 'master {bridge}' | wc -l",
+        ]
+    )
 
     return int(out.strip())
 
@@ -43,42 +49,41 @@ def remove_vath_br1test_nad(namespace):
         interface_name=BR1TEST,
         namespace=namespace,
     ) as nad:
-        yield nad
+        with utilities.network.network_nad(
+            nad_type=utilities.network.LINUX_BRIDGE,
+            nad_name=BR2TEST,
+            interface_name=BR1TEST,
+            namespace=namespace,
+        ):
+            yield nad
 
 
 @pytest.fixture()
-def remove_vath_br2test_nad(namespace):
-    with utilities.network.network_nad(
-        nad_type=utilities.network.LINUX_BRIDGE,
-        nad_name=BR2TEST,
-        interface_name=BR1TEST,
-        namespace=namespace,
-    ) as nad:
-        yield nad
-
-
-@pytest.fixture()
-def remove_vath_bridge_device(utility_pods, schedulable_nodes):
+def remove_vath_bridge_device(
+    utility_pods, schedulable_nodes, worker_node1, remove_vath_br1test_nad
+):
     with network_utils.network_device(
         interface_type=utilities.network.LINUX_BRIDGE,
         nncp_name="veth-removed",
-        interface_name=BR1TEST,
+        interface_name=remove_vath_br1test_nad.bridge_name,
         network_utility_pods=utility_pods,
-        nodes=schedulable_nodes,
+        node_selector=worker_node1.name,
     ) as dev:
         yield dev
 
 
 @pytest.fixture()
-def remove_vath_bridge_attached_vma(namespace, unprivileged_client):
+def remove_vath_bridge_attached_vma(namespace, unprivileged_client, worker_node1):
     name = "vma"
+    networks = {"net1": BR1TEST, "net2": BR2TEST}
     with VirtualMachineForTests(
         namespace=namespace.name,
         name=name,
-        networks=NETWORKS,
-        interfaces=sorted(NETWORKS.keys()),
+        networks=networks,
+        interfaces=sorted(networks.keys()),
         client=unprivileged_client,
         body=fedora_vm_body(name=name),
+        node_selector=worker_node1.name,
         teardown=False,
     ) as vm:
         vm.start(wait=True)
@@ -87,53 +92,46 @@ def remove_vath_bridge_attached_vma(namespace, unprivileged_client):
 
 
 @pytest.fixture()
-def remove_vath_bridge_attached_vmb(namespace, unprivileged_client):
-    name = "vmb"
-    with VirtualMachineForTests(
-        namespace=namespace.name,
-        name=name,
-        networks=NETWORKS,
-        interfaces=sorted(NETWORKS.keys()),
-        client=unprivileged_client,
-        body=fedora_vm_body(name=name),
-        teardown=False,
-    ) as vm:
-        vm.start(wait=True)
-        vm.vmi.wait_until_running()
-        yield vm
+def worker_pod(utility_pods, worker_node1):
+    return network_utils.get_worker_pod(
+        network_utility_pods=utility_pods, worker_node=worker_node1
+    )
+
+
+@pytest.fixture()
+def veth_interfaces_exists(
+    utility_pods, worker_node1, worker_pod, remove_vath_bridge_device
+):
+    assert (
+        count_veth_devices_on_host(
+            pod=worker_pod, bridge=remove_vath_bridge_device.bridge_name
+        )
+        == 2
+    )
 
 
 @pytest.mark.polarion("CNV-681")
 def test_veth_removed_from_host_after_vm_deleted(
     skip_rhel7_workers,
+    worker_node1,
     utility_pods,
+    worker_pod,
     remove_vath_br1test_nad,
-    remove_vath_br2test_nad,
     remove_vath_bridge_device,
     remove_vath_bridge_attached_vma,
-    remove_vath_bridge_attached_vmb,
+    veth_interfaces_exists,
 ):
     """
     Check that veth interfaces are removed from host after VM deleted
     """
-    for vm in (remove_vath_bridge_attached_vma, remove_vath_bridge_attached_vmb):
-        vmi_interfaces = vm.vmi.instance.status.interfaces or []
-        for pod in utility_pods:
-            if pod.node.name == vm.vmi.node.name:
-                _delete_vm_and_compare_veth(
-                    pod=pod, vm=vm, vmi_interfaces=vmi_interfaces
-                )
-                break
-
-
-def _delete_vm_and_compare_veth(pod, vm, vmi_interfaces):
-    host_veth_before_delete = count_veth_devices_on_host(pod=pod)
-    expect_host_veth = host_veth_before_delete - len(vmi_interfaces)
-    vm.delete(wait=True)
-
+    remove_vath_bridge_attached_vma.delete(wait=True)
     sampler = TimeoutSampler(
-        timeout=180, sleep=1, func=count_veth_devices_on_host, pod=pod
+        timeout=180,
+        sleep=1,
+        func=count_veth_devices_on_host,
+        pod=worker_pod,
+        bridge=remove_vath_bridge_device.bridge_name,
     )
     for sample in sampler:
-        if sample == expect_host_veth:
+        if sample == 0:
             return True
