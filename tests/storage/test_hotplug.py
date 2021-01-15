7b015585679dad9d82ba43a12790bd22d90757bb
Yan Du <yadu@redhat.com>
2021-01-15 19:35:39 +0800

    [Storage] Update the hotplug test case
    - Add step to configure hotplug feature gate configuration in hco
    - Remove pytest.mark.bugzilla since the bug is verified
    
    Change-Id: I8ab224c952dfc29acd2d8fe4f61145c8c7f5d7b2

--- tests/storage/test_hotplug.py
+++ tests/storage/test_hotplug.py
@@ -2,8 +2,7 @@
 Automation for Hot Plug
 """
 import pytest
-
-from utilities.infra import BUG_STATUS_CLOSED
+from resources.resource import ResourceEditor
 
 
 @pytest.fixture()
@@ -11,9 +10,20 @@ def feature_gate_data(kubevirt_config_cm):
     return kubevirt_config_cm.instance["data"]["feature-gates"].split(",")
 
 
-@pytest.mark.bugzilla(
-    1910857, skip_when=lambda bug: bug.status not in BUG_STATUS_CLOSED
-)
+@pytest.fixture()
+def enabled_hotplugvolumes_feature_gate(hyperconverged_resource):
+    with ResourceEditor(
+        patches={
+            hyperconverged_resource: {
+                "spec": {"featureGates": {"HotplugVolumes": True}}
+            }
+        }
+    ) as edits:
+        yield edits
+
+
 @pytest.mark.polarion("CNV-5508")
-def test_hotplugvolumes_feature_gate(feature_gate_data):
+def test_hotplugvolumes_feature_gate(
+    enabled_hotplugvolumes_feature_gate, feature_gate_data
+):
     assert "HotplugVolumes" in feature_gate_data
