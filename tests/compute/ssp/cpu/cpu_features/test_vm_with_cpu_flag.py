e79e8f7c33be81027a1db7053bd4a70c8aa8fcc1
rnetser <rnetser@redhat.com>
2021-02-07 11:11:16 +0200

    VM with CPU flag - use nodes_common_cpu_model
    - Use nodes_common_cpu_model to get a CPU model which
    is supported on all nodes.
    - Cosmetic changes.
    
    Change-Id: I4ecb4d4b2e5621dc2320200e70cfb6fb9ac1f532

--- tests/compute/ssp/cpu/cpu_features/test_vm_with_cpu_flag.py
+++ tests/compute/ssp/cpu/cpu_features/test_vm_with_cpu_flag.py
@@ -2,7 +2,6 @@
 VM with CPU flag
 """
 import pytest
-from resources.node import Node
 from resources.utils import TimeoutExpiredError
 
 from utilities import console
@@ -10,40 +9,23 @@ from utilities.virt import (
     FEDORA_CLOUD_INIT_PASSWORD,
     VirtualMachineForTests,
     fedora_vm_body,
+    wait_for_console,
     wait_for_vm_interfaces,
 )
 
 
 @pytest.fixture()
-def cpu_module(admin_client):
-    """
-    Get the cpu module supported on nodes (query node with feature.node.kubernetes.io/cpu-model-{module})
-    Skip the test if no node has the following CPU module:
-    "Haswell", "Haswell-noTSX", "Westmere", "IvyBridge", "SandyBridge"
-    """
-    cpu_module = ["Haswell", "Haswell-noTSX", "Westmere", "IvyBridge", "SandyBridge"]
-    for cpu in cpu_module:
-        node_with_cpu = Node.get(
-            admin_client,
-            label_selector=f"feature.node.kubernetes.io/cpu-model-{cpu}=true",
-        )
-        if len(list(node.name for node in node_with_cpu)) > 1:
-            return cpu
-    pytest.skip(msg=f"Did not find node with CPU modules: {cpu_module}")
-
-
-@pytest.fixture()
-def cpu_flag_vm_positive(cpu_module, namespace, unprivileged_client):
+def cpu_flag_vm_positive(nodes_common_cpu_model, namespace, unprivileged_client):
     name = "vm-cpu-flags-positive"
     with VirtualMachineForTests(
         name=name,
         namespace=namespace.name,
-        cpu_flags={"model": cpu_module},
+        cpu_flags={"model": nodes_common_cpu_model},
         body=fedora_vm_body(name=name),
         cloud_init_data=FEDORA_CLOUD_INIT_PASSWORD,
         client=unprivileged_client,
     ) as vm:
-        vm.start(wait=True, timeout=240)
+        vm.start(wait=True)
         vm.vmi.wait_until_running()
         wait_for_vm_interfaces(vmi=vm.vmi)
         yield vm
@@ -80,22 +62,21 @@ def test_vm_with_cpu_flag_negative(cpu_flag_vm_negative):
     """
     Negative test:
     Test VM with wrong cpu model,
-    VM should not run in this case since cpu model not exists on host
+    VM should not run in this case since cpu model not exist on any of the nodes
     """
     with pytest.raises(TimeoutExpiredError):
         cpu_flag_vm_negative.vmi.wait_until_running(timeout=60)
 
 
 @pytest.mark.polarion("CNV-1269")
-def test_vm_with_cpu_flag_positive_case(cpu_flag_vm_positive):
+def test_vm_with_cpu_flag_positive_case(cpu_flag_vm_positive, nodes_common_cpu_model):
     """
-    Test VM with cpu flag, test the VM started and enter the console
+    Test VM with cpu flag, test CPU model and console access
     """
-    assert cpu_flag_vm_positive.cpu_flags["model"] == (
+    assert (
         cpu_flag_vm_positive.instance["spec"]["template"]["spec"]["domain"]["cpu"][
             "model"
         ]
+        == nodes_common_cpu_model
     )
-    with console.Fedora(vm=cpu_flag_vm_positive) as vm_console:
-        vm_console.sendline("cat /etc/redhat-release | wc -l\n")
-        vm_console.expect("1", timeout=20)
+    wait_for_console(vm=cpu_flag_vm_positive, console_impl=console.Fedora)
