c81e68e84807cfd608b51ce5082095ccab718560
amastbau <amastbau@redhat.com>
2021-03-01 18:42:36 +0200

    [MTV] add config file for v2v tests
    Change-Id: Ie851f8e2a1045e26f51a9009a89de0dfedfd276a

---
+++ tests/global_config_v2v.py
@@ -0,0 +1,44 @@
+import pytest_testconfig
+from resources.datavolume import DataVolume
+from resources.storage_class import StorageClass
+
+
+global config
+global_config = pytest_testconfig.load_python(
+    py_file="tests/global_config.py", encoding="utf-8"
+)
+
+no_unprivileged_client = True
+mtv_namespace = "openshift-rhmtv"
+
+storage_class_matrix = [
+    {
+        StorageClass.Types.NFS: {
+            "volume_mode": DataVolume.VolumeMode.FILE,
+            "access_mode": DataVolume.AccessMode.RWX,
+        }
+    },
+    {
+        StorageClass.Types.CEPH_RBD: {
+            "volume_mode": DataVolume.VolumeMode.BLOCK,
+            "access_mode": DataVolume.AccessMode.RWX,
+            "default": True,
+        }
+    },
+]
+
+
+for _dir in dir():
+    val = locals()[_dir]
+    if not (
+        isinstance(val, bool)
+        or isinstance(val, list)
+        or isinstance(val, dict)
+        or isinstance(val, str)
+    ):
+        continue
+
+    if _dir in ["encoding", "py_file"]:
+        continue
+
+    config[_dir] = locals()[_dir]  # noqa: F821
