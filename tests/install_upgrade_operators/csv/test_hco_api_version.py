78c4aa44654ecbe34d658746c0abffb6cc609d5c
Satyajit Bulage <sbulage@redhat.com>
2021-03-02 22:27:53 +0530

    Check HCO CRD api version to 'v1beta1'.
    Change-Id: I3716c09be44c5f2ac914876ae6f016b91e30f7c4
    Signed-off-by: Satyajit Bulage <sbulage@redhat.com>

---
+++ tests/install_upgrade_operators/csv/test_hco_api_version.py
@@ -0,0 +1,10 @@
+import pytest
+from ocp_resources.resource import Resource
+
+
+@pytest.mark.polarion("CNV-5832")
+def test_hyperconverged_cr_api_version(hyperconverged_resource):
+    """
+    This test will check the Hyperconverged CR's api_version for v1beta1
+    """
+    assert Resource.ApiVersion.V1BETA1 in hyperconverged_resource.instance.apiVersion
