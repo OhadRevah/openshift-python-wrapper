0877b11d48c4c2dff7316e8a8f6e6990dc7b84f0
Zvi Cahana <zcahana@redhat.com>
2021-03-03 21:18:41 +0200

    Add HCO namespace deletion test
    Change-Id: I1aeea8b04e433213d4e524ddba6b85a2f0d497bb

---
+++ tests/install_upgrade_operators/product_uninstall/test_remove_namespace.py
@@ -0,0 +1,78 @@
+import logging
+from http import HTTPStatus
+
+import pytest
+from kubernetes.client.rest import ApiException
+
+
+LOGGER = logging.getLogger(__name__)
+
+DELETION_ERROR_MESSAGE = (
+    r"denied the request.*"
+    r"HyperConverged CR is still present.*"
+    r"please remove it before deleting the containing namespace"
+)
+
+
+@pytest.fixture()
+def remove_hyperconverged_resource(hyperconverged_resource):
+    hyperconverged_resource.delete(wait=True, timeout=600)
+
+
+@pytest.mark.install
+class TestRemoveNamespace:
+    @pytest.mark.polarion("CNV-5846")
+    def test_block_namespace_removal(
+        self,
+        hyperconverged_resource,
+        hco_namespace,
+    ):
+        """
+        testcase to verify that HCO namespace deletion is blocked while the HyperConverged
+        resource still exists.
+
+        test plan:
+
+            1. delete HCO namespace
+            2. verify that the deletion failed with a proper error message
+        """
+
+        LOGGER.info("Attempting to delete HCO namespace")
+
+        with pytest.raises(
+            ApiException,
+            match=DELETION_ERROR_MESSAGE,
+        ) as excinfo:
+            hco_namespace.delete()
+
+        assert (
+            excinfo.value.status == HTTPStatus.FORBIDDEN
+        ), f"Unexpected HTTP status {excinfo.value.status} when deleting HCO namespace"
+
+        assert hco_namespace.exists, "HCO namespace does not exist"
+
+        assert (
+            hyperconverged_resource.instance.metadata.get("deletionTimestamp") is None
+        ), "deletionTimestamp is set on HCO namespace"
+
+    @pytest.mark.polarion("CNV-5847")
+    @pytest.mark.run(after="test_block_removal")
+    def test_unblock_namespace_removal(
+        self,
+        remove_hyperconverged_resource,
+        hco_namespace,
+    ):
+        """
+        testcase to verify that HCO namespace deletion is unblocked when the HyperConverged
+        resource does not exist.
+
+        test plan:
+
+            1. delete HCO CR (via a fixture)
+            2. delete HCO namespace
+            3. verify that HCO namespace is now removed
+        """
+
+        hco_namespace.delete(wait=True)
+
+        assert not hco_namespace.exists, "HCO namespace still exists"
