d455b3d40c40710984b604827f313f7c30c792f6
Meni Yakove <myakove@redhat.com>
2021-03-04 09:05:22 +0200

    Network: OVS optin fix and clean code.
    Change-Id: Icda0d2fcb46174c22f4c164e31e8009d009867f7

--- tests/network/ovs_optin/test_ovs_optin.py
+++ tests/network/ovs_optin/test_ovs_optin.py
@@ -24,18 +24,6 @@ def wait_for_ovs_removed(admin_client, ovs_daemonset, network_addons_config):
     )
 
 
-def updated_metadata(hyperconverged_resource, annotations):
-    return {
-        "metadata": {
-            "annotations": annotations,
-            "name": hyperconverged_resource.name,
-            "resourceVersion": hyperconverged_resource.instance.metadata.resourceVersion,
-        },
-        "kind": hyperconverged_resource.kind,
-        "apiVersion": hyperconverged_resource.api_version,
-    }
-
-
 @pytest.fixture()
 def hyperconverged_ovs_annotations_disabled(
     hyperconverged_resource,
@@ -50,8 +38,6 @@ def hyperconverged_ovs_annotations_disabled(
         }
     ):
         yield
-    wait_for_ovs_status(network_addons_config=network_addons_config, status=False)
-    hyperconverged_ovs_annotations_enabled.wait_until_deployed()
 
 
 @pytest.fixture()
@@ -60,29 +46,12 @@ def hyperconverged_ovs_annotations_removed(
     network_addons_config,
     hyperconverged_ovs_annotations_enabled,
 ):
-    origin_annotations = hyperconverged_resource.instance.to_dict()["metadata"][
-        "annotations"
-    ]
-    annotations = origin_annotations.copy()
-    annotations.pop(DEPLOY_OVS)
     with ResourceEditor(
         patches={
-            hyperconverged_resource: updated_metadata(
-                hyperconverged_resource=hyperconverged_resource, annotations=annotations
-            )
-        },
-        user_backups={
-            hyperconverged_resource: updated_metadata(
-                hyperconverged_resource=hyperconverged_resource,
-                annotations=origin_annotations,
-            )
-        },
-        action="replace",
+            hyperconverged_resource: {"metadata": {"annotations": {DEPLOY_OVS: None}}}
+        }
     ):
         yield
-
-    wait_for_ovs_status(network_addons_config=network_addons_config, status=False)
-    hyperconverged_ovs_annotations_enabled.wait_until_deployed()
 
 
 class TestOVSOptIn:
@@ -90,9 +59,9 @@ class TestOVSOptIn:
     def test_ovs_installed(
         self,
         admin_client,
+        network_addons_config,
         hyperconverged_ovs_annotations_enabled,
         hyperconverged_ovs_annotations_fetched,
-        network_addons_config,
     ):
         verify_ovs_installed_with_annotations(
             admin_client=admin_client,
@@ -101,13 +70,13 @@ class TestOVSOptIn:
             network_addons_config=network_addons_config,
         )
 
-    @pytest.mark.polarion("CNV-5531")
-    def test_ovs_not_installed_annotations_disabled(
+    @pytest.mark.polarion("CNV-5533")
+    def test_ovs_not_installed_annotations_removed(
         self,
         admin_client,
-        hyperconverged_ovs_annotations_enabled,
-        hyperconverged_ovs_annotations_disabled,
         network_addons_config,
+        hyperconverged_ovs_annotations_enabled,
+        hyperconverged_ovs_annotations_removed,
     ):
         wait_for_ovs_removed(
             admin_client=admin_client,
@@ -115,13 +84,13 @@ class TestOVSOptIn:
             network_addons_config=network_addons_config,
         )
 
-    @pytest.mark.polarion("CNV-5533")
-    def test_ovs_not_installed_annotations_removed(
+    @pytest.mark.polarion("CNV-5531")
+    def test_ovs_not_installed_annotations_disabled(
         self,
         admin_client,
-        hyperconverged_ovs_annotations_enabled,
-        hyperconverged_ovs_annotations_removed,
         network_addons_config,
+        hyperconverged_ovs_annotations_enabled,
+        hyperconverged_ovs_annotations_disabled,
     ):
         wait_for_ovs_removed(
             admin_client=admin_client,
