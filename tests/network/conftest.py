b4b09b2aa98b1b551fa5e50636694a06c43f4614
Ofir Nash <onash@redhat.com>
2021-03-22 18:16:43 +0200

    Network: remove network_interface fixture.
    Remove the fixture network_interface as it is no longer used. Remove also fixture ovs_worker_pods as no longer needed.
    (Defined independently on each device worker instead of calling the network_fixture).
    
    Change-Id: Ibffd2cee6a8d54eab7ce200bf23d371ad51f20fa

--- tests/network/conftest.py
+++ tests/network/conftest.py
@@ -6,14 +6,9 @@ Pytest conftest file for CNV network tests
 
 import pytest
 from ocp_resources.pod import Pod
-from pytest_testconfig import config as py_config
 
 from utilities.infra import ClusterHosts
-from utilities.network import (
-    get_ipv6_address,
-    ip_version_data_from_matrix,
-    network_device,
-)
+from utilities.network import get_ipv6_address, ip_version_data_from_matrix
 
 
 IPV6_STR = "ipv6"
@@ -61,63 +56,6 @@ def vlan_tag_id(index_number):
     if vlan_id > vlan_upper_range:
         raise ValueError(f"VLAN ID: {vlan_id} is out of range")
     return vlan_id
-
-
-@pytest.fixture(scope="session")
-def ovs_worker_pods(schedulable_nodes, admin_client):
-    """
-    Get ovs-* pods, of worker (schedulable) nodes only, from openshift-sdn namespace.
-    """
-
-    def _ovs_pods(namespace, label):
-        # First get all ovs-* pods.
-        return list(Pod.get(admin_client, namespace=namespace, label_selector=label))
-
-    def _worker_pods(pods):
-        # Now filter only the pods that run on worker nodes.
-        worker_pods = []
-        for pod in pods:
-            for node in schedulable_nodes:
-                if node.name == pod.node.name:
-                    worker_pods.append(pod)
-        return worker_pods
-
-    ovs_pods = _ovs_pods(namespace="openshift-sdn", label="app=ovs")
-    worker_pods = _worker_pods(pods=ovs_pods)
-    if not worker_pods:
-        ovs_pods = _ovs_pods(namespace=py_config["hco_namespace"], label="app=ovs-cni")
-        worker_pods = _worker_pods(pods=ovs_pods)
-
-    return worker_pods
-
-
-@pytest.fixture(scope="class")
-def network_interface(
-    request,
-    bridge_device_matrix__class__,
-    index_number,
-    ovs_worker_pods,
-    schedulable_node_ips,
-    multi_nics_nodes,
-    utility_pods,
-    hosts_common_available_ports,
-    schedulable_nodes,
-):
-    params = request.param if hasattr(request, "param") else {}
-    mtu = params.get("mtu")
-    ports = [hosts_common_available_ports[0]] if multi_nics_nodes else []
-    unique_index = next(index_number)
-    interface_name = f"br{unique_index}test"
-    with network_device(
-        interface_type=bridge_device_matrix__class__,
-        nncp_name=f"{interface_name}-nncp",
-        interface_name=interface_name,
-        network_utility_pods=utility_pods,
-        nodes=schedulable_nodes,
-        ports=ports,
-        mtu=mtu,
-    ) as iface:
-        yield iface
 
 
 @pytest.fixture(scope="session")
