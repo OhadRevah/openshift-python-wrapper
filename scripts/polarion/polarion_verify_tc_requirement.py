886ade0084d9c82cff6cd04ea75ab1476939ca9e
Meni Yakove <myakove@redhat.com>
2021-03-31 13:24:31 +0300

    tox: Polarion verify requirement fix.
    Change-Id: I854c3a2e1b140834813a8394163bb092cb475814

--- scripts/polarion/polarion_verify_tc_requirement.py
+++ scripts/polarion/polarion_verify_tc_requirement.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 from pylero.exceptions import PyleroLibException
-from pylero.work_item import Requirement
+from pylero.work_item import Requirement, TestCase
 
 from scripts.polarion.polarion_utils import PROJECT, git_diff_added_removed_lines
 
@@ -12,13 +12,27 @@ LOGGER = logging.getLogger(__name__)
 
 
 def has_verify():
-    added_ids, removed_ids = git_diff_added_removed_lines()
+    added_ids, _ = git_diff_added_removed_lines()
+    missing = []
     for _id in added_ids:
-        try:
-            return Requirement(project_id=PROJECT, work_item_id=_id)
-        except PyleroLibException:
+        has_req = False
+        tc = TestCase(project_id=PROJECT, work_item_id=_id)
+        for link in tc.linked_work_items:
+            try:
+                Requirement(project_id=PROJECT, work_item_id=link.work_item_id)
+                has_req = True
+                break
+            except PyleroLibException:
+                continue
+
+        if not has_req:
             LOGGER.error(f"{_id}: Is missing requirement")
-            sys.exit(1)
+            missing.append(_id)
+
+    if missing:
+        missing_str = "\n".join(missing)
+        LOGGER.error(f"Cases with missing requirement: {missing_str}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
