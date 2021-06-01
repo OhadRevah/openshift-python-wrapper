08a485edd7c8a3ec2893c03216ca5548f125e852
Debarati Basu-Nag <dbasunag@redhat.com>
2021-06-01 08:31:19 -0400

    key metrics
    Change-Id: I3db01841c33b2a262381926f8f6d7b1b92a36037

--- tests/metrics/conftest.py
+++ tests/metrics/conftest.py
@@ -1,10 +1,12 @@
 import logging
+import time
 
 import pytest
 from ocp_resources.resource import ResourceEditor
 
-from tests.metrics.utils import get_mutation_component_value_from_prometheus
-from utilities.virt import Prometheus
+from tests.metrics.utils import create_vms, get_mutation_component_value_from_prometheus
+from utilities.infra import create_ns
+from utilities.virt import Prometheus, running_vm
 
 
 LOGGER = logging.getLogger(__name__)
@@ -47,3 +49,50 @@ def mutation_count_before_change(request, prometheus):
         prometheus=prometheus,
         component_name=component_name,
     )
+
+
+@pytest.fixture(scope="class")
+def unique_namespace(unprivileged_client):
+    """
+    Creates a namespace to be used by key metrics test cases.
+
+    Yields:
+        Namespace object to be used by the tests
+    """
+    namespace_name = f"key-metrics-{time.time()}".replace(".", "-")
+    yield from create_ns(unprivileged_client=unprivileged_client, name=namespace_name)
+
+
+@pytest.fixture(scope="class")
+def vm_list(unique_namespace):
+    """
+    Creates n vms, waits for them all to go to running state and cleans them up at the end
+
+    Args:
+        unique_namespace (Namespace): Creates namespaces to be used by the test
+
+    Yields:
+        list: list of VirtualMachineForTests created
+    """
+    vms_list = create_vms(
+        name_prefix="key-metric-vm", namespace_name=unique_namespace.name
+    )
+    for vm in vms_list:
+        running_vm(vm=vm)
+    yield vms_list
+    for vm in vms_list:
+        vm.clean_up()
+
+
+@pytest.fixture()
+def first_metric_vm(vm_list):
+    """
+    Returns the first vm from the list of created vms
+
+    Args:
+        vm_list (list): list of VirtualMachineForTests created
+
+    Returns:
+        VirtualMachineForTests: a VirtualMachineForTests object
+    """
+    return vm_list[0]
--- tests/metrics/utils.py
+++ tests/metrics/utils.py
@@ -1,11 +1,17 @@
 import logging
+import re
 from collections import defaultdict
 
 from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
 
+from utilities.constants import TIMEOUT_5MIN
+from utilities.virt import VirtualMachineForTests, fedora_vm_body
+
 
 LOGGER = logging.getLogger(__name__)
 KUBEVIRT_CR_ALERT_NAME = "KubevirtHyperconvergedClusterOperatorCRModification"
+CURL_QUERY = "curl -k https://localhost:8443/metrics"
+NUM_TEST_VMS = 3
 
 
 def prometheus_query(prometheus, query):
@@ -71,6 +77,35 @@ def get_changed_mutation_component_value(prometheus, component_name, previous_va
         raise
 
 
+def get_query_result(prometheus, query, timeout=TIMEOUT_5MIN):
+    """
+    Performs Prometheus query, waits for the results to show up, returns the query results
+
+    Args:
+        prometheus (Prometheus Object): Prometheus object.
+        query (str): Prometheus query string (for strings with special characters they need to be parsed by the
+        caller)
+        timeout: (Int): Timeout value in seconds
+
+    Returns:
+        list: List of query results
+    """
+    sampler = TimeoutSampler(
+        wait_timeout=timeout,
+        sleep=5,
+        func=get_metric_by_prometheus_query,
+        prometheus=prometheus,
+        query=query,
+    )
+    try:
+        for sample in sampler:
+            if sample["data"]["result"]:
+                return sample["data"]["result"]
+    except TimeoutExpiredError:
+        LOGGER.error(f'No vm(s) found via prometheus query: "{query}"')
+        raise
+
+
 def get_all_prometheus_alerts(prometheus):
     """This function will give all alerts.
 
@@ -133,3 +168,144 @@ def get_all_hco_cr_modification_alert(prometheus):
         if alert["labels"]["alertname"] == KUBEVIRT_CR_ALERT_NAME:
             present_alerts.append(alert)
     return present_alerts
+
+
+def get_vm_names_from_metric(prometheus, query, timeout=TIMEOUT_5MIN):
+    """
+    Retrieves a list of vm names from a given prometheus api query result
+
+    Args:
+        prometheus (Prometheus Object): Prometheus object.
+        query (str): Prometheus query string (for strings with special characters they need to be parsed by the
+        caller)
+        timeout: (Int): Timeout value in seconds
+
+    Returns:
+        list: List of vm names that are present in the query result
+    """
+    result = get_query_result(prometheus=prometheus, query=query, timeout=timeout)
+    return [name.get("metric").get("name") for name in result]
+
+
+def parse_vm_metric_results(raw_output):
+    """
+    Parse metrics received from virt-handler pod
+
+    Args:
+        raw_output (str): raw metric output received from virt-handler pods
+
+    Returns:
+        dict: Dictionary of parsed output
+    """
+    regex_metrics = r"(?P<metric>\S+)\{(?P<labels>[^\}]+)\}[ ](?P<value>\d+)"
+    metric_results = {}
+    for line in raw_output.splitlines():
+        if line.startswith("# HELP"):
+            metric, description = line[7:].split(" ", 1)
+            metric_results.setdefault(metric, {})["help"] = description
+        elif line.startswith("# TYPE"):
+            metric, metric_type = line[7:].split(" ", 1)
+            metric_results.setdefault(metric, {})["type"] = metric_type
+        elif re.match(regex_metrics, line):
+            metric_instance_dict = re.match(regex_metrics, line).groupdict()
+            metric_instance_dict["labeldict"] = {
+                val[0]: val[-1]
+                for val in [
+                    label.partition("=")
+                    for label in metric_instance_dict["labels"].split(",")
+                ]
+            }
+            metric_results.setdefault(metric_instance_dict["metric"], {}).setdefault(
+                "results", []
+            ).append(metric_instance_dict)
+        else:
+            metric, metric_type = line.split(" ", 1)
+            metric_results.setdefault(metric, {})["type"] = metric_type
+    return metric_results
+
+
+def assert_vm_metric_virt_handler_pod(query, vm):
+    """
+    Get vm metric information from virt-handler pod
+
+    Args:
+        query (str): Prometheus query string
+        vm (VirtualMachineForTests): A VirtualMachineForTests
+
+    """
+    pod = vm.vmi.virt_handler_pod
+    output = parse_vm_metric_results(
+        raw_output=pod.execute(command=["bash", "-c", f"{CURL_QUERY}"])
+    )
+    assert (
+        output
+    ), f'No query output found from virt-handler pod "{pod.name}" for query: "{CURL_QUERY}"'
+    metrics_list = []
+    if query in output:
+        metrics_list = [
+            result["labeldict"]
+            for result in output[query]["results"]
+            if "labeldict" in result and vm.name in result["labeldict"]["name"]
+        ]
+    assert metrics_list, (
+        f'Virt-handler pod query:"{CURL_QUERY}" did not return any vm metric information for vm: {vm.name} '
+        f"from virt-handler pod: {pod.name}. "
+    )
+    assert_validate_vm_metric(vm=vm, metrics_list=metrics_list)
+
+
+def assert_validate_vm_metric(vm, metrics_list):
+    """
+    Validate vm metric information fetched from virt-handler pod
+
+    Args:
+        vm (VirtualMachineForTests): A VirtualMachineForTests
+        metrics_list (list): List of metrics entries collected from associated Virt-handler pod
+
+    """
+    expected_values = {
+        "kubernetes_vmi_label_kubevirt_io_nodeName": vm.vmi.node.name,
+        "namespace": vm.namespace,
+        "node": vm.vmi.node.name,
+    }
+    LOGGER.info(
+        f"Virt-handler pod metrics associated with vm: {vm.name} are: {metrics_list}"
+    )
+    metric_data_mismatch = [
+        entity
+        for key in expected_values
+        for entity in metrics_list
+        if not entity.get(key, None) or expected_values[key] not in entity[key]
+    ]
+
+    assert not metric_data_mismatch, (
+        f"Vm metric validation via virt-handler pod {vm.vmi.virt_handler_pod}"
+        f" failed: {metric_data_mismatch}"
+    )
+
+
+def create_vms(name_prefix, namespace_name, vm_count=NUM_TEST_VMS):
+    """
+     Create n number of fedora vms.
+
+     Args:
+         name_prefix (str): prefix to be used to name virtualmachines
+         namespace_name (str): Namespace to be used for vm creation
+         vm_count (int): Number of vms to be created
+
+    Returns:
+        list: List of VirtualMachineForTests
+    """
+    vms_list = []
+    for idx in range(vm_count):
+        vm_name = f"{name_prefix}-{idx}"
+        with VirtualMachineForTests(
+            name=vm_name,
+            namespace=namespace_name,
+            body=fedora_vm_body(name=vm_name),
+            teardown=False,
+            running=True,
+            ssh=True,
+        ) as vm:
+            vms_list.append(vm)
+    return vms_list
