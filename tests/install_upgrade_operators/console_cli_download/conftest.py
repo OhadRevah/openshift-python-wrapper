31e6ffe15c077197157ee892f0bd68b497dcf1a6
Issac Besso <ibesso@redhat.com>
2021-08-31 21:18:19 +0300

    add disconnected virtctl tests
    new tests covering validating, downloading and extracting
    virtctl archives URLs
    
    Change-Id: I8fb97a4807f1b6311ed9485c78885c2933a7a464

---
+++ tests/install_upgrade_operators/console_cli_download/conftest.py
@@ -0,0 +1,102 @@
+import os.path
+import re
+import shutil
+
+import pytest
+from ocp_resources.console_cli_download import ConsoleCLIDownload
+from ocp_resources.route import Route
+
+from tests.install_upgrade_operators.console_cli_download.utils import (
+    download_and_extract_virtctl_from_cluster,
+)
+from utilities.virt import run_command
+
+
+@pytest.fixture()
+def console_cli_downloads_spec_links(admin_client):
+    """
+    Get console cli downloads spec links
+
+    Returns:
+        ConsoleCLIDownload instance.spec.links
+    """
+    console_cli_download_resource_content = ConsoleCLIDownload(
+        name="virtctl-clidownloads-kubevirt-hyperconverged", client=admin_client
+    )
+    assert console_cli_download_resource_content.exists
+    return console_cli_download_resource_content.instance.spec.links
+
+
+@pytest.fixture()
+def all_virtctl_urls(console_cli_downloads_spec_links):
+    """This fixture returns URLs for the various OSs to download virtctl"""
+    all_virtctl_urls = [entry["href"] for entry in console_cli_downloads_spec_links]
+    assert all_virtctl_urls, (
+        "No URL entries found in the resource: "
+        f"console_cli_download_resource_content={console_cli_downloads_spec_links}"
+    )
+    return all_virtctl_urls
+
+
+@pytest.fixture()
+def internal_fqdn(admin_client, hco_namespace):
+    """
+    This fixture returns the prefix url for the cluster, which is used to identify if certain links are routed or
+    served from within the cluster
+    """
+    cluster_route = Route(
+        name="hyperconverged-cluster-cli-download", namespace=hco_namespace.name
+    )
+    assert cluster_route.exists
+    return cluster_route.instance.spec.host
+
+
+@pytest.fixture()
+def non_internal_fqdns(all_virtctl_urls, internal_fqdn):
+    """
+    Get URLs containing FQDN that is not matching the cluster's route
+
+    Returns:
+        list: list of all non-internal FQDNs
+    """
+    return [
+        virtctl_url
+        for virtctl_url in all_virtctl_urls
+        if f"//{internal_fqdn}" not in virtctl_url
+    ]
+
+
+@pytest.fixture()
+def downloaded_and_extracted_virtctl_binary_for_os(request, all_virtctl_urls, tmpdir):
+    """
+    This fixture downloads the virtctl archive from the provided OS, and extracts it to a temporary dir
+    """
+    url_for_os = [url for url in all_virtctl_urls if request.param in url][0]
+    extracted_files = download_and_extract_virtctl_from_cluster(
+        tmpdir=tmpdir, virtctl_url=url_for_os
+    )
+    assert (
+        len(extracted_files) == 1
+    ), f"Only a single file expected in archive: extracted_files={extracted_files}"
+    virtctl_binary_full_path = extracted_files[0]
+    yield virtctl_binary_full_path
+    shutil.rmtree(os.path.dirname(virtctl_binary_full_path))
+
+
+@pytest.fixture()
+def virtctl_client_and_server_versions(downloaded_and_extracted_virtctl_binary_for_os):
+    """
+    Get the client and server versions from the virtctl version command
+
+    Returns:
+        tuple: re results, virtctl version command output (for triaging)
+    """
+    _, virtctl_output, _ = run_command(
+        command=[f"{downloaded_and_extracted_virtctl_binary_for_os} version"],
+        shell=True,
+    )
+    client_and_server_versions = re.findall(
+        r'(?:Client|Server).*version.*{GitVersion:"v(.*)",\s+GitCommit',
+        virtctl_output,
+    )
+    return client_and_server_versions, virtctl_output
---
+++ tests/install_upgrade_operators/console_cli_download/test_disconnected_virtctl.py
@@ -0,0 +1,61 @@
+import os.path
+
+import pytest
+
+
+class TestDisconnectedVirtctlDownload:
+    @pytest.mark.parametrize(
+        "downloaded_and_extracted_virtctl_binary_for_os",
+        [
+            pytest.param(
+                "win",
+                marks=(pytest.mark.polarion("CNV-6914"),),
+                id="test_download_virtcli_binary_win",
+            ),
+            pytest.param(
+                "mac",
+                marks=(pytest.mark.polarion("CNV-6954"),),
+                id="test_download_virtcli_binary_mac",
+            ),
+        ],
+        indirect=True,
+    )
+    def test_download_virtcli_binary(
+        self,
+        downloaded_and_extracted_virtctl_binary_for_os,
+    ):
+        assert os.path.exists(downloaded_and_extracted_virtctl_binary_for_os)
+
+
+class TestDisconnectedVirtctlDownloadAndExecute:
+    @pytest.mark.parametrize(
+        "downloaded_and_extracted_virtctl_binary_for_os",
+        [
+            pytest.param(
+                "linux",
+                marks=(pytest.mark.polarion("CNV-6913"),),
+                id="test_download_virtcli_binary_linux",
+            ),
+        ],
+        indirect=True,
+    )
+    def test_download_and_execute_virtcli_binary_linux(
+        self, virtctl_client_and_server_versions
+    ):
+        client_and_server_versions, virtctl_output = virtctl_client_and_server_versions
+        assert (
+            len(client_and_server_versions) == 2
+        ), f"regex did not produced the expected number of matches: virtctl_output={virtctl_output}"
+        assert len(set(client_and_server_versions)) == 1, (
+            "Compare error: virtctl client and server versions are not identical: "
+            f"client_and_server_versions={client_and_server_versions}"
+        )
+
+
+class TestDisconnectedVirtctlAllLinksInternal:
+    @pytest.mark.polarion("CNV-6915")
+    def test_all_links_internal(self, all_virtctl_urls, non_internal_fqdns):
+        assert not non_internal_fqdns, (
+            "Found virtctl URLs that do not point to the cluster internally: "
+            f"violating_fqdns={non_internal_fqdns} all_virtctl_urls={all_virtctl_urls}"
+        )
