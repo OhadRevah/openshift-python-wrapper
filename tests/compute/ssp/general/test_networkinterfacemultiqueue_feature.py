559c67cc22826e49b80494408fb25f430eec07ca
rnetser <rnetser@redhat.com>
2021-10-11 12:49:36 +0300

    test_networkinterfacemultiqueue_feature - fix ordering
    Change-Id: I8feabbe1c946ce46a9c8be1bf05dc3b788ce8b1c

--- tests/compute/ssp/general/test_networkinterfacemultiqueue_feature.py
+++ tests/compute/ssp/general/test_networkinterfacemultiqueue_feature.py
@@ -5,6 +5,7 @@ Test networkInterfaceMultiqueue feature with cpu core/socket/thread combinations
 import pytest
 from ocp_resources.resource import ResourceEditor
 from ocp_resources.template import Template
+from pytest_testconfig import py_config
 
 from tests.os_params import (
     RHEL_LATEST,
@@ -14,7 +15,7 @@ from tests.os_params import (
     WINDOWS_LATEST_OS,
 )
 from utilities.constants import TIMEOUT_2MIN
-from utilities.virt import wait_for_vm_interfaces
+from utilities.virt import vm_instance_from_template, wait_for_vm_interfaces
 
 
 pytestmark = pytest.mark.post_upgrade
@@ -69,15 +70,31 @@ def update_validate_cpu_in_vm(
     validate_vm_cpu_spec(vm=vm, cores=cores, sockets=sockets, threads=threads)
 
 
+@pytest.fixture(scope="class")
+def network_interface_multiqueue_vm(
+    request,
+    unprivileged_client,
+    namespace,
+    golden_image_data_volume_scope_class,
+):
+    with vm_instance_from_template(
+        request=request,
+        unprivileged_client=unprivileged_client,
+        namespace=namespace,
+        data_volume=golden_image_data_volume_scope_class,
+    ) as multiqueue_vm:
+        yield multiqueue_vm
+
+
 @pytest.mark.parametrize(
-    "golden_image_data_volume_multi_storage_scope_class,"
-    "golden_image_vm_instance_from_template_multi_storage_scope_class",
+    "golden_image_data_volume_scope_class, network_interface_multiqueue_vm",
     [
         (
             {
                 "dv_name": RHEL_LATEST_OS,
                 "image": RHEL_LATEST["image_path"],
                 "dv_size": RHEL_LATEST["dv_size"],
+                "storage_class": py_config["default_storage_class"],
             },
             {
                 "vm_name": RHEL_LATEST_OS,
@@ -92,63 +109,61 @@ def update_validate_cpu_in_vm(
     indirect=True,
 )
 @pytest.mark.sno
-@pytest.mark.usefixtures("golden_image_data_volume_multi_storage_scope_class")
+@pytest.mark.usefixtures("golden_image_data_volume_scope_class")
 class TestLatestRHEL:
     """
     Test networkInterfaceMultiqueue on latest RHEL with different cpu core/socket/thread combinations.
     """
 
-    @pytest.mark.order("first")
+    @pytest.mark.dependency(name="rhel_default_cpu_values")
     @pytest.mark.polarion("CNV-3221")
     def test_default_cpu_values(
         self,
-        golden_image_vm_instance_from_template_multi_storage_scope_class,
+        network_interface_multiqueue_vm,
     ):
-        golden_image_vm_instance_from_template_multi_storage_scope_class.ssh_exec.executor().is_connective(  # noqa: E501
+        network_interface_multiqueue_vm.ssh_exec.executor().is_connective(  # noqa: E501
             tcp_timeout=TIMEOUT_2MIN
         )
 
+    @pytest.mark.order(after="TestLatestRHEL::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["rhel_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
-    def test_feature_disabled(
-        self, golden_image_vm_instance_from_template_multi_storage_scope_class
-    ):
+    def test_feature_disabled(self, network_interface_multiqueue_vm):
         update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class,
+            vm=network_interface_multiqueue_vm,
             network_multiqueue=False,
         )
 
+    @pytest.mark.order(after="TestLatestRHEL::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["rhel_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
-    def test_four_cores(
-        self, golden_image_vm_instance_from_template_multi_storage_scope_class
-    ):
-        update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class, cores=4
-        )
+    def test_four_cores(self, network_interface_multiqueue_vm):
+        update_validate_cpu_in_vm(vm=network_interface_multiqueue_vm, cores=4)
 
+    @pytest.mark.order(after="TestLatestRHEL::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["rhel_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
-    def test_four_sockets(
-        self, golden_image_vm_instance_from_template_multi_storage_scope_class
-    ):
+    def test_four_sockets(self, network_interface_multiqueue_vm):
         update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class,
+            vm=network_interface_multiqueue_vm,
             sockets=4,
         )
 
+    @pytest.mark.order(after="TestLatestRHEL::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["rhel_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
-    def test_four_threads(
-        self, golden_image_vm_instance_from_template_multi_storage_scope_class
-    ):
+    def test_four_threads(self, network_interface_multiqueue_vm):
         update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class,
+            vm=network_interface_multiqueue_vm,
             threads=4,
         )
 
+    @pytest.mark.order(after="TestLatestRHEL::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["rhel_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
-    def test_two_cores_two_sockets_two_threads(
-        self, golden_image_vm_instance_from_template_multi_storage_scope_class
-    ):
+    def test_two_cores_two_sockets_two_threads(self, network_interface_multiqueue_vm):
         update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class,
+            vm=network_interface_multiqueue_vm,
             cores=4,
             sockets=2,
             threads=2,
@@ -156,14 +171,14 @@ class TestLatestRHEL:
 
 
 @pytest.mark.parametrize(
-    "golden_image_data_volume_multi_storage_scope_class,"
-    "golden_image_vm_instance_from_template_multi_storage_scope_class",
+    "golden_image_data_volume_scope_class, network_interface_multiqueue_vm",
     [
         (
             {
                 "dv_name": WINDOWS_LATEST_OS,
                 "image": WINDOWS_LATEST["image_path"],
                 "dv_size": WINDOWS_LATEST["dv_size"],
+                "storage_class": py_config["default_storage_class"],
             },
             {
                 "vm_name": WINDOWS_LATEST_OS,
@@ -175,29 +190,31 @@ class TestLatestRHEL:
     ],
     indirect=True,
 )
-@pytest.mark.usefixtures("golden_image_data_volume_multi_storage_scope_class")
+@pytest.mark.usefixtures("golden_image_data_volume_scope_class")
 class TestLatestWindows:
     """
     Test networkInterfaceMultiqueue on latest Windows with different cpu core/socket/thread combinations.
     """
 
-    @pytest.mark.order("first")
+    @pytest.mark.dependency(name="windows_default_cpu_values")
     @pytest.mark.polarion("CNV-3221")
     def test_default_cpu_values(
         self,
-        golden_image_vm_instance_from_template_multi_storage_scope_class,
+        network_interface_multiqueue_vm,
     ):
-        golden_image_vm_instance_from_template_multi_storage_scope_class.ssh_exec.executor().is_connective(  # noqa: E501
+        network_interface_multiqueue_vm.ssh_exec.executor().is_connective(  # noqa: E501
             tcp_timeout=TIMEOUT_2MIN
         )
 
+    @pytest.mark.order(after="TestLatestWindows::test_default_cpu_values")
+    @pytest.mark.dependency(depends=["windows_default_cpu_values"])
     @pytest.mark.polarion("CNV-3221")
     def test_four_cores_two_sockets_two_threads(
         self,
-        golden_image_vm_instance_from_template_multi_storage_scope_class,
+        network_interface_multiqueue_vm,
     ):
         update_validate_cpu_in_vm(
-            vm=golden_image_vm_instance_from_template_multi_storage_scope_class,
+            vm=network_interface_multiqueue_vm,
             cores=4,
             sockets=2,
             threads=2,
