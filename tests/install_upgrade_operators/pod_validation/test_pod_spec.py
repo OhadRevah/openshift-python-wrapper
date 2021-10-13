a35117bcea1d3f15f6e99060b1e2fabe8290dd32
Debarati Basu-Nag <dbasunag@redhat.com>
2021-10-13 14:58:19 -0400

    new tests validating resources.requests for all cnv pods containers
    Change-Id: If7fbb2220353205bb62037cf54ed79d8e3c8f68d

--- tests/install_upgrade_operators/pod_validation/test_pod_spec.py
+++ tests/install_upgrade_operators/pod_validation/test_pod_spec.py
@@ -1,7 +1,9 @@
 import pytest
 
 from tests.install_upgrade_operators.pod_validation.utils import (
+    validate_cnv_pod_cpu_min_value,
     validate_cnv_pods_priority_class_name_exists,
+    validate_cnv_pods_resource_request,
     validate_priority_class_value,
 )
 
@@ -14,3 +16,44 @@ def test_pods_priority_class_name(cnv_pods):
 @pytest.mark.polarion("CNV-7262")
 def test_pods_priority_class_value(cnv_pods):
     validate_priority_class_value(cnv_pods=cnv_pods)
+
+
+@pytest.mark.parametrize(
+    "request_field",
+    [
+        pytest.param(
+            "cpu",
+            marks=(pytest.mark.polarion("CNV-7306")),
+            id="test_pods_resource_request_cpu",
+        ),
+        pytest.param(
+            "memory",
+            marks=(pytest.mark.polarion("CNV-7307")),
+            id="test_pods_resource_request_memory",
+        ),
+    ],
+)
+def test_pods_resource_request_cpu(cnv_pods, request_field):
+    validate_cnv_pods_resource_request(cnv_pods=cnv_pods, request_field=request_field)
+
+
+@pytest.mark.parametrize(
+    "cpu_min_value",
+    [
+        pytest.param(
+            5,
+            marks=(pytest.mark.polarion("CNV-7341")),
+            id="test_pods_resource_request_cpu",
+        ),
+    ],
+)
+def test_pods_resource_request_cpu_value(cnv_pods, cpu_min_value):
+    """Test validates that resources.requests.cpu value for all cnv pods meet minimum threshold requirement"""
+    cpu_error = {}
+    for pod in cnv_pods:
+        invalid_cpu = validate_cnv_pod_cpu_min_value(
+            cnv_pod=pod, cpu_min_value=cpu_min_value
+        )
+        if invalid_cpu:
+            cpu_error[pod.name] = invalid_cpu
+    assert not cpu_error, f"For following pods invalid cpu values found: {cpu_error}"
--- tests/install_upgrade_operators/pod_validation/utils.py
+++ tests/install_upgrade_operators/pod_validation/utils.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from utilities.infra import BUG_STATUS_CLOSED, get_bug_status
 
@@ -13,25 +14,21 @@ VALID_PRIORITY_CLASS = [
 LOGGER = logging.getLogger(__name__)
 
 
-# TODO: remove this function when all the pod related bugs for missing spec.priorityClassName has been addressed
-def get_cnv_pod_names_with_open_bugs():
-    """
-    For multiple pods currently we have missing spec.priorityClassName, this would cause the test validating existence
-    of spec.priorityClassName for all cnv pods, to fail. To ensure test continues to run while those bugs are
-    getting fixed, this workaround is being added. This utility function will be removed when all the associated bugs
-    have been fixed
-
-    Returns:
-        list: names of pod types
-    """
-    pods_with_bugs = {
-        "cdi-operator": 2008949,
-        "hostpath-provisioner-operator": 2008949,
-        "hyperconverged-cluster-cli-download": 2008938,
-        "node-maintenance-operator": 2008960,
-        "ssp-operator": 2008975,
-        "virt-template-validator": 2008975,
-    }
+# TODO: remove this function when all the pod related bugs for missing spec.priorityClassName/resources.requests has
+# been addressed
+def get_cnv_pod_names_with_open_bugs(field_name):
+    if field_name == "priorityClass":
+        pods_with_bugs = {
+            "hostpath-provisioner": 2028209,
+            "hostpath-provisioner-csi": 2028209,
+            "node-maintenance-operator-controller-manager": 2008960,
+        }
+    elif field_name == "resources":
+        pods_with_bugs = {
+            "hostpath-provisioner": 2015327,
+        }
+    else:
+        raise AssertionError(f"Invalid field_name {field_name}")
     return [
         pod_type
         for pod_type, bug_id in pods_with_bugs.items()
@@ -43,16 +40,9 @@ def get_cnv_pod_names_with_open_bugs():
 
 
 def validate_cnv_pods_priority_class_name_exists(cnv_pods):
-    """
-    Validates spec.priorityClassName is present for all cnv pods
-
-    Args:
-        cnv_pods(list): list of cnv pods
-
-    Raises:
-        AssertionError: if pods without spec.priorityClassName is found.
-    """
-    cnv_pod_names_with_open_bugs = get_cnv_pod_names_with_open_bugs()
+    cnv_pod_names_with_open_bugs = get_cnv_pod_names_with_open_bugs(
+        field_name="priorityClass"
+    )
     LOGGER.info(
         f"Following pods has associated bugzilla open: {cnv_pod_names_with_open_bugs}"
     )
@@ -70,15 +60,6 @@ def validate_cnv_pods_priority_class_name_exists(cnv_pods):
 
 
 def validate_priority_class_value(cnv_pods):
-    """
-    Validates spec.priorityClassName contains valid values for all cnv pods
-
-    Args:
-        cnv_pods(list): list of cnv pods
-
-    Raises:
-        AssertionError: if pods with invalid values for spec.priorityClassName is found.
-    """
     pods_invalid_priority_class = {
         pod.name: pod.instance.spec.priorityClassName
         for pod in cnv_pods
@@ -89,3 +70,53 @@ def validate_priority_class_value(cnv_pods):
         f"For the following pods, unexpected priority class found"
         f": {pods_invalid_priority_class}"
     )
+
+
+def validate_cnv_pod_resource_request(cnv_pod, request_field):
+    containers = cnv_pod.instance.spec.containers
+
+    missing_field_values = [
+        container["name"]
+        for container in containers
+        if not container.get("resources", {}).get("requests", {}).get(request_field)
+    ]
+    return missing_field_values
+
+
+def validate_cnv_pod_cpu_min_value(cnv_pod, cpu_min_value):
+    containers = cnv_pod.instance.spec.containers
+    cpu_values = {
+        container["name"]: container.get("resources", {}).get("requests", {}).get("cpu")
+        for container in containers
+    }
+    LOGGER.info(f"For {cnv_pod.name} cpu_values: {cpu_values}")
+    cpu_value_pattern = re.compile(r"^\d+")
+    # Get the pods for which resources.requests.cpu value does not meet minimum threshold requirement
+    invalid_cpus = {
+        key: value
+        for key, value in cpu_values.items()
+        if value and int(cpu_value_pattern.findall(value)[0]) < cpu_min_value
+    }
+    return invalid_cpus
+
+
+def validate_cnv_pods_resource_request(cnv_pods, request_field):
+    cnv_pod_names_with_open_bugs = get_cnv_pod_names_with_open_bugs(
+        field_name="resources"
+    )
+    errors = {}
+    for pod in cnv_pods:
+        value = validate_cnv_pod_resource_request(
+            cnv_pod=pod, request_field=request_field
+        )
+        if value:
+            LOGGER.error(
+                f"For {pod.name}, resources.requests.{request_field} is missing."
+            )
+            if not pod.name.startswith(tuple(cnv_pod_names_with_open_bugs)):
+                LOGGER.info(f"For pod {pod.name}, missing {request_field}")
+                errors[pod.name] = value
+
+    assert (
+        not errors
+    ), f"For following pods resource.requests fields were missing: {errors}"
