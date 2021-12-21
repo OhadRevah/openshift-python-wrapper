e092e7520f83df8d599daca5342a128dbc9b04c2
Denys Shchedrivyi <dshchedr@redhat.com>
2021-12-21 15:54:44 -0500

    Add SSP Alerts test: SSPFailingToReconcile
    Change-Id: I0f4a0a43600bd3ae7f75f2970be09e164a97eac9

--- tests/compute/ssp/alerts/test_ssp_alerts.py
+++ tests/compute/ssp/alerts/test_ssp_alerts.py
@@ -1,8 +1,9 @@
 import pytest
+from ocp_resources.deployment import Deployment
 
 from tests.compute.utils import verify_no_listed_alerts_on_cluster
 from utilities.constants import SSP_OPERATOR, VIRT_TEMPLATE_VALIDATOR
-from utilities.infra import update_custom_resource
+from utilities.infra import get_pod_by_name_prefix, update_custom_resource
 
 
 SSP_DOWN = "SSPDown"
@@ -18,6 +19,15 @@ SSP_ALERTS_LIST = [
     SSP_HIGH_RATE_REJECTED_VMS,
     SSP_FAILING_TO_RECONCILE,
 ]
+
+
+def verify_ssp_pod_is_running(dyn_client, hco_namespace):
+    pod = get_pod_by_name_prefix(
+        dyn_client=dyn_client,
+        pod_prefix=SSP_OPERATOR,
+        namespace=hco_namespace.name,
+    )
+    pod.wait_for_status(status=pod.Status.RUNNING)
 
 
 @pytest.fixture()
@@ -43,6 +53,30 @@ def alert_not_firing_before_running_test(prometheus, request):
             f"Alert {alert} should not be in Firing or in Pending state on a cluster before running test"
         )
     return alert
+
+
+@pytest.fixture()
+def template_validator_finalizer(hco_namespace):
+    deployment = Deployment(name=VIRT_TEMPLATE_VALIDATOR, namespace=hco_namespace.name)
+    with update_custom_resource(
+        patch={
+            deployment: {
+                "metadata": {"finalizers": ["ssp.kubernetes.io/temporary-finalizer"]}
+            }
+        }
+    ):
+        yield
+
+
+@pytest.fixture()
+def deleted_ssp_operator_pod(admin_client, hco_namespace):
+    get_pod_by_name_prefix(
+        dyn_client=admin_client,
+        pod_prefix=SSP_OPERATOR,
+        namespace=hco_namespace.name,
+    ).delete(wait=True)
+    yield
+    verify_ssp_pod_is_running(dyn_client=admin_client, hco_namespace=hco_namespace)
 
 
 class TestSSPAlerts:
@@ -78,5 +112,26 @@ class TestSSPAlerts:
         alert_not_firing_before_running_test,
         paused_ssp_operator,
         scaled_deployment,
+    ):
+        prometheus.alert_sampler(alert=alert_not_firing_before_running_test)
+
+    @pytest.mark.order(after="test_no_ssp_alerts_on_healthy_cluster")
+    @pytest.mark.parametrize(
+        "alert_not_firing_before_running_test",
+        [
+            pytest.param(
+                SSP_FAILING_TO_RECONCILE,
+                marks=pytest.mark.polarion("CNV-7711"),
+            ),
+        ],
+        indirect=True,
+    )
+    def test_alert_ssp_failing_to_reconcile(
+        self,
+        prometheus,
+        alert_not_firing_before_running_test,
+        paused_ssp_operator,
+        template_validator_finalizer,
+        deleted_ssp_operator_pod,
     ):
         prometheus.alert_sampler(alert=alert_not_firing_before_running_test)
