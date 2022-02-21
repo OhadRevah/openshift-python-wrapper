8702a4066c2b437a47ca465c4126b5c68bfc9794
Debarati Basu-Nag <dbasunag@redhat.com>
2022-02-21 16:23:53 -0500

    created separate fixture - based on review of cherry-pick
    Change-Id: I97320e1292f27f71779b2fb2553b8a20dd2f1d6c

--- tests/install_upgrade_operators/csv/test_immutable_image_using_sha.py
+++ tests/install_upgrade_operators/csv/test_immutable_image_using_sha.py
@@ -6,16 +6,23 @@ from tests.install_upgrade_operators.csv.utils import get_package_manifest_image
 pytestmark = pytest.mark.sno
 
 
+@pytest.fixture()
+def hco_package_stable_channel_images(admin_client):
+    """
+    Get a list of all images in the kubevirt-hyperconverged package on the stable channel
+    """
+    return get_package_manifest_images(admin_client=admin_client)
+
+
 @pytest.mark.polarion("CNV-4751")
-def test_immutable_image_using_sha(skip_if_nightly_channel, admin_client):
+def test_immutable_image_using_sha(
+    skip_if_nightly_channel, hco_package_stable_channel_images
+):
     """
     check all images of the stable channel on the kubevirt-hyperconverged Package Manifest.
     make sure all images have SHA256 in their string (this indicates they are immutable)
     """
     # verify all images contain "sha256" in their name. on failure this will be a list of images without "sha256"
     assert not list(
-        filter(
-            lambda image: "sha256" not in image,
-            get_package_manifest_images(admin_client=admin_client),
-        )
+        filter(lambda image: "sha256" not in image, hco_package_stable_channel_images)
     )
