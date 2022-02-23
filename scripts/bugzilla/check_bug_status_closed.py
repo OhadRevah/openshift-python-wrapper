19b51caeac0a0313fca807a3cd47dc5f31beddef
Meni Yakove <myakove@redhat.com>
2022-02-23 21:17:25 +0200

    Find also bugs used by 'is_bug_open'
    To skip check for bug url (only) add <skip-bug-check> after the bug
    only in comments
    
    example:
      https://bugzilla.redhat.com/show_bug.cgi\?id=123 <skip-bug-check>
    
    Change-Id: Ia71906f9a823da0c6553f791349a39c76e627d7d

--- scripts/bugzilla/check_bug_status_closed.py
+++ scripts/bugzilla/check_bug_status_closed.py
@@ -2,6 +2,7 @@ import os
 import re
 from configparser import ConfigParser
 from pathlib import Path
+from xmlrpc.client import Fault
 
 import bugzilla
 
@@ -22,34 +23,76 @@ def get_connection_params(conf_file_name):
     return params_dict
 
 
-def get_bug_status(bug):
+def get_bug_status(bug_id):
     bugzilla_connection_params = get_connection_params(conf_file_name="bugzilla.cfg")
     bzapi = bugzilla.Bugzilla(
         url=bugzilla_connection_params["bugzilla_url"],
         user=bugzilla_connection_params["bugzilla_username"],
         api_key=bugzilla_connection_params["bugzilla_api_key"],
     )
-    return bzapi.getbug(objid=bug).status
+    try:
+        return bzapi.getbug(objid=bug_id).status
+    except Fault:
+        print(f"Failed to get bug status for bug {bug_id}")
 
 
-if __name__ == "__main__":
+def all_python_files():
+    exclude_dirs = [".tox"]
+    for root, _, files in os.walk(os.path.abspath(os.curdir)):
+        if [_dir for _dir in exclude_dirs if _dir in root]:
+            continue
+
+        for filename in files:
+            if filename.endswith(".py") and filename != os.path.split(__file__)[-1]:
+                yield os.path.join(root, filename)
+
+
+def get_all_bugs_from_file(file_content):
+    """
+    Try to find all bugs in the file.
+    Looking for the following patterns:
+    - bug_id=12345  # call in is_bug_open
+    - bug_id = 12345  # when bug is constant
+    - https://bugzilla.redhat.com/show_bug.cgi?id=12345  # when bug is in a link in comments
+    - pytest.mark.bugzilla(12345)  # when bug is in a marker
+
+    Args:
+        file_content (str): The content of the file.
+
+    Returns:
+        list: A list of bugs.
+    """
+    _pytest_bugzilla_marker_bugs = re.findall(
+        r"pytest.mark.bugzilla.*?(\d{7,})", file_content, re.DOTALL
+    )
+    _is_bug_open_bugs = re.findall(r"(?:bug_id=|.*bug.* = )(\d{7,})", file_content)
+    _bugzilla_url_bugs = re.findall(
+        r"https://bugzilla.redhat.com/show_bug.cgi\?id=(\d{7,}(?! <skip-bug-check>))",
+        file_content,
+    )
+    return set(_pytest_bugzilla_marker_bugs + _is_bug_open_bugs + _bugzilla_url_bugs)
+
+
+def main():
     closed_bugs = {}
-    for root, dirs, files in os.walk(os.path.abspath(os.curdir)):
-        for file in files:
-            if not file.endswith(".py") or file in __file__:
-                continue
-
-            with open(os.path.join(root, file), "r") as fd:
-                data = fd.read()
-                _bugs = re.findall(r"@pytest.mark.bugzilla(.*\n.*|.*)", data)
-                for _bug in _bugs:
-                    bug_id = re.findall(r"\d+", _bug)
-                    bug_id = bug_id[0] if bug_id else None
-                    if bug_id and get_bug_status(bug=bug_id) in BUG_STATUS_CLOSED:
-                        closed_bugs.setdefault(file, []).append(bug_id)
+    for filename in all_python_files():
+        filename_for_key = re.findall(r"cnv-tests/.*", filename)[0]
+        with open(filename, "r") as fd:
+            for _bug in get_all_bugs_from_file(file_content=fd.read()):
+                bug_status = get_bug_status(bug_id=_bug)
+                if bug_status in BUG_STATUS_CLOSED:
+                    closed_bugs.setdefault(filename_for_key, []).append(
+                        f"{_bug} [{bug_status}]"
+                    )
 
     if closed_bugs:
-        print("The following bugs are closed and needs to be removed:")
+        print(
+            f"The following bugs are closed and needs to be removed ({len(closed_bugs)}):"
+        )
         for key, value in closed_bugs.items():
             print(f"    {key}:  {' '.join(list(set(value)))}")
         exit(1)
+
+
+if __name__ == "__main__":
+    main()
