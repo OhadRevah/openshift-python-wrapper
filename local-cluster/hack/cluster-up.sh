9300810a34395e7e49d3efe47ce2445a337c975c
Meni Yakove <myakove@redhat.com>
2022-03-03 17:52:16 +0200

    Fix typo in checkout
    Change-Id: I275751f54136ee44b667b3b8c4e41765a153d760

--- local-cluster/hack/cluster-up.sh
+++ local-cluster/hack/cluster-up.sh
@@ -6,7 +6,7 @@ hco_dir=local-cluster/_hco
 rm -rf $hco_dir
 git clone https://github.com/kubevirt/hyperconverged-cluster-operator.git $hco_dir
 pushd $hco_dir
-git chechout release-1.6
+git checkout release-1.6
 make cluster-up
 make cluster-sync
 popd
