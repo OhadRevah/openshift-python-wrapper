d303d2d6de5c9285090d18ecbd49b97b481e9080
Anat Wax <awax@redhat.com>
2022-03-10 15:31:05 +0200

    Remove skip on BM nodes for bond tests
    Currently we have 4 BM clusters with bond support.
    
    Change-Id: I40de5ee99828373aef1e5a723320153147ea8366

--- tests/network/bond/test_over_linux_bond_on_default_interface.py
+++ tests/network/bond/test_over_linux_bond_on_default_interface.py
@@ -97,7 +97,6 @@ def lbodi_pod_with_bond(utility_pods, lbodi_bond):
             return pod
 
 
-@pytest.mark.usefixtures("skip_if_workers_bms")
 @pytest.mark.destructive
 class TestBondConnectivityWithNodesDefaultInterface:
     @pytest.mark.polarion("CNV-3432")
