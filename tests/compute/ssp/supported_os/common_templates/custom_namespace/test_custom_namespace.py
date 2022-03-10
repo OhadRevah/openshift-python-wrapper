0558cc5d745960f3d366c101f1218d2b695157a4
Sarah Bennert <sbennert@redhat.com>
2022-03-10 08:03:54 -0500

    Custom template namespace (additional tests)
    Automation for Polarion:
      - CNV-8142 (verify custom namespace template reconciliation upon
        deletion)
      - CNV-8164 (verify custom namespace template edit reconciliation)
      - CNV-8163 (create vm using custom namespace template)
      - CNV-8152 (verify default namespace template reconciliation upon
        deletion post-revert)
      - CNV-8189 (verify default template namespace edit reconciliation
        post-revert)
      - CNV-8190 (verify no edit reconciliation in custom template
        namespace post-revert)
    
    Change-Id: I54fe06cfa2ee52de21d8f6d5d53f1f67598b24da
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/319438
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Tested-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/compute/ssp/supported_os/common_templates/custom_namespace/test_custom_namespace.py
+++ tests/compute/ssp/supported_os/common_templates/custom_namespace/test_custom_namespace.py
@@ -1,13 +1,21 @@
+import logging
+
 import pytest
+from kubernetes.client import ApiException
 from ocp_resources.namespace import Namespace
+from ocp_resources.utils import TimeoutExpiredError
 from openshift.dynamic.exceptions import ForbiddenError
 
 from tests.compute.ssp.supported_os.common_templates.custom_namespace.utils import (
     get_template_by_name,
     verify_base_templates_exist_in_namespace,
+    wait_for_edited_label_reconciliation,
+    wait_for_template_by_name,
 )
 from utilities.constants import OPENSHIFT_NAMESPACE, UNPRIVILEGED_USER
 
+
+LOGGER = logging.getLogger(__name__)
 
 TESTS_CLASS_NAME = "TestCustomNamespace"
 
@@ -52,6 +60,72 @@ class TestCustomNamespace:
                 name=template_name,
             )
 
+    @pytest.mark.polarion("CNV-8142")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_deleted_template_in_custom_namespace_reconciled",
+        depends=[f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_custom_namespace"],
+    )
+    def test_deleted_template_in_custom_namespace_reconciled(
+        self,
+        admin_client,
+        custom_vm_template_namespace,
+        deleted_custom_namespace_template,
+    ):
+        try:
+            wait_for_template_by_name(
+                client=admin_client,
+                namespace_name=custom_vm_template_namespace.name,
+                name=deleted_custom_namespace_template.name,
+            )
+        except TimeoutExpiredError:
+            LOGGER.error(
+                f"Template {deleted_custom_namespace_template.name} in custom namespace "
+                f"{custom_vm_template_namespace.name} not reconciled after deletion."
+            )
+            raise
+
+    @pytest.mark.polarion("CNV-8164")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_edited_template_in_custom_namespace_reconciled",
+        depends=[f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_custom_namespace"],
+    )
+    def test_edited_template_in_custom_namespace_reconciled(
+        self,
+        admin_client,
+        custom_vm_template_namespace,
+        edited_custom_namespace_template,
+    ):
+        try:
+            wait_for_edited_label_reconciliation(
+                template=edited_custom_namespace_template
+            )
+        except TimeoutExpiredError:
+            LOGGER.error(
+                f"Template {edited_custom_namespace_template.name} in custom namespace "
+                f"{custom_vm_template_namespace.name} not reconciled after label edit."
+            )
+            raise
+
+    @pytest.mark.polarion("CNV-8163")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_create_vm_custom_template_namespace",
+        depends=[f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_custom_namespace"],
+    )
+    def test_create_vm_custom_template_namespace(
+        self,
+        first_base_template,
+        custom_vm_template_namespace,
+        vm_from_template_labels,
+    ):
+        try:
+            vm_from_template_labels.deploy()
+        except ApiException:
+            LOGGER.error(
+                f"Failed to create VM using template {first_base_template.name} "
+                f"from custom namespace {custom_vm_template_namespace.name}"
+            )
+            raise
+
     @pytest.mark.polarion("CNV-8143")
     @pytest.mark.dependency(
         name=f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_default_namespace_after_revert",
@@ -72,3 +146,69 @@ class TestCustomNamespace:
             original_base_templates=base_templates,
             namespace=Namespace(name=OPENSHIFT_NAMESPACE),
         )
+
+    @pytest.mark.polarion("CNV-8152")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_deleted_template_default_namespace_reconciled_after_revert",
+        depends=[
+            f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_default_namespace_after_revert",
+        ],
+    )
+    def test_deleted_template_default_namespace_reconciled_after_revert(
+        self, admin_client, deleted_default_namespace_template
+    ):
+        try:
+            wait_for_template_by_name(
+                client=admin_client,
+                namespace_name=OPENSHIFT_NAMESPACE,
+                name=deleted_default_namespace_template.name,
+            )
+        except TimeoutExpiredError:
+            LOGGER.error(
+                f"Template {deleted_default_namespace_template.name} in default namespace "
+                f"{OPENSHIFT_NAMESPACE} not reconciled after deletion."
+            )
+            raise
+
+    @pytest.mark.polarion("CNV-8189")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_edited_template_default_namespace_reconciled_after_revert",
+        depends=[
+            f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_default_namespace_after_revert",
+        ],
+    )
+    def test_edited_template_default_namespace_reconciled_after_revert(
+        self, admin_client, edited_default_namespace_template
+    ):
+        try:
+            wait_for_edited_label_reconciliation(
+                template=edited_default_namespace_template
+            )
+        except TimeoutExpiredError:
+            LOGGER.error(
+                f"Template {edited_default_namespace_template.name} in default namespace "
+                f"{OPENSHIFT_NAMESPACE} not reconciled after label edit."
+            )
+            raise
+
+    @pytest.mark.polarion("CNV-8190")
+    @pytest.mark.dependency(
+        name=f"{TESTS_CLASS_NAME}::test_edited_template_custom_namespace_not_reconciled_after_revert",
+        depends=[
+            f"{TESTS_CLASS_NAME}::test_base_templates_exist_in_default_namespace_after_revert",
+        ],
+    )
+    def test_edited_template_custom_namespace_not_reconciled_after_revert(
+        self,
+        admin_client,
+        custom_vm_template_namespace,
+        edited_custom_namespace_template,
+    ):
+        with pytest.raises(TimeoutExpiredError):
+            wait_for_edited_label_reconciliation(
+                template=edited_custom_namespace_template
+            )
+            LOGGER.error(
+                f"Template {edited_custom_namespace_template.name} in custom namespace "
+                f"{custom_vm_template_namespace.name} reconciled after label edit post-revert."
+            )
--- tests/compute/ssp/supported_os/common_templates/custom_namespace/utils.py
+++ tests/compute/ssp/supported_os/common_templates/custom_namespace/utils.py
@@ -1,11 +1,18 @@
 import logging
+from contextlib import contextmanager
 
+from ocp_resources.resource import Resource, ResourceEditor
 from ocp_resources.template import Template
 from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
 
-from utilities.constants import TIMEOUT_1MIN
-from utilities.virt import get_base_templates_list
+from utilities.constants import TIMEOUT_1MIN, TIMEOUT_3MIN
+from utilities.hco import wait_for_hco_conditions
+from utilities.ssp import wait_for_ssp_conditions
+from utilities.virt import VirtualMachineForTestsFromTemplate, get_base_templates_list
 
+
+TEMPLATE_PATCH_LABEL = f"{Resource.ApiGroup.APP_KUBERNETES_IO}/managed-by"
+TEMPLATE_PATCH_LABEL_VALUE = "general-kenobi"
 
 LOGGER = logging.getLogger(__name__)
 
@@ -35,6 +42,64 @@ def get_template_by_name(client, namespace_name, name):
     return template
 
 
+def wait_for_template_by_name(client, namespace_name, name, timeout=TIMEOUT_3MIN):
+    for sample in TimeoutSampler(
+        wait_timeout=timeout,
+        sleep=5,
+        func=get_template_by_name,
+        exceptions_dict={AssertionError: []},
+        client=client,
+        namespace_name=namespace_name,
+        name=name,
+    ):
+        if sample:
+            LOGGER.info(f"Template {name} found in namespace {namespace_name}")
+            return
+
+
+def delete_template_by_name(admin_client, namespace_name, template_name):
+    template = get_template_by_name(
+        client=admin_client,
+        namespace_name=namespace_name,
+        name=template_name,
+    )
+    template.clean_up()
+    return template
+
+
+def patch_template_labels(admin_client, hco_namespace, template):
+    with ResourceEditor(
+        patches={
+            template: {
+                "metadata": {
+                    "labels": {TEMPLATE_PATCH_LABEL: TEMPLATE_PATCH_LABEL_VALUE}
+                }
+            }
+        }
+    ):
+        yield template
+
+    wait_for_ssp_conditions(
+        admin_client=admin_client,
+        hco_namespace=hco_namespace,
+    )
+    wait_for_hco_conditions(
+        admin_client=admin_client,
+        hco_namespace=hco_namespace,
+    )
+
+
+def wait_for_edited_label_reconciliation(template, timeout=TIMEOUT_3MIN):
+    for sample in TimeoutSampler(
+        wait_timeout=timeout,
+        sleep=5,
+        func=lambda: template.instance.metadata.labels[TEMPLATE_PATCH_LABEL]
+        != TEMPLATE_PATCH_LABEL_VALUE,
+    ):
+        if sample:
+            return
+
+
 def verify_base_templates_exist_in_namespace(
     client, original_base_templates, namespace
 ):
@@ -60,6 +125,43 @@ def verify_base_templates_exist_in_namespace(
                     return True
 
     except TimeoutExpiredError:
-        error_message = missing_template_names or "all templates are missing"
-        LOGGER.error(f"Templates not in namespace {namespace.name}: {error_message}")
+        LOGGER.error(
+            f"Templates not in namespace {namespace.name}: {missing_template_names}"
+        )
         raise
+
+
+def extract_template_labels(template_labels):
+    extracted_vm_template_labels = {}
+    for key in template_labels.keys():
+        label_prefix, label_name = key.split("/")
+        if label_prefix in (
+            Template.Labels.OS,
+            Template.Labels.WORKLOAD,
+            Template.Labels.FLAVOR,
+        ):
+            extracted_vm_template_labels[label_prefix] = label_name
+
+    return extracted_vm_template_labels
+
+
+@contextmanager
+def diskless_vm_from_template(client, name, namespace, base_template_labels):
+    extracted_template_labels = extract_template_labels(
+        template_labels=base_template_labels
+    )
+    template_labels = Template.generate_template_labels(
+        os=extracted_template_labels[Template.Labels.OS],
+        workload=extracted_template_labels[Template.Labels.WORKLOAD],
+        flavor=extracted_template_labels[Template.Labels.FLAVOR],
+    )
+    vm = VirtualMachineForTestsFromTemplate(
+        client=client,
+        name=name,
+        namespace=namespace.name,
+        labels=template_labels,
+        diskless_vm=True,
+    )
+    yield vm
+    if vm.exists:
+        vm.clean_up()
