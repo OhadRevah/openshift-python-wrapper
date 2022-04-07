687c412f1ccd93a1d064d70384b9f12dd89a6f34
Meni Yakove <myakove@redhat.com>
2022-04-07 18:15:11 +0300

    py config for iterop sno run
    Change-Id: I3af8bf0a41b08ebe57cb5f6d3469d8c8f8c67831

---
+++ tests/global_config_interop_sno.py
@@ -0,0 +1,27 @@
+import pytest_testconfig
+from ocp_resources.datavolume import DataVolume
+
+
+global config
+global_config = pytest_testconfig.load_python(
+    py_file="tests/global_config.py", encoding="utf-8"
+)
+
+storage_class_matrix = [
+    {
+        "sno-storage": {
+            "volume_mode": DataVolume.VolumeMode.BLOCK,
+            "access_mode": DataVolume.AccessMode.RWO,
+        }
+    },
+]
+
+for _dir in dir():
+    val = locals()[_dir]
+    if type(val) not in [bool, list, dict, str]:
+        continue
+
+    if _dir in ["encoding", "py_file"]:
+        continue
+
+    config[_dir] = locals()[_dir]  # noqa: F821
