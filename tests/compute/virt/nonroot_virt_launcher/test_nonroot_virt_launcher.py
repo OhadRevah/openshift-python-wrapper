394e8be8b2b78905e8ea318420c1746bb3cc849f
akriti gupta <akrgupta@redhat.com>
2022-06-03 15:16:14 +0530

    test nonRoot FeatureGate in HCO CR
    Signed-off-by: akriti gupta <akrgupta@redhat.com>
    Change-Id: Iddfd1563ac2f0f7c631f47d0b4d4129e935ae4e1
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/413287
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>

--- tests/compute/virt/nonroot_virt_launcher/test_nonroot_virt_launcher.py
+++ tests/compute/virt/nonroot_virt_launcher/test_nonroot_virt_launcher.py
@@ -6,7 +6,12 @@ from pytest_testconfig import config as py_config
 
 from tests.compute.utils import validate_pause_optional_migrate_unpause_linux_vm
 from tests.os_params import RHEL_LATEST, RHEL_LATEST_LABELS, RHEL_LATEST_OS
-from utilities.virt import migrate_vm_and_verify, vm_instance_from_template
+from utilities.hco import ResourceEditorValidateHCOReconcile, wait_for_hco_conditions
+from utilities.virt import (
+    migrate_vm_and_verify,
+    vm_instance_from_template,
+    wait_for_kubevirt_conditions,
+)
 
 
 NONROOT_ANNOTATION = f"{Resource.ApiGroup.KUBEVIRT_IO}/nonroot"
@@ -48,6 +53,32 @@ def migrated_nonroot_vm_scope_class(
     nonroot_vm_scope_class,
 ):
     migrate_vm_and_verify(vm=nonroot_vm_scope_class)
+
+
+@pytest.fixture()
+def hco_cr_with_nonroot_featuregate_as_false(
+    hyperconverged_resource_scope_function, admin_client, hco_namespace
+):
+    with ResourceEditorValidateHCOReconcile(
+        patches={
+            hyperconverged_resource_scope_function: {
+                "spec": {
+                    "featureGates": {
+                        "nonRoot": False,
+                    }
+                }
+            }
+        },
+    ):
+        wait_for_kubevirt_conditions(
+            admin_client=admin_client,
+            hco_namespace=hco_namespace,
+        )
+        wait_for_hco_conditions(
+            admin_client=admin_client,
+            hco_namespace=hco_namespace,
+        )
+        yield
 
 
 @pytest.mark.parametrize(
@@ -92,3 +123,25 @@ class TestNonRootVirtLauncherPod:
             vm=nonroot_vm_scope_class, migrate=True
         )
         assert_virt_launcher_pod_is_root(vm=nonroot_vm_scope_class)
+
+
+@pytest.mark.polarion("CNV-8520")
+def test_default_nonroot_featuregate_in_hco_cr(hco_spec, kubevirt_feature_gates):
+    assert hco_spec["featureGates"][
+        "nonRoot"
+    ], f"NonRoot Feature Gate is set as {hco_spec['featureGates']['nonRoot']} in HCO CR"
+    assert (
+        "NonRoot" in kubevirt_feature_gates
+    ), f"FeatureGates does not have NonRoot in the List {kubevirt_feature_gates}"
+
+
+@pytest.mark.polarion("CNV-8521")
+def test_nonroot_featuregate_false(
+    hco_cr_with_nonroot_featuregate_as_false, hco_spec, kubevirt_feature_gates
+):
+    assert not hco_spec["featureGates"][
+        "nonRoot"
+    ], f"NonRoot Feature Gate is set as {hco_spec['featureGates']['nonRoot']} in HCO CR"
+    assert (
+        "NonRoot" not in kubevirt_feature_gates
+    ), f"FeatureGates have NonRoot in the List {kubevirt_feature_gates}"
