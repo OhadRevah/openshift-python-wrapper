7c6345baf0dc99ce1bdf0dca270113e16671902c
Meni Yakove <myakove@gmail.com>
2022-06-27 16:34:00 +0300

    Update utilities install_requires
    Use: pip install git+https://code.engineering.redhat.com/gerrit/cnv-tests
    
    Change-Id: I219e1a1dea41bbea6d6faaee80a0583036959616
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/417018
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>
    Tested-by: Meni Yakove <myakove@redhat.com>

--- setup.py
+++ setup.py
@@ -5,11 +5,21 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="cnv-resources",
+    name="cnv-utilities",
     version="1.0",
-    packages=find_packages(include=["resources", "utilities"]),
+    packages=find_packages(include=["utilities"]),
     include_package_data=True,
     package_data={"": ["utilities/manifests/*"]},
-    install_requires=["kubernetes", "openshift", "xmltodict"],
+    install_requires=[
+        "kubernetes",
+        "openshift",
+        "xmltodict",
+        "python-bugzilla",
+        "netaddr",
+        "paramiko",
+        "pytest",
+        "jira",
+        "openshift-python-wrapper",
+    ],
     python_requires=">=3.6",
 )
