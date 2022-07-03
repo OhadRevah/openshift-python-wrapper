4ec173b81d56bc3e5812bebd5faba1df5ed97ff9
Jenia Peimer <jpeimer@redhat.com>
2022-07-03 23:01:17 +0300

    SNO global config: storage class matrix
    Change-Id: I7903df175f3399a6509d40c57fc964054bcd51ed
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/418203
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Tested-by: Jenia Peimer <jpeimer@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>

---
+++ tests/global_config_sno.py
@@ -0,0 +1,40 @@
+import pytest_testconfig
+from ocp_resources.datavolume import DataVolume
+from ocp_resources.storage_class import StorageClass
+
+from utilities.storage import HppCsiStorageClass
+
+
+global config
+global_config = pytest_testconfig.load_python(
+    py_file="tests/global_config.py", encoding="utf-8"
+)
+
+HPP_VOLUME_MODE_ACCESS_MODE = {
+    "volume_mode": DataVolume.VolumeMode.FILE,
+    "access_mode": DataVolume.AccessMode.RWO,
+}
+
+new_hpp_storage_class_matrix = [
+    {HppCsiStorageClass.Name.HOSTPATH_CSI_BASIC: HPP_VOLUME_MODE_ACCESS_MODE},
+    {HppCsiStorageClass.Name.HOSTPATH_CSI_PVC_BLOCK: HPP_VOLUME_MODE_ACCESS_MODE},
+]
+
+storage_class_matrix = [
+    {
+        StorageClass.Types.NFS: {
+            "volume_mode": DataVolume.VolumeMode.FILE,
+            "access_mode": DataVolume.AccessMode.RWX,
+        }
+    },
+]
+
+for _dir in dir():
+    val = locals()[_dir]
+    if type(val) not in [bool, list, dict, str, int]:
+        continue
+
+    if _dir in ["encoding", "py_file"]:
+        continue
+
+    config[_dir] = locals()[_dir]  # noqa: F821
