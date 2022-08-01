88513cef669d87286b828c36aa2922b698f694b6
Jenia Peimer <jpeimer@redhat.com>
2022-08-01 15:06:36 +0300

    [Storage] Fix storage_class_for_snapshot fixture
    Previously we would get an Error when there's no
    storage class for snapshot, because the fixture yields nothing
    
    Change-Id: If794fd20ad2e3c9c7d92f839d55a5d4b1d067a90
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/422344
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Kevin Goldblatt <kgoldbla@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Tested-by: Jenia Peimer <jpeimer@redhat.com>

--- tests/storage/upgrade/conftest.py
+++ tests/storage/upgrade/conftest.py
@@ -184,11 +184,18 @@ def fedora_vm_for_hotplug_upg_ssh_connectivity(fedora_vm_for_hotplug_upg):
 @pytest.fixture(scope="session")
 def storage_class_for_snapshot(admin_client):
     available_storage_classes = py_config["storage_class_matrix"]
+    sc_for_snapshot = None
+    sc_names = []
     for sc in available_storage_classes:
         sc_name = [*sc][0]
         if is_snapshot_supported_by_sc(sc_name=sc_name, client=admin_client):
-            LOGGER.info(f"Storage class for snapshot: {sc_name}")
-            yield sc_name
+            sc_for_snapshot = sc_name
+            LOGGER.info(f"Storage class for snapshot: {sc_for_snapshot}")
+            break
+        sc_names.append(sc_name)
+    if not sc_for_snapshot:
+        LOGGER.warning(f"No Storage class among {sc_names} supports snapshots")
+    yield sc_for_snapshot
 
 
 @pytest.fixture(scope="session")
